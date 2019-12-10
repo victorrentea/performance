47fbc81499dd370cc5a19b88d36b4def281a82c0
Victor Rentea <victorrentea@gmail.com>
2019-12-10 16:54:14 +0200

    Lansarea in paralel a unei procesari indepndente (poate un WS tertz) ma lasa sa fac propria treaba in timp ce astept.

--- concurrency/src/main/java/victor/training/concurrency/APlusPlus.java
+++ concurrency/src/main/java/victor/training/concurrency/APlusPlus.java
@@ -2,8 +2,10 @@ package victor.training.concurrency;
 
 import static victor.training.concurrency.ConcurrencyUtil.log;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +14,7 @@ public class APlusPlus {
 	private static int bumbacTotal;
 	private static final Object monitor = new Object();
 
-	public static void culegeBumbac() {
+	public static int culegeBumbac() {
 		log("Ma pornesc la cules");
 		int bumbac = 0;
 		for (int i = 0; i < 1000_000; i++) {
@@ -22,40 +24,29 @@ public class APlusPlus {
 			bumbacTotal += bumbac;
 		}
 		log("AM terminat");
+		return 3;
 	}
 
 	static ExecutorService pool;
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		pool = Executors.newFixedThreadPool(2); // asta face springu
 		buzinss();
 		pool.shutdown();
 	}
 
-	private static void buzinss() {
+	private static void buzinss() throws InterruptedException, ExecutionException {
 		long t0 = System.currentTimeMillis();
-		pool.execute(new Runnable() {
-			public void run() {
-				culegeBumbac();				
-			}
-		});
-		pool.execute(new Runnable() {
-			public void run() {
-				culegeBumbac();				
-			}
-		});
-		pool.execute(new Runnable() {
-			public void run() {
-				culegeBumbac();				
-			}
-		});
+//		Future<Integer>
+		
+		Future<Integer> future3 = pool.submit(() -> culegeBumbac());
+//		pool.execute(() -> culegeBumbac());
+//		pool.execute(() -> culegeBumbac());
+		
 		ConcurrencyUtil.sleep2(100);
-		int fostu = -1;
-		int actualu = 0;
-		while (fostu != actualu) { // buzy-wait
-			fostu = actualu;
-			actualu = bumbacTotal;
-			ConcurrencyUtil.sleep2(2);
-		}
+		System.out.println("Am terminat treaba mea");
+		
+		future3.get(); // blocheaza threadul curent pana e gata si ala
+		
 		System.out.println("Total = " + bumbacTotal);
 		
 		long t1 = System.currentTimeMillis();
