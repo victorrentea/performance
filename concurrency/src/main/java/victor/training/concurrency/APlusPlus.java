f780477b91f397f0f93b518b97e451a90e440cd5
Victor Rentea <victorrentea@gmail.com>
2019-12-10 15:21:23 +0200

    Map-Reduce. Am redus timpul in care rulez sincronizat cu alte threadrui la o singura instructiune: +=.
    Idee: rulezi cat mai mult timp poti de unul singur

--- concurrency/src/main/java/victor/training/concurrency/APlusPlus.java
+++ concurrency/src/main/java/victor/training/concurrency/APlusPlus.java
@@ -5,37 +5,56 @@ import java.util.concurrent.atomic.AtomicInteger;
 import lombok.extern.slf4j.Slf4j;
 
 public class APlusPlus {
-    private static AtomicInteger a = new AtomicInteger(0);
-    private static final Object monitor = new Object();
+	private static int a;
+	private static final Object monitor = new Object();
 
-    public static class ThreadA extends Thread {
-        public void run() {
-    		for (int i = 0; i < 100_000; i++) {
-        		a.getAndIncrement();
-            }
-        }
-    }
-    public static class ThreadB extends Thread {
-        public void run() {
-    		for (int i = 0; i < 100_000; i++) {
-    			a.getAndIncrement();
-            }
-        }
-    }
-    // TODO (bonus): ConcurrencyUtil.useCPU(1)
-    public static void main(String[] args) throws InterruptedException {
-        ThreadA threadA = new ThreadA();
-        ThreadB threadB = new ThreadB();
+	public static class ThreadA extends Thread {
+		public void run() {
+			int aLocal = 0;
+			for (int i = 0; i < 1000_000/10; i++) {
+				aLocal++;
+				aLocal++;
+				aLocal++;
+				aLocal++;
+				aLocal++;
+				aLocal++;
+				aLocal++;
+				aLocal++;
+				aLocal++;
+				aLocal++;
+			}
+			synchronized (monitor) {
+				a += aLocal;
+			}
+		}
+	}
 
-        long t0 = System.currentTimeMillis();
+	public static class ThreadB extends Thread {
+		public void run() {
+			int aLocal = 0;
+			for (int i = 0; i < 1000_000; i++) {
+				aLocal++;
+			}
+			synchronized (monitor) {
+				a += aLocal;
+			}
+		}
+	}
 
-        threadA.start();
-        threadB.start();
-        threadA.join();
-        threadB.join();
+	// TODO (bonus): ConcurrencyUtil.useCPU(1)
+	public static void main(String[] args) throws InterruptedException {
+		ThreadA threadA = new ThreadA();
+		ThreadB threadB = new ThreadB();
 
-        long t1 = System.currentTimeMillis();
-        System.out.println("Total = " + a);
-        System.out.println("Took = " + (t1 - t0));
-    }
+		long t0 = System.currentTimeMillis();
+
+		threadA.start();
+		threadB.start();
+		threadA.join();
+		threadB.join();
+
+		long t1 = System.currentTimeMillis();
+		System.out.println("Total = " + a);
+		System.out.println("Took = " + (t1 - t0));
+	}
 }
