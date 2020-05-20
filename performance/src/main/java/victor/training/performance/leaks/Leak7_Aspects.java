37b736471d7539d7f164c057c06afae1a3ad00b5
Victor Rentea <victorrentea@gmail.com>
2020-05-20 12:49:45 +0300

    Cacheable

--- performance/src/main/java/victor/training/performance/leaks/Leak7_Aspects.java
+++ performance/src/main/java/victor/training/performance/leaks/Leak7_Aspects.java
@@ -2,9 +2,11 @@ package victor.training.performance.leaks;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +17,14 @@ import java.time.LocalDateTime;
 public class Leak7_Aspects {
 	@Autowired
     private  Stuff stuff;
-	@GetMapping
-	public String test() {
-		stuff.stuff(LocalDateTime.now().toString());
+	@GetMapping("{id}")
+	public String get(@PathVariable long id) {
+		stuff.stuff(id);
 		return "Tools won't shield you from stupidity.";
+	}
+	@GetMapping("{id}/update")
+	public void update(@PathVariable long id) {
+		stuff.updateStuff(id, "nume nou");
 	}
 }
 
@@ -26,8 +32,12 @@ public class Leak7_Aspects {
 @Slf4j
 class Stuff {
 	@Cacheable("stuff")
-	public BigObject20MB stuff(String timestamp) {
-		log.debug("Calling method for {}", timestamp);
+	public BigObject20MB stuff(long id) {
+		log.debug("Calling expensive method for id {}", id);
 	    return new BigObject20MB();
+	}
+	@CacheEvict(cacheNames = "stuff",key = "#id")
+	public void updateStuff(long id, String newName) {
+		// UPDATE SET = in DB
 	}
 }
