f177d131a173d80c3d06a81b09f9bcef687cf15e
Victor Rentea <victorrentea@gmail.com>
2020-05-20 13:41:16 +0300

    My first pool

--- performance/src/main/java/victor/training/performance/pools/PrimaMeaPiscina.java
+++ performance/src/main/java/victor/training/performance/pools/PrimaMeaPiscina.java
@@ -1,10 +1,17 @@
 package victor.training.performance.pools;
 
+import org.jooq.lambda.Unchecked;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class PrimaMeaPiscina {
 
@@ -12,20 +19,33 @@ public class PrimaMeaPiscina {
         Barman barman = new Barman();
         String comenzi ="b,b,v,v,v,b,b";
 
-//        ExecutorService pool = Executors.newFixedThreadPool(3);
-        List<Drink> drinks = new ArrayList<>();
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+        Callable<Beer> pourBeer = barman::pourBeer;
+        Callable<Vodka> pourVodka = barman::pourVodka;
+
+        List<Future<? extends Drink>> futureDrinks = new ArrayList<>();
 
         long t0 = System.currentTimeMillis();
         for (String comanda : comenzi.split(",")) {
             if (comanda.equals("b")) {
-                drinks.add(barman.pourBeer());
+                Future<Beer> futureBeer = pool.submit(pourBeer);
+                futureDrinks.add(futureBeer);
+//                futureBeer.get();
             } else {
-                drinks.add(barman.pourVodka());
+                Future<Vodka> futureVodka = pool.submit(pourVodka);
+                futureDrinks.add(futureVodka);
             }
         }
-        long t1 = System.currentTimeMillis();
+        // pana aici ai plasat comenzile
+
+        List<? extends Drink> drinks = futureDrinks.stream()
+                .map(Unchecked.function(Future::get)) // .get blocheaza threadul main pana e gata respectivul future
+                .collect(toList());
 
         System.out.println("Beu: " + drinks);
+
+        long t1 = System.currentTimeMillis();
+
         System.out.println("Took " + (t1-t0));
     }
 }
