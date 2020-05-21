5bdb25c28e818c3f038dfd99da2478e3935f048e
Victor Rentea <victorrentea@gmail.com>
2020-05-21 12:59:48 +0300

    Start dynamic queries

--- performance/src/main/java/victor/training/jpa/perf/AddressRepo.java
+++ performance/src/main/java/victor/training/jpa/perf/AddressRepo.java
@@ -3,10 +3,15 @@ package victor.training.jpa.perf;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface AddressRepo extends JpaRepository<Address, Long> {
 
+//    @Query(nativeQuery = true)
     Page<Address> findAllByCityId(Long id, Pageable page);
+
+    @Query(nativeQuery= true, value = "SELECT count(*) FROM PARENT")
+    int countX();
 }
