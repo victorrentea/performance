fc3466f0766b4883786cd40ebf0d146a16b6105f
Victor Rentea <victorrentea@gmail.com>
2020-05-22 12:12:16 +0300

    ParallelStreams: NU CARE CUMVA sa faci vreun apel blocant in vreun pas de procesare pe un stream parallel. Vei bloca TOT JVM-ul. pentru ca commonpoolul pe care lucrezi by default este shareuit intot JVM

--- performance/src/main/java/victor/training/performance/java8/ParallelStreams.java
+++ performance/src/main/java/victor/training/performance/java8/ParallelStreams.java
@@ -2,12 +2,16 @@ package victor.training.performance.java8;
 
 import victor.training.performance.ConcurrencyUtil;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static java.util.stream.Collectors.toList;
 import static victor.training.performance.ConcurrencyUtil.log;
+import static victor.training.performance.ConcurrencyUtil.sleep2;
 
 public class ParallelStreams {
 
@@ -15,6 +19,8 @@ public class ParallelStreams {
         System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "3"); // by def paralleismul common pool = NCore -1
         List<Integer> numbers = IntStream.range(1, 11).boxed().collect(toList());
 
+//        Iterable<Integer> iterable = numbers::iterator;
+//        Stream<Integer> stream = StreamSupport.stream(iterable.spliterator(), false);
 
         numbers.parallelStream()
                 .filter(n -> {
@@ -23,6 +29,7 @@ public class ParallelStreams {
                 })
                 .map(n -> {
                     log("Square " + n);
+                    sleep2(1000); // chemi un WS, citesti un fisier, apelezi DB
                     return n * n;
                 })
                 .forEach(n -> log("OUT: " + n));
