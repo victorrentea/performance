614bfb707270c7e2a65f2e92d60e0a0906e42ef9
Victor Rentea <victorrentea@gmail.com>
2020-05-22 12:05:49 +0300

    ParallelStreams: un item este procesat integral (print toti pasii chainuitzi pe stream) in acelasi worker thread.

--- performance/src/main/java/victor/training/performance/java8/ParallelStreams.java
+++ performance/src/main/java/victor/training/performance/java8/ParallelStreams.java
@@ -1,4 +1,31 @@
 package victor.training.performance.java8;
 
+import victor.training.performance.ConcurrencyUtil;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+import static victor.training.performance.ConcurrencyUtil.log;
+
 public class ParallelStreams {
+
+    public static void main(String[] args) {
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "3"); // by def paralleismul common pool = NCore -1
+        List<Integer> numbers = IntStream.range(1, 11).boxed().collect(toList());
+
+
+        numbers.parallelStream()
+                .filter(n -> {
+                    log("Filtrez " + n);
+                    return n % 2 == 1;
+                })
+                .map(n -> {
+                    log("Square " + n);
+                    return n * n;
+                })
+                .forEach(n -> log("OUT: " + n));
+        ;
+    }
 }
