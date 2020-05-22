1b1ba6e51eba2e746a8300c7e99903da8611ba85
Victor Rentea <victorrentea@gmail.com>
2020-05-22 11:49:35 +0300

    cancel(interrupt=true) mege pe pool-uri 'standard', non Fork-Join

---
+++ performance/src/main/java/victor/training/performance/pools/spring/CancelCompletable.java
@@ -0,0 +1,60 @@
+package victor.training.performance.pools.spring;
+
+import lombok.Data;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import victor.training.performance.pools.BarmanJavaSE;
+import victor.training.performance.pools.Beer;
+import victor.training.performance.pools.Vodka;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static java.util.concurrent.CompletableFuture.runAsync;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
+public class CancelCompletable {
+    public static void main(String[] args) throws InterruptedException {
+
+
+//        FJP
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+
+        List<Future<?>> list = new ArrayList<>();
+        for (int i = 0; i < 400; i++) {
+            Future<?> futureVoid = pool.submit(new TaskCancel(i));
+            list.add(futureVoid);
+        }
+        Thread.sleep(500);
+        list.forEach(c -> c.cancel(true)); // true nu conteaza.
+        // dar cancel pare ca merge: nu mai ruleaza celelate
+
+
+        Thread.sleep(5000);
+
+
+    }
+}
+
+@Data
+@Slf4j
+class TaskCancel implements Runnable{
+    private final int i;
+
+    @SneakyThrows
+    @Override
+    public void run() {
+
+        log.debug("START " + i);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.debug("DONE " + i);
+    }
+}
