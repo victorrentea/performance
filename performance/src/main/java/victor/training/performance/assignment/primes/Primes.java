445c4190e172fafb24734bf18be442e80073848b
Victor Rentea <victorrentea@gmail.com>
2020-05-24 04:30:50 +0300

    Primes: bugfix

--- performance/src/main/java/victor/training/performance/assignment/primes/Primes.java
+++ performance/src/main/java/victor/training/performance/assignment/primes/Primes.java
@@ -54,8 +54,11 @@ public class Primes {
 
 
     public static boolean isPrime(long n) {
-        if (n == 1 || n % 2 == 0) {
+        if (n == 1 || n == 2) {
             return true;
+        }
+        if (n % 2 == 0) {
+            return false;
         }
         for (long i = 3; i < Math.sqrt(n); i += 2) {
             if (n % i == 0) {
