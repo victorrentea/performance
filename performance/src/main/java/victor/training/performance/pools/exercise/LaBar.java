b36426c4db9d28bc62a96ab9af4ad2a1fd077701
Victor Rentea <victorrentea@gmail.com>
2020-07-27 10:28:31 +0300

    NU busy waiting

--- performance/src/main/java/victor/training/performance/pools/exercise/LaBar.java
+++ performance/src/main/java/victor/training/performance/pools/exercise/LaBar.java
@@ -5,39 +5,48 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import victor.training.performance.ConcurrencyUtil;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 @Slf4j
 public class LaBar {
    //   private static final Logger log = LoggerFactory.getLogger(LaLigheane.class);
 
-   public static void main(String[] args) {
+   public static void main(String[] args) throws ExecutionException, InterruptedException {
      new LaBar().mergi();
    }
 
    private BarMan barMan = new BarMan();
 
-   public void mergi() {
+   public void mergi() throws ExecutionException, InterruptedException {
       log.debug("Trimit comanda");
       // cele doua apeluri nu depind unul de altul
 
-      for (int i = 0; i < 10_000; i++) {
-         Thread t = new Thread() {
-            @Override
-            public void run() {
-               // INTELIGENT
+      ExecutorService pool = Executors.newSingleThreadExecutor();
 
-               // Daca vrei sa vezi de ce te concediaza sefu cand faci new Thread, citeste din cartea Java Concurrency in Practice despre .interrupt.
-            }
-         };
+      Future<Bere> futureBeer = pool.submit(new Callable<Bere>() {
+         @Override
+         public Bere call() throws Exception {
+            return barMan.toarnaBere();
+         }
+      });
 
+      // mai jos, main-ul blocheaza unul dintre core-uri intergral (100% cpu usage pe 1 core)
+      // multithreadingul de multe ori te faci sa cauti alt job (in paralel) - ca vrei tu sau sefu
+      while (!futureBeer.isDone()) { // buzy waiting- o forma de polling -pe care nu vrei sa o faci niciodata
+//         ConcurrencyUtil.sleep2(1);
+         log.debug("Mai dureaza?");
       }
+      Bere bere = futureBeer.get();
 
-      Bere bere = barMan.toarnaBere();
-      Vodka vodka = barMan.toarnaVodka();
+      pool.submit(new Callable<Vodka>() {
+         @Override
+         public Vodka call() throws Exception {
+            return barMan.toarnaVodka();
+         }
+      });
+
       log.debug("Am trimis comanda");
-      log.debug("Savurez " + bere + " cu " + vodka);
+      log.debug("Savurez " + bere + " cu " /*+ vodka*/);
    }
 }
 
