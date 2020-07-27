183519a508515f1442342ec39a8d945182c6c658
Victor Rentea <victorrentea@gmail.com>
2020-07-27 10:18:34 +0300

    NU facem niciodata new Thread

--- performance/src/main/java/victor/training/performance/pools/exercise/LaBar.java
+++ performance/src/main/java/victor/training/performance/pools/exercise/LaBar.java
@@ -21,6 +21,19 @@ public class LaBar {
    public void mergi() {
       log.debug("Trimit comanda");
       // cele doua apeluri nu depind unul de altul
+
+      for (int i = 0; i < 10_000; i++) {
+         Thread t = new Thread() {
+            @Override
+            public void run() {
+               // INTELIGENT
+
+               // Daca vrei sa vezi de ce te concediaza sefu cand faci new Thread, citeste din cartea Java Concurrency in Practice despre .interrupt.
+            }
+         };
+
+      }
+
       Bere bere = barMan.toarnaBere();
       Vodka vodka = barMan.toarnaVodka();
       log.debug("Am trimis comanda");
---
+++ performance/src/main/java/victor/training/performance/pools/exercise/LaOraDeInfo.java
@@ -0,0 +1,7 @@
+package victor.training.performance.pools.exercise;
+
+public class LaOraDeInfo {
+   public static void main(String[] args) {
+      while (true) ;
+   }
+}
