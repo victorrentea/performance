a4d051a2570b8abc6c047503f1fe772adeeb51f6
Victor Rentea <victorrentea@gmail.com>
2020-07-28 09:32:17 +0300

    Date mutabile din 2 treaduri

---
+++ performance/src/main/java/victor/training/performance/DoarAleMele_ThreadLocals.java
@@ -0,0 +1,28 @@
+package victor.training.performance;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class DoarAleMele_ThreadLocals implements Runnable {
+   private static final Logger log = LoggerFactory.getLogger(DoarAleMele_ThreadLocals.class);
+
+   public static void main(String[] args) {
+      ExecutorService pool = Executors.newFixedThreadPool(2);
+
+      pool.submit(new DoarAleMele_ThreadLocals());
+      pool.submit(new DoarAleMele_ThreadLocals());
+   }
+
+   static int n = 0;
+
+   @Override
+   public void run() {
+      n++;
+      ConcurrencyUtil.sleep2(1000);
+      log.debug("Citesc: " + n);
+   }
+}
