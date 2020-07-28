b9ca27aa52bfc6e098bdb88186463a1fa50bad83
Victor Rentea <victorrentea@gmail.com>
2020-07-28 11:14:52 +0300

    JMH sa demonstram ca for unrolled nu e mai rapid decat ala default

---
+++ performance-jmh/src/main/java/victor/perf/ForLoopBoxking.java
@@ -0,0 +1,60 @@
+package victor.perf;
+
+import lombok.SneakyThrows;
+import org.apache.commons.io.IOUtils;
+import org.jooq.lambda.Unchecked;
+import org.openjdk.jmh.annotations.*;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.LongStream;
+
+import static java.util.stream.Collectors.toList;
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 5, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 20, time = 200, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(1)
+public class ForLoopBoxking {
+
+   private List<Long> list;
+
+   @Setup
+   public void initData() {
+      list = LongStream.range(1, 10_000_000)
+          .boxed().collect(toList());
+   }
+
+   @Benchmark
+   public long sumaUnrolleed() {
+      long sum = 0L;
+      for (int i = 0; i < list.size() / 4; i++) {
+         sum += list.get(i * 4);
+         sum += list.get(i * 4 + 1);
+         sum += list.get(i * 4 + 2);
+         sum += list.get(i * 4 + 3);
+      }
+      return sum;
+   }
+
+   @Benchmark
+   public long sumaBruta() {
+      long sum = 0L;
+      for (Long e : list) {
+         sum += e;
+      }
+      return sum;
+   }
+
+
+}
+
--- performance/src/main/java/victor/training/performance/interview/SummingBoxes.java
+++ performance/src/main/java/victor/training/performance/interview/SummingBoxes.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.stream.LongStream;
 
 public class SummingBoxes {
-
+//     77 68 72 71 76 60
     public static void main(String[] args) {
         List<Long> list = LongStream.range(1, 10_000_000)
 				.boxed().collect(toList());
@@ -14,8 +14,12 @@ public class SummingBoxes {
         long t0 = System.currentTimeMillis();
  //
         long sum = 0L;
-        for (Long i : list) {
-            sum += i;
+//        for (Long i : list) {
+        for (int i = 0; i < list.size() / 4; i++) {
+            sum += list.get(i*4);
+            sum += list.get(i*4 +1);
+            sum += list.get(i*4+ 2);
+            sum += list.get(i*4+ 3);
         }
 
         long t1 = System.currentTimeMillis();
