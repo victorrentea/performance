4aa32812c2555ea323292770ff943a10b01ee305
Victor Rentea <victorrentea@gmail.com>
2020-07-30 11:59:25 +0300

    atunci cand esti 100% sigur ca un ID tre sa existe in baza, sa faci findById e ineficient--> cauzeaza un SELECT

--- performance/src/main/java/victor/training/performance/batch/basic/DataFileGenerator.java
+++ performance/src/main/java/victor/training/performance/batch/basic/DataFileGenerator.java
@@ -14,7 +14,7 @@ public class DataFileGenerator {
         try (Writer writer = new FileWriter("data.xml")){ // TODO how to optimize this?
             writer.write("<root>\n");
             for (int i = 0; i < recordCount; i++) {
-                String cityName = "City " + i / 1000;
+                String cityName = "City " + i % 20; // 1 2 3 4 5 6 7 8 9 10 1 2 3 4
                 writer.write("<person><name>elem"+i+"</name><city>"+cityName+"</city></person>\n");
             }
             writer.write("</root>");
--- performance/src/main/java/victor/training/performance/batch/basic/PersonConverter.java
+++ performance/src/main/java/victor/training/performance/batch/basic/PersonConverter.java
@@ -3,19 +3,36 @@ package victor.training.performance.batch.basic;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toMap;
+
 public class PersonConverter implements ItemProcessor<PersonXml, Person> {
    @Autowired
    private CityRepo cityRepo;
+   private Map<String, Long> cities;
    @Override
-   public Person process(PersonXml item) throws Exception {
+   public Person process(PersonXml xmlItem) throws Exception {
       Person entity = new Person();
-      entity.setName(item.getName());
+      entity.setName(xmlItem.getName());
 
-      City city = cityRepo.findByName(item.getCity());
-      if (city == null) {
+      if (cities == null) {
+         cities = cityRepo.findAll().stream().collect(toMap(City::getName, City::getId));
+      }
+
+      Long cityId = cities.get(xmlItem.getCity());
+
+      City city;
+      if (cityId == null) {
+         // de 10 ori in total
          city = new City();
-         city.setName(item.getCity());
+         city.setName(xmlItem.getCity());
          cityRepo.save(city);
+         cities.put(city.getName(), city.getId());
+      } else {
+         // 9990 ori
+         city = cityRepo.findById(cityId).get();
       }
 
       entity.setCity(city);
