7a3b662e3698a0cc90caf6766d794706fe6639eb
Victor Rentea <victorrentea@gmail.com>
2020-07-31 16:12:03 +0300

    in case you are doing lots of computation per item, the stream() overhead compared to an imperative for is negelctible

--- performance-jmh/src/main/java/victor/perf/StreamsCPUOnlyTest.java
+++ performance-jmh/src/main/java/victor/perf/StreamsCPUOnlyTest.java
@@ -1,43 +1,43 @@
-//package victor.perf;
-//
-//import org.openjdk.jmh.annotations.*;
-//
-//import java.util.concurrent.TimeUnit;
-//import java.util.stream.IntStream;
-//
-//@State(Scope.Thread)
-//@BenchmarkMode(Mode.AverageTime)
-//@OutputTimeUnit(TimeUnit.MICROSECONDS)
-//@Warmup(iterations = 5, time = 500, timeUnit = TimeUnit.MILLISECONDS)
-//@Measurement(iterations = 50, time = 200, timeUnit = TimeUnit.MILLISECONDS)
-//@Fork(1)
-//public class StreamsCPUOnlyTest {
-//
-//
-//    public static final int N = 10_000;
-//
-//    @Benchmark
-//	public int forClassic() {
-//		int sum = 0;
-//		for (int i = 0; i< N; i++) {
-//			sum += cpuOnlyTask(i);
-//		}
-//		return sum;
-//	}
-//
-//	@Benchmark
-//	public int streamSerial() {
-//		return IntStream.range(0, N).map(this::cpuOnlyTask).sum();
-//	}
-//
-//	@Benchmark
-//	public int streamParallel() {
-//		return IntStream.range(0, N).parallel().map(this::cpuOnlyTask).sum();
-//	}
-//
-//	public int cpuOnlyTask(int n) {
+package victor.perf;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 5, time = 500, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 50, time = 200, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(1)
+public class StreamsCPUOnlyTest {
+
+
+    public static final int N = 10_000;
+
+    @Benchmark
+	public int forClassic() {
+		int sum = 0;
+		for (int i = 0; i< N; i++) {
+			sum += cpuOnlyTask(i);
+		}
+		return sum;
+	}
+
+	@Benchmark
+	public int streamSerial() {
+		return IntStream.range(0, N).map(this::cpuOnlyTask).sum();
+	}
+
+	@Benchmark
+	public int streamParallel() {
+		return IntStream.range(0, N).parallel().map(this::cpuOnlyTask).sum();
+	}
+
+	public int cpuOnlyTask(int n) {
 //		return n * n;
-////		return (int) Math.sqrt(n);
-//	}
-//
-//}
+		return (int) Math.sqrt(n);
+	}
+
+}
