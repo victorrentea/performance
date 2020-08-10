982c7990ddc3e43e5fc60130ca71a549e3a5e491
Victor Rentea <victorrentea@gmail.com>
2020-08-10 16:10:18 +0300

    added lambda memory leak

--- performance/src/main/java/victor/training/performance/leaks/BigObject20MB.java
+++ performance/src/main/java/victor/training/performance/leaks/BigObject20MB.java
@@ -6,4 +6,8 @@ import java.util.Date;
 public class BigObject20MB implements Serializable {
 	public Date date = new Date();
 	public int[] largeArray = new int[5*1024*1024];
+
+	public int lookup(int index) {
+		return largeArray[index];
+	}
 }
