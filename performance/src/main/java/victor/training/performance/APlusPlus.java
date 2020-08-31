fc4a1dd8923d171d62aa054e79164957324d325e
Victor Rentea <victorrentea@gmail.com>
2020-08-31 15:41:58 +0300

    fix

--- performance/src/main/java/victor/training/performance/APlusPlus.java
+++ performance/src/main/java/victor/training/performance/APlusPlus.java
@@ -1,37 +1,21 @@
 package victor.training.performance;
 
-import victor.training.performance.java8.CompletableFutures;
-
-import java.util.concurrent.Callable;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.function.Supplier;
-
 public class APlusPlus {
-    private static int population;
-    private static final Object MONITOR = new Object();
+    private static Integer population = 0;
 
-    public static class Taska implements  Supplier<Integer> {
-        @Override
-        public Integer get()  {
-            int localPopulation = 0;
-            for (int i = 0; i < 100_000; i++) {
-                localPopulation++;
+    public static class ThreadA extends Thread {
+        public void run() {
+            for (int i = 0; i < 10_000; i++) {
+                population++;
             }
-            return localPopulation;
         }
     }
 
-    public static class Taskb implements Callable<Integer> {
-
-        @Override
-        public Integer call() throws Exception {
-            int localPopulation = 0;
-            for (int i = 0; i < 100_000; i++) {
-                localPopulation++;
+    public static class ThreadB extends Thread {
+        public void run() {
+            for (int i = 0; i < 10_000; i++) {
+                population++;
             }
-            return localPopulation;
         }
     }
 
@@ -39,12 +23,10 @@ public class APlusPlus {
 
 
     public static void main(String[] args) throws InterruptedException {
-        Taska threadA = new Taska();
-        Taskb threadB = new Taskb();
+        ThreadA threadA = new ThreadA();
+        ThreadB threadB = new ThreadB();
 
         long t0 = System.currentTimeMillis();
-
-        CompletableFuture.supplyAsync(new Taska())
 
         threadA.start();
         threadB.start();
