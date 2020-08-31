7ca89c937da0d5842e8613017f447386a2336176
Victor Rentea <victorrentea@gmail.com>
2020-08-31 10:11:45 +0300

    infamous SB problem

--- performance/src/main/java/victor/training/performance/interview/TheInfamousStringBuilderProblem.java
+++ performance/src/main/java/victor/training/performance/interview/TheInfamousStringBuilderProblem.java
@@ -1,43 +1,22 @@
 package victor.training.performance.interview;
 
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TheInfamousStringBuilderProblem {
     public static void main(String[] args) throws IOException {
-        // TODO explore:
-        String a = "a"; // goes to String Pool
-        String b = "b";
-        if (a == "a") {
-            System.out.println("WTH?!");
-        }
-        String c = a + b + a; //2
-        c += a; // one more
+        List<String> elements = IntStream.range(1, 10).mapToObj(String::valueOf).collect(toList());
 
+        // TODO join elements with "," and print them on the console
 
-        List<String> list = asList("a", "b", "c");
-
-        String s = infamous(list);
-
-        try (Writer w = new FileWriter("out.txt")) {
-            w.write(s);
-        }
-        // TODO more elements ?
-
-        // TODO Where could you offload data? File, CLOB, httpServletResponse.getWriter()
-
-        System.out.println("Done");
-    }
-
-    private static String infamous(List<String> list) {
-        String s = "Header";
-        for (String string : list) {
-            s += string + "\n";
-        }
-        return s;
+        // TODO 1M
+        // TODO JFR +=
     }
 }
