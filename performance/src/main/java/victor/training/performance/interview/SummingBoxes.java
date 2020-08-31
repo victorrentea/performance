f5930b9851502aa55a484d3936d1c2bd879b5b39
Victor Rentea <victorrentea@gmail.com>
2020-08-31 10:37:39 +0300

    cel mai rapid+

--- performance/src/main/java/victor/training/performance/interview/SummingBoxes.java
+++ performance/src/main/java/victor/training/performance/interview/SummingBoxes.java
@@ -7,15 +7,19 @@ import java.util.stream.LongStream;
 
 public class SummingBoxes {
 
+
+
     public static void main(String[] args) {
-        List<Long> list = LongStream.range(1, 10_000_000)
-				.boxed().collect(toList());
+        long[] list = LongStream.range(1, 10_000_000)
+				.toArray();
 
         long t0 = System.currentTimeMillis();
 
-        Long sum = 0L;
-        for (Long i : list) {
+        long sum = 0L;
+        for (long i : list) {
             sum += i;
+            // daca sum e Long
+//            sum = new Long(sum.longValue() + i);
         }
 
         long t1 = System.currentTimeMillis();
--- performance/src/main/java/victor/training/performance/interview/TheInfamousStringBuilderProblem.java
+++ performance/src/main/java/victor/training/performance/interview/TheInfamousStringBuilderProblem.java
@@ -12,18 +12,30 @@ public class TheInfamousStringBuilderProblem {
       List<String> elements = IntStream.range(1,10_000).boxed().map(String::valueOf).collect(Collectors.toList());
 
       long t0 = System.currentTimeMillis();
-      try (Writer writer = new BufferedWriter(new FileWriter(new File("out.txt")))) {
-         concatenateAllAndWrite(elements.stream(), writer);
-      }
-//      StringBuilder s= new StringBuilder();
-//      for (String element : elements) {
-//         s.append(element).append(",");
-//      }
 //      try (Writer writer = new BufferedWriter(new FileWriter(new File("out.txt")))) {
-//         writer.write(s.toString());
+//         concatenateAllAndWrite(elements.stream(), writer);
 //      }
+      String s = m(elements);
+      try (Writer writer = new BufferedWriter(new FileWriter(new File("out.txt")))) {
+         writer.write(s);
+      }
       long t1 = System.currentTimeMillis();
       System.out.println("Took " + (t1-t0));
+   }
+
+   private static String m(List<String> elements) {
+      StringBuilder s = new StringBuilder();
+      for (String element : elements) {
+         s.append(element).append(",");
+//         s = s + element + ",";
+//         s = new StringBuilder(s).append(element).append(",").toString();
+      }
+
+      // Morala: daca vreodata concatenezi multe stringuri cu + intre ele (fara niic un for)
+      // lasa-le cu +, ca in bytecode iese StringBuilder oricum din Java 8+
+//      String csvLine = new StringBuilder().append(order.getId()).append(";").toString();
+//      clsLine += order.getFirstName() + ";";
+      return s.toString();
    }
 
    private static void concatenateAllAndWrite(Stream<String> elementStream, Writer writer) {
