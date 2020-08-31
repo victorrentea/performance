d27cf3582e90e35be3bd00bbcacb7e883cf8eb1c
Victor Rentea <victorrentea@gmail.com>
2020-08-31 14:24:54 +0300

    Thread pools in sring

--- performance/src/main/java/victor/training/performance/pools/BarmanApp.java
+++ performance/src/main/java/victor/training/performance/pools/BarmanApp.java
@@ -3,13 +3,13 @@ package victor.training.performance.pools;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.SimpleThreadScope;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
@@ -17,9 +17,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
@@ -30,7 +32,7 @@ import static victor.training.performance.ConcurrencyUtil.sleepq;
 public class BarmanApp {
     public static void main(String[] args) {
         SpringApplication.run(BarmanApp.class, args)
-                .close(); // Note: .close to stop executors after CLRunner finishes
+              ;//  .close(); // Note: .close to stop executors after CLRunner finishes
     }
 
     // TO discuss propagation of thread local data
@@ -45,10 +47,10 @@ public class BarmanApp {
     private PropagateRequestContext propagateRequestContext;
 
     @Bean
-    public ThreadPoolTaskExecutor executor() {
+    public ThreadPoolTaskExecutor executor(@Value("${barman.count}") int size) {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(1);
-        executor.setMaxPoolSize(1);
+        executor.setCorePoolSize(size);
+        executor.setMaxPoolSize(size);
         executor.setQueueCapacity(500);
         executor.setThreadNamePrefix("barman-");
         executor.initialize();
@@ -64,25 +66,47 @@ public class BarmanApp {
 class DrinkerService {
     @Autowired
     private Barman barman;
+    @Autowired
+    private ThreadPoolTaskExecutor pool;
 
     public List<Object> orderDrinks() throws ExecutionException, InterruptedException {
         log.debug("Submitting my order");
 
-        ExecutorService pool = Executors.newFixedThreadPool(2);
-        Future<Beer> futureBeer = pool.submit(() -> barman.pourBeer());
+//        ExecutorService pool = Executors.newFixedThreadPool(2);
+//        ExecutorService pool = Executors.newCachedThreadPool();
+//        ThreadPoolSECONDS,Executor pool = new ThreadPoolExecutor(
+////            2, 2,
+////            1, TimeUnit.
+//            new ArrayBlockingQueue<>(2),
+//            new CallerRunsPolicy());
+////            new DiscardOldestPolicy());
+
+        List<Future<Beer>> futureBeers = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+//            sleepq(500);
+            Future<Beer> futureBeer = pool.submit(() -> barman.pourBeer());
+            futureBeers.add(futureBeer);
+        }
 
         Future<Vodka> futureVodka = pool.submit(() -> barman.pourVodka());
 
+        log.debug("aici a plecat cu comanda chelenerul");
 
         // inteligent
         //
-        Beer beer = futureBeer.get();
+        List<Beer> beers = futureBeers.stream().map(futureBeer -> {
+            try {
+                return futureBeer.get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new IllegalArgumentException();
+            }
+        }).collect(Collectors.toList());
         Vodka vodka = futureVodka.get();
 
 //        Beer beer = barman.pourBeer();
 //        Vodka vodka = barman.pourVodka();
-        log.debug("Got my order! Thank you lad! " + asList(beer, vodka));
-        return asList(beer, vodka);
+        log.debug("Got my order! Thank you lad! " + asList(beers, vodka));
+        return asList(beers, vodka);
     }
 
 }
