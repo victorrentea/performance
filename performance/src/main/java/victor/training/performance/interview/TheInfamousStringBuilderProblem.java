b8f64ba58b3f5b2885c2c7d69b63b0d0e8713d45
Victor Rentea <victorrentea@gmail.com>
2020-08-31 10:09:29 +0300

    added alternative

--- performance/src/main/java/victor/training/performance/interview/TheInfamousStringBuilderProblem.java
+++ performance/src/main/java/victor/training/performance/interview/TheInfamousStringBuilderProblem.java
@@ -9,12 +9,19 @@ import java.util.stream.Stream;
 public class TheInfamousStringBuilderProblem {
    public static void main(String[] args) throws IOException {
 
-      List<String> elements = IntStream.range(1,1_000_000).boxed().map(String::valueOf).collect(Collectors.toList());
+      List<String> elements = IntStream.range(1,10_000).boxed().map(String::valueOf).collect(Collectors.toList());
 
       long t0 = System.currentTimeMillis();
       try (Writer writer = new BufferedWriter(new FileWriter(new File("out.txt")))) {
          concatenateAllAndWrite(elements.stream(), writer);
       }
+//      StringBuilder s= new StringBuilder();
+//      for (String element : elements) {
+//         s.append(element).append(",");
+//      }
+//      try (Writer writer = new BufferedWriter(new FileWriter(new File("out.txt")))) {
+//         writer.write(s.toString());
+//      }
       long t1 = System.currentTimeMillis();
       System.out.println("Took " + (t1-t0));
    }
