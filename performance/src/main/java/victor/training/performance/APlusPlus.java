36012c3ff49d063f5a1944b6595b2ffe5c90b2a7
Victor Rentea <victorrentea@gmail.com>
2020-08-31 15:08:09 +0300

    CompletableFuture

--- performance/src/main/java/victor/training/performance/APlusPlus.java
+++ performance/src/main/java/victor/training/performance/APlusPlus.java
@@ -1,52 +1,59 @@
 package victor.training.performance;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import lombok.extern.slf4j.Slf4j;
+import victor.training.performance.java8.CompletableFutures;
 
+import java.util.concurrent.*;
+import java.util.function.Supplier;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
+@Slf4j
 public class APlusPlus {
-    private static int population;
-    private static final Object MONITOR = new Object();
+   private static int population;
+   private static final Object MONITOR = new Object();
 
-    public static class ThreadA extends Thread {
+   public static class Taska implements Supplier<Integer> {
+      @Override
+      public Integer get() {
+         int localPopulation = 0;
+         for (int i = 0; i < 100_000; i++) {
+            localPopulation++;
+         }
+         return localPopulation;
+      }
+   }
 
-        public  void run() {
-            int localPopulation = 0;
-            for (int i = 0; i < 100_000; i++) {
-                localPopulation++;
-        }
-            synchronized (MONITOR) {
-                population+=localPopulation;
-            }
-        }
-    }
+   public static class Taskb implements Supplier<Integer> {
+      @Override
+      public Integer get() {
+         int localPopulation = 0;
+         for (int i = 0; i < 100_000; i++) {
+            localPopulation++;
+         }
+         return localPopulation;
+      }
+   }
 
-    public static class ThreadB extends Thread {
-        public void run() {
-            int localPopulation = 0;
-            for (int i = 0; i < 100_000; i++) {
-                localPopulation++;
-            }
-            synchronized (MONITOR) {
-                population += localPopulation;
-            }
-        }
-    }
-
-    // TODO (bonus): ConcurrencyUtil.useCPU(1)
+   // TODO (bonus): ConcurrencyUtil.useCPU(1)
 
 
-    public static void main(String[] args) throws InterruptedException {
-        ThreadA threadA = new ThreadA();
-        ThreadB threadB = new ThreadB();
+   public static void main(String[] args) throws InterruptedException, ExecutionException {
+      Taska threadA = new Taska();
+      Taskb threadB = new Taskb();
 
-        long t0 = System.currentTimeMillis();
+      long t0 = System.currentTimeMillis();
 
-        threadA.start();
-        threadB.start();
-        threadA.join();
-        threadB.join();
+      Integer r = supplyAsync(new Taska())
+              .thenCombineAsync(supplyAsync(new Taskb()), (r1, r2) -> {
+                 log.debug("Plus");
+                 return r1 + r2;
+              })
+          .get();
 
-        long t1 = System.currentTimeMillis();
-        System.out.println("Total = " + population);
-        System.out.println("Took = " + (t1 - t0));
-    }
+
+      long t1 = System.currentTimeMillis();
+      System.out.println("Total = " + r);
+      System.out.println("Took = " + (t1 - t0));
+   }
 }
