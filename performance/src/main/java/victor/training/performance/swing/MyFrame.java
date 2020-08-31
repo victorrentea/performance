32a604876e0dbb152dc2b9af873ef1104c3105d3
Victor Rentea <victorrentea@gmail.com>
2020-08-31 16:06:49 +0300

    add swing

---
+++ performance/src/main/java/victor/training/performance/swing/MyFrame.java
@@ -0,0 +1,31 @@
+package victor.training.performance.swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+
+	public JButton button1 = new JButton("Press Me");
+	public JPanel panel1 = new JPanel();
+	public JPanel panel2 = new JPanel();
+	public JTextArea textArea = new JTextArea(3, 8);
+	public JTextField textField = new JTextField(8);
+
+	public MyFrame() {
+
+		getContentPane().setLayout(new GridLayout(1, 2, 20, 20));
+		getContentPane().add(button1);
+		getContentPane().add(panel1);
+		panel1.add(panel2);
+		panel2.add(textArea);
+		panel1.add(textField);
+
+		setTitle("OO example: Observer, Composite, TemplateMethod");
+		setSize(300, 200);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		setVisible(true);
+	}
+
+}
