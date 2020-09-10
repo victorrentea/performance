660020c08d328f367a57aef19b64a9d0a6d62d1f
Victor Rentea <victorrentea@gmail.com>
2020-09-10 15:31:01 +0300

    DO NOT BLOCK OTHER. Don;t sleep while holding locks that others want to take

--- performance/src/main/java/victor/training/performance/APlusPlus.java
+++ performance/src/main/java/victor/training/performance/APlusPlus.java
@@ -1,42 +1,50 @@
 package victor.training.performance;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Collections;
 
 public class APlusPlus {
-    private static AtomicInteger population = new AtomicInteger(0);
+   private static Integer population = 0;
+   private static final Object MONITOR = new Object();
 
-    public static class ThreadA extends Thread {
-        public void run() {
-            for (int i = 0; i < 10_000; i++) {
-                population.incrementAndGet();
+   public static class ThreadA extends Thread {
+      public void run() {
+         for (int i = 0; i < 5_000; i++) {
+            ConcurrencyUtil.sleepq(1); // DB call , HTTP call.
+            synchronized (MONITOR) {
+               population++;
             }
-        }
-    }
+         }
+      }
+   }
 
-    public static class ThreadB extends Thread {
-        public void run() {
-            for (int i = 0; i < 10_000; i++) {
-                population.incrementAndGet();
+   public static class ThreadB extends Thread {
+      public void run() {
+         for (int i = 0; i < 5_000; i++) {
+            ConcurrencyUtil.sleepq(1);
+            synchronized (MONITOR) {
+               population++;
+
             }
-        }
-    }
+         }
+      }
+   }
 
-    // TODO (bonus): ConcurrencyUtil.useCPU(1)
-    // TODO (extra bonus): Analyze with JFR
+   // TODO (bonus): ConcurrencyUtil.useCPU(1)
+   // TODO (extra bonus): Analyze with JFR
 
-    public static void main(String[] args) throws InterruptedException {
-        ThreadA threadA = new ThreadA();
-        ThreadB threadB = new ThreadB();
+   public static void main(String[] args) throws InterruptedException {
+      ThreadA threadA = new ThreadA();
+      ThreadB threadB = new ThreadB();
 
-        long t0 = System.currentTimeMillis();
+      long t0 = System.currentTimeMillis();
 
-        threadA.start();
-        threadB.start();
-        threadA.join();
-        threadB.join();
+      threadA.start();
+      threadB.start();
+      threadA.join();
+      threadB.join();
 
-        long t1 = System.currentTimeMillis();
-        System.out.println("Total = " + population);
-        System.out.println("Took = " + (t1 - t0));
-    }
+      long t1 = System.currentTimeMillis();
+      System.out.println("Total = " + population);
+      System.out.println("Took = " + (t1 - t0));
+   }
 }
