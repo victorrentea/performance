2451f01411edf4a7fd6fac2d0e0c15c65d200c3f
Victor Rentea <victorrentea@gmail.com>
2020-09-10 18:18:32 +0300

    clean code

--- performance/src/main/java/victor/training/performance/assignment/search/CarSearchCriteria.java
+++
@@ -1,8 +0,0 @@
-package victor.training.performance.assignment.search;
-
-public class CarSearchCriteria {
-    public String make;
-    public Integer year;
-    public Integer pageSize;
-    public Integer pageNumber;
-}
