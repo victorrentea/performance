a26f9ee0f333f9d3cd08a3fcb427c9e33b9b5fd5
Victor Rentea <victorrentea@gmail.com>
2020-11-16 03:30:55 +0200

    add exception benchmark

---
+++ performance-jmh/src/main/java/victor/perf/ExceptionBenchmark.java
@@ -0,0 +1,63 @@
+package victor.perf;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.concurrent.TimeUnit;
+
+@Fork(1)
+@Warmup(iterations = 2)
+@Measurement(iterations = 10)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+public class ExceptionBenchmark {
+    private static final int LIMIT = 10_000;
+
+    @Benchmark
+    public void doNotThrowException(Blackhole blackhole) {
+        for (int i = 0; i < LIMIT; i++) {
+            blackhole.consume(new Object());
+        }
+    }
+
+    @Benchmark
+    public void throwAndCatchException(Blackhole blackhole) {
+        for (int i = 0; i < LIMIT; i++) {
+            try {
+                throw new Exception();
+            } catch (Exception e) {
+                blackhole.consume(e);
+            }
+        }
+    }
+
+    @Benchmark
+    public void createExceptionWithoutThrowingIt(Blackhole blackhole) {
+        for (int i = 0; i < LIMIT; i++) {
+            blackhole.consume(new Exception());
+        }
+    }
+
+    @Benchmark
+    @Fork(value = 1, jvmArgs = "-XX:-StackTraceInThrowable")
+    public void throwExceptionWithoutAddingStackTrace(Blackhole blackhole) {
+        for (int i = 0; i < LIMIT; i++) {
+            try {
+                throw new Exception();
+            } catch (Exception e) {
+                blackhole.consume(e);
+            }
+        }
+    }
+
+    @Benchmark
+    public void throwExceptionAndUnwindStackTrace(Blackhole blackhole) {
+        for (int i = 0; i < LIMIT; i++) {
+            try {
+                throw new Exception();
+            } catch (Exception e) {
+                blackhole.consume(e.getStackTrace());
+            }
+        }
+    }
+}
