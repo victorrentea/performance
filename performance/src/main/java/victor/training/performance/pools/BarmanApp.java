dd5b7076e0175c85155bfe18690c8f3ce74caa25
Victor Rentea <victorrentea@gmail.com>
2020-12-07 14:21:15 +0200

    no more DB relation from Barman

--- performance/src/main/java/victor/training/performance/pools/BarmanApp.java
+++ performance/src/main/java/victor/training/performance/pools/BarmanApp.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.SimpleThreadScope;
 import org.springframework.scheduling.annotation.Async;
@@ -25,7 +28,11 @@ import static java.util.concurrent.CompletableFuture.supplyAsync;
 import static victor.training.performance.ConcurrencyUtil.sleepq;
 
 @EnableAsync
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    DataSourceTransactionManagerAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
 public class BarmanApp {
     public static void main(String[] args) {
         SpringApplication.run(BarmanApp.class, args)
