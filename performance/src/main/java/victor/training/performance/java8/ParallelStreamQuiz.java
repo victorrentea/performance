a38d7c2bdf5aaba81a24ff9d35730337dd8d0317
Victor Rentea <victorrentea@gmail.com>
2020-12-07 22:31:54 +0200

    ParallelStreamQuiz

---
+++ performance/src/main/java/victor/training/performance/java8/ParallelStreamQuiz.java
@@ -0,0 +1,29 @@
+package victor.training.performance.java8;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Arrays.asList;
+
+@Slf4j
+public class ParallelStreamQuiz {
+   public static void main(String[] args) {
+      List<Integer> list = asList(1, 2, 3, 4);
+      Optional<Integer> fi = list.parallelStream()
+          .filter(i -> {
+             log.debug("Filter " + i);
+             return true;
+          })
+          .sequential()
+          .map(i -> {
+             log.debug("Map " + i);
+             return i;
+          })
+          .parallel()
+          .findFirst();
+      System.out.println(fi);
+
+   }
+}
