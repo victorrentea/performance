ef3ba2aba62fc4b80df080a65a0c0feddcc66b03
Victor Rentea <victorrentea@gmail.com>
2020-12-15 17:03:34 +0200

    Executarea parallelStream pe thread pool propriu custom

--- performance/src/main/java/victor/training/performance/java8/ParallelStreams.java
+++ performance/src/main/java/victor/training/performance/java8/ParallelStreams.java
@@ -3,6 +3,8 @@ package victor.training.performance.java8;
 import victor.training.performance.ConcurrencyUtil;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -11,29 +13,33 @@ import static java.util.stream.Collectors.toList;
 import static victor.training.performance.ConcurrencyUtil.*;
 
 public class ParallelStreams {
-   public static void main(String[] args) {
+   public static void main(String[] args) throws ExecutionException, InterruptedException {
       List<Integer> numbers = IntStream.range(1, 50).boxed().collect(toList());
 
 
-      numbers.parallelStream()
+      Stream<Integer> stream = numbers.parallelStream()
           .filter(n -> {
-             log("Filter " +n);
+             log("Filter " + n);
+             // DB, IO/ NET/ API/ File
              return n % 2 == 1;
           })
-          .parallel()
+//          .sorted()
           .map(n -> {
-             log("Map " +n);
+             log("Map " + n);
              return n * n;
           })
-          .flatMap(n -> m(n))
-          .sequential()
-          .forEach(x -> {
-             System.out.println("Out " +x);
-          });
+          .flatMap(n -> m(n));
+
+      ForkJoinPool pool = new ForkJoinPool(4);
+
+      List<Integer> nums = pool.submit(() -> stream.collect(toList())).get();
+
+
+      System.out.println(nums);
 
    }
 
    private static Stream<Integer> m(Integer n) {
-      return IntStream.range(1,2).boxed().parallel().peek(e -> log("Peek " + e));
+      return IntStream.range(n,n+1).boxed().parallel().peek(e -> log("Peek " + e));
    }
 }
