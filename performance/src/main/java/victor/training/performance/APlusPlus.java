c57afed0641078992817a23735eb0e20de21a970
Victor Rentea <victorrentea@gmail.com>
2021-03-23 11:19:47 +0200

    map-reduce blocant

--- performance/src/main/java/victor/training/performance/APlusPlus.java
+++ performance/src/main/java/victor/training/performance/APlusPlus.java
@@ -1,40 +1,57 @@
 package victor.training.performance;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class APlusPlus {
-    private static Integer population = 0;
+   private static AtomicInteger population = new AtomicInteger(0);
 
-    public static class ThreadA extends Thread {
-        public void run() {
-            for (int i = 0; i < 10_000; i++) {
-                population++;
+   static final Map<Object, Object> hashMap = new ConcurrentHashMap<>();
+
+   public static class ThreadA extends Thread {
+      public void run() {
+         int populationLocal = 0;
+         for (int i = 0; i < 1000_000; i++) {
+            populationLocal++;
+            hashMap.put(i, "x");
+//            hashMap.get(i);
+         }
+         population.addAndGet(populationLocal);
+      }
+   }
+
+   public static class ThreadB extends Thread {
+      public void run() {
+         int populationLocal = 0;
+         for (int i = 0; i < 1000_000; i++) {
+            populationLocal++;
+            if (hashMap.containsKey(i)) {
+               System.out.println("Hit!");
             }
-        }
-    }
+         }
+         population.addAndGet(populationLocal);
+      }
+   }
 
-    public static class ThreadB extends Thread {
-        public void run() {
-            for (int i = 0; i < 10_000; i++) {
-                population++;
-            }
-        }
-    }
+   // TODO (bonus): ConcurrencyUtil.useCPU(1)
+   // TODO (extra bonus): Analyze with JFR
 
-    // TODO (bonus): ConcurrencyUtil.useCPU(1)
-    // TODO (extra bonus): Analyze with JFR
+   public static void main(String[] args) throws InterruptedException {
+      ThreadA threadA = new ThreadA();
+      ThreadB threadB = new ThreadB();
 
-    public static void main(String[] args) throws InterruptedException {
-        ThreadA threadA = new ThreadA();
-        ThreadB threadB = new ThreadB();
+      long t0 = System.currentTimeMillis();
 
-        long t0 = System.currentTimeMillis();
+      threadA.start();
+      threadB.start();
+      threadA.join();
+      threadB.join();
 
-        threadA.start();
-        threadB.start();
-        threadA.join();
-        threadB.join();
-
-        long t1 = System.currentTimeMillis();
-        System.out.println("Total = " + population);
-        System.out.println("Took = " + (t1 - t0));
-    }
+      long t1 = System.currentTimeMillis();
+      System.out.println("Total = " + population);
+      System.out.println("Took = " + (t1 - t0));
+   }
 }
