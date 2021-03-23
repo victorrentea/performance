b7beb7164b1e955825bc9ef99762cd2617697537
Victor Rentea <victorrentea@gmail.com>
2021-03-23 13:03:27 +0200

    Am intors un CompletableFuture lui spring

--- performance/src/main/java/victor/training/performance/leaks/SheepController.java
+++ performance/src/main/java/victor/training/performance/leaks/SheepController.java
@@ -2,10 +2,10 @@ package victor.training.performance.leaks;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,77 +15,95 @@ import victor.training.performance.ConcurrencyUtil;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.sql.DataSource;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @RestController
 @RequestMapping("load")
 @RequiredArgsConstructor
 public class SheepController {
-    private final SheepService service;
+   private final SheepService service;
 
-    @GetMapping("create")
-    public Long create(@RequestParam(defaultValue = "Bisisica") String name) {
-        log.debug("create " + name);
-        return service.create(name);
-    }
-    // TODO Starve Connections
-    // TODO Starve Threads
+   @GetMapping("create")
+   public CompletableFuture<Long> create(@RequestParam(defaultValue = "Bisisica") String name) {
+      log.debug("create " + name);
+      return service.create(name);
+   }
+   // TODO Starve Connections
+   // TODO Starve Threads
 
-    @GetMapping("search")
-    public List<Sheep> search(@RequestParam(defaultValue = "Bisisica") String name) {
-        log.debug("search for " + name);
-        return service.search(name);
-    }
+   @GetMapping("search")
+   public List<Sheep> search(@RequestParam(defaultValue = "Bisisica") String name) {
+      log.debug("search for " + name);
+      return service.search(name);
+   }
 }
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional
 class SheepService {
-    private final SheepRepo repo;
-    private final ShepardService shepard;
+   private final SheepRepo repo;
+   private final ShepardService shepard;
+   private final DataSource dataSource;
 
-    public Long create(String name) {
-        String sn = shepard.registerSheep(name);
-        log.debug("Persist");
-        Sheep sheep = repo.save(new Sheep(name, sn));
-        return sheep.getId();
-    }
-    public List<Sheep> search(String name) {
-        return repo.getByNameLike(name);
-    }
+   private final ExecutorService pool = Executors.newFixedThreadPool(200);
+
+   @SneakyThrows
+   public CompletableFuture<Long> create(String name) {
+//      Future<String> snFuture = pool.submit(() -> shepard.registerSheep(name));
+      CompletableFuture<String> futureSN = CompletableFuture
+          .supplyAsync(() -> shepard.registerSheep(name), pool);
+      log.debug("Persist");
+
+
+      CompletableFuture<Sheep> futureSheep = futureSN.thenApply(sn -> new Sheep(name, sn));
+      CompletableFuture<Long> futureId = futureSheep.thenApply(sheep -> repo.save(sheep).getId());
+
+      return futureId;
+   }
+
+   public List<Sheep> search(String name) {
+      return repo.getByNameLike(name);
+   }
 }
+
 @Slf4j
 @Service
 class ShepardService {
-    public String registerSheep(String name) {
-        log.debug("Calling external WS");
-        ConcurrencyUtil.sleepq(500);
-        return UUID.randomUUID().toString();
-    }
+   public String registerSheep(String name) {
+      log.debug("Calling external WS");
+      ConcurrencyUtil.sleepq(500);
+      return UUID.randomUUID().toString();
+   }
 }
 
 interface SheepRepo extends JpaRepository<Sheep, Long> {
-    List<Sheep> getByNameLike(String name);
+   List<Sheep> getByNameLike(String name);
 }
 
 
 @Entity
-@Data // Don't
+@Data
+    // Don't
 class Sheep {
-    @GeneratedValue
-    @Id
-    private Long id;
+   @GeneratedValue
+   @Id
+   private Long id;
 
-    private String name;
-    private String sn;
+   private String name;
+   private String sn;
 
-    public Sheep() {}
-    public Sheep(String name, String sn) {
-        this.name = name;
-        this.sn = sn;
-    }
+   public Sheep() {
+   }
+
+   public Sheep(String name, String sn) {
+      this.name = name;
+      this.sn = sn;
+   }
 }
--- performance/src/main/java/victor/training/performance/pools/BarService.java
+++ performance/src/main/java/victor/training/performance/pools/BarService.java
@@ -1,6 +1,7 @@
 package victor.training.performance.pools;
 
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,6 +11,8 @@ import victor.training.performance.pools.drinks.Beer;
 import victor.training.performance.pools.drinks.Vodka;
 
 import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
 
 import static java.util.Arrays.asList;
 import static victor.training.performance.ConcurrencyUtil.sleepq;
@@ -29,10 +32,28 @@ public class BarService implements CommandLineRunner {
       log.debug("" + orderDrinks());
    }
 
+   @SneakyThrows
    public List<Object> orderDrinks() {
       log.debug("Submitting my order");
-      Beer beer = barman.pourBeer();
-      Vodka vodka = barman.pourVodka();
+
+//      ExecutorService pool = Executors.newFixedThreadPool(2);
+//      ExecutorService pool = Executors.newCachedThreadPool();
+
+      ExecutorService pool = new ThreadPoolExecutor(
+          2, 4,
+          5, TimeUnit.MILLISECONDS,
+          new ArrayBlockingQueue<>(2),
+          new CallerRunsPolicy()
+      );
+
+      Future<Beer> futureBeer = pool.submit(() -> barman.pourBeer());
+      Future<Vodka> futureVodka = pool.submit(() -> barman.pourVodka());
+
+      log.debug("Fata a plecat pe cal sa-mi aduca comanda");
+
+      Beer beer = futureBeer.get();
+      Vodka vodka = futureVodka.get();
+
       log.debug("Got my order: " + asList(beer, vodka));
       return null;
    }
@@ -48,7 +69,7 @@ class Barman {
    public Beer pourBeer() {
       String currentUsername = null; // TODO ThreadLocals... , requestContext.getCurrentUser()
       log.debug("Pouring Beer to " + currentUsername + "...");
-      sleepq(1000);
+      sleepq(2000); // REST / DB call
       return new Beer();
    }
 
