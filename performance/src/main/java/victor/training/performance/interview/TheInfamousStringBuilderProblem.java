5c7d8fde82175773c42f98479b2b4cd674776b21
Victor Rentea <victorrentea@gmail.com>
2021-04-26 09:28:50 +0300

    String Builder e mai bun ca += pentru >5.000 , dar streamlining (sa dai afara din memorie datele pe un Writer/OutputStream .txt .csv .xml (STAX), PDF jasper, Am scris in zipuri, xlsx cu POI, CLOB, ServletOutputStream). Am citit din JSON, XML cu Stax este mult mai performanta ca lasa GC in pace.

--- performance/src/main/java/victor/training/performance/interview/TheInfamousStringBuilderProblem.java
+++ performance/src/main/java/victor/training/performance/interview/TheInfamousStringBuilderProblem.java
@@ -1,22 +1,43 @@
 package victor.training.performance.interview;
 
-import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TheInfamousStringBuilderProblem {
     public static void main(String[] args) throws IOException {
-        List<String> elements = IntStream.range(1, 10).mapToObj(String::valueOf).collect(toList());
-
+        List<String> elements = IntStream.range(1, 300_000).mapToObj(String::valueOf).collect(toList());
+//300000 * 5 = 1,5M
         // TODO join elements with "," and print them on the console
+
+        long t0 = System.currentTimeMillis();
+
+
+
+
+        // citesti in chunkuri (batch) cate 500-1000 elemente
+        generate(new OutputStreamWriter(System.out), elements);
+
+        long t1 = System.currentTimeMillis();
+
+        System.out.println("\n Took: " + (t1-t0));
+
+//        System.out.println(s);
 
         // TODO 300K
         // TODO JFR +=
+    }
+
+    private static void generate(Writer writer, List<String> elements) throws IOException {
+//        StringBuilder s = new StringBuilder();
+        for (String element : elements) {
+            writer.write(element);
+//            s.append(element);
+        }
+//        return s.toString();
     }
 }
