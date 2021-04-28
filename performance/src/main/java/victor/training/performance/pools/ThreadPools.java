861c19908e6f0cfe089ed915a71a67f000607a3f
Victor Rentea <victorrentea@gmail.com>
2021-04-28 12:44:19 +0300

    Caller Runs policy

--- performance/src/main/java/victor/training/performance/pools/ThreadPools.java
+++ performance/src/main/java/victor/training/performance/pools/ThreadPools.java
@@ -1,7 +1,8 @@
 package victor.training.performance.pools;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
+import java.util.Scanner;
+import java.util.concurrent.*;
+import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static victor.training.performance.ConcurrencyUtil.log;
@@ -11,20 +12,26 @@ public class ThreadPools {
 
    public static void main(String[] args) throws InterruptedException {
       // TODO Executor that keeps a fixed number (3) of threads until it is shut down
-      ExecutorService executor = null; //Executors. ?
+//      ExecutorService executor = Executors.newFixedThreadPool(3);
 
       // TODO Executor that grows the thread pool as necessary, and kills inactive ones after 1 min
-      // TODO ExecutorService executor = Executors. ?
+//      ExecutorService executor = Executors.newCachedThreadPool();
 
       // TODO Executor that have at least 3 thread but can grow up to 10 threads. Inactive threads die in 1 second.
       // TODO Vary the fixed-sized queue to see it grow the pool and then Rejecting tasks
+      ThreadPoolExecutor executor = new ThreadPoolExecutor(
+          2, 4,
+          1, TimeUnit.SECONDS,
+          new ArrayBlockingQueue<>(2),
+          new CallerRunsPolicy());
 
       for (int i = 0; i < 40; i++) {
          MyTask task = new MyTask();
-         log("Submitted new task #" + task.id);
+         log("Submitting new task #" + task.id);
          executor.submit(task);
          sleepSomeTime(100, 200); // simulate random request rate
       }
+      new Scanner(System.in).nextLine();
       // TODO shutdown the executor !
    }
 }
