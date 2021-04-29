95e4b9beb1129d344fdb5306ab1100f1bc95a2f4
Victor Rentea <victorrentea@gmail.com>
2021-04-29 09:42:58 +0300

    Race bugs + threading control

--- performance/src/main/java/victor/training/performance/APlusPlus.java
+++ performance/src/main/java/victor/training/performance/APlusPlus.java
@@ -1,40 +1,58 @@
 package victor.training.performance;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class APlusPlus {
-    private static Integer population = 0;
+   private static final Object monitor = new Object();
+   private static AtomicInteger populationGlobal = new AtomicInteger();
+    private static List<Integer> listGlobal = Collections.synchronizedList(new ArrayList<>());
 
     public static class ThreadA extends Thread {
         public void run() {
-            for (int i = 0; i < 10_000; i++) {
+            int population = 0;
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < 100_000; i++) {
                 population++;
+                list.add(i);
             }
+            populationGlobal.addAndGet(population);
+            listGlobal.addAll(list);
         }
     }
 
     public static class ThreadB extends Thread {
         public void run() {
-            for (int i = 0; i < 10_000; i++) {
-                population++;
+            int population = 0;
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < 100_000; i++) {
+                population ++;
+                list.add(i);
             }
+            populationGlobal.addAndGet(population);
+            listGlobal.addAll(list);
         }
     }
+   public static void main(String[] args) throws InterruptedException {
+      ThreadA threadA = new ThreadA();
+      ThreadB threadB = new ThreadB();
 
-    // TODO (bonus): ConcurrencyUtil.useCPU(1)
-    // TODO (extra bonus): Analyze with JFR
+      long t0 = System.currentTimeMillis();
+//        synchronized (APlusPlus.class) {
+//
+//        }
+      threadA.start();
+      threadB.start();
+      threadA.join();
+      threadB.join();
 
-    public static void main(String[] args) throws InterruptedException {
-        ThreadA threadA = new ThreadA();
-        ThreadB threadB = new ThreadB();
+      long t1 = System.currentTimeMillis();
+      System.out.println("Total = " + populationGlobal);
+       System.out.println("Total = " + listGlobal.size());
+      System.out.println("Took = " + (t1 - t0));
+   }
 
-        long t0 = System.currentTimeMillis();
 
-        threadA.start();
-        threadB.start();
-        threadA.join();
-        threadB.join();
-
-        long t1 = System.currentTimeMillis();
-        System.out.println("Total = " + population);
-        System.out.println("Took = " + (t1 - t0));
-    }
 }
