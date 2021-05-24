b6a937cd62ddf09a9795e58b3a158390a05bcf85
Victor Rentea <victorrentea@gmail.com>
2021-05-24 10:47:59 +0300

    Streamlining: not keeping a lot of data in memory at once: iterate over it or process it in pages.
    "a" + 1 +x + "\n" --> StringBuilder
    for() {
    s+=    === pain for memory and CPU =>> StringBuilder
    }

--- performance/src/main/java/victor/training/performance/interview/TheInfamousStringBuilderProblem.java
+++ performance/src/main/java/victor/training/performance/interview/TheInfamousStringBuilderProblem.java
@@ -1,22 +1,55 @@
 package victor.training.performance.interview;
 
-import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.toList;
-
+import javax.servlet.http.HttpServletResponse;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.sql.Clob;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class TheInfamousStringBuilderProblem {
-    public static void main(String[] args) throws IOException {
-        List<String> elements = IntStream.range(1, 10).mapToObj(String::valueOf).collect(toList());
+   public static void main(String[] args) throws IOException {
+      Stream<String> elements = IntStream.range(1, 30_000).mapToObj(String::valueOf);
 
-        // TODO join elements with "," and print them on the console
+      // TODO join elements with "," and print them on the console
 
-        // TODO 300K
-        // TODO JFR +=
-    }
+      long t0 = System.currentTimeMillis();
+
+
+      try (Writer writer = new FileWriter("a.txt")) {
+         concatenate(elements, writer);
+      }
+
+      long t1 = System.currentTimeMillis();
+
+      System.out.println(t1 - t0);
+
+      // TODO 300K
+      // TODO JFR +=
+   }
+
+   private static void concatenate(Stream<String> elements, Writer writer) throws IOException {
+      elements.forEach(element ->
+          {
+             try {
+                writer.write("a" + element + "\n");
+             } catch (IOException e) {
+                e.printStackTrace();
+             }
+          }
+      );
+//       String result= "";
+//
+//       for (String element : elements) {
+//          result += element + "\n";
+//
+//       }
+//       return result;
+   }
+
+
 }
