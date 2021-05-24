ffd46e4e133efcc50966c16f49dde3f9fe18835f
Victor Rentea <victorrentea@gmail.com>
2021-05-24 23:19:46 +0300

    cleanup

--- performance-jmh/run.bat
+++
@@ -1,5 +0,0 @@
-call mvn clean package
-java -jar target\benchmarks.jar
-rem java -XX:+UnlockDiagnosticVMOptions -XX:+PrintAssembly -jar target\benchmarks.jar > out.txt
-rem to see generated assembly code, you need to put in your jre/bin/server a dll generated here: https://dropzone.nfshost.com/hsdis/
-
