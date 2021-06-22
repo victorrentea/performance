8e5f0faa5106c8b8028db26a39f0a36a4e19e1e7
Victor Rentea <victorrentea@gmail.com>
2021-06-22 10:55:35 +0300

    add monitoring

--- src/main/java/victor/training/performance/leaks/LeaksApp.java
+++ src/main/java/victor/training/performance/leaks/LeaksApp.java
@@ -1,11 +1,15 @@
 package victor.training.performance.leaks;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Slf4j
 @EnableAsync
@@ -13,6 +17,24 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableMBeanExport
 @SpringBootApplication
 public class LeaksApp {
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry meterRegistry) {
+        return new TimedAspect(meterRegistry);
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor shepardPool() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(20);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("s-");
+//        executor.setRejectedExecutionHandler(new CallerRunsPolicy()); // make the shepard call execute on http pool again
+        executor.initialize();
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        return executor;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(LeaksApp.class, args);
     }
