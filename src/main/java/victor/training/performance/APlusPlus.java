5c04e3688e28ff61d3999a60d3a811eca0a2e6b9
Victor Rentea <victorrentea@gmail.com>
2021-10-18 11:54:56 +0300

    COncurrency today (TM) = immutability + map-reduce

--- src/main/java/victor/training/performance/APlusPlus.java
+++ src/main/java/victor/training/performance/APlusPlus.java
@@ -1,50 +1,48 @@
 package victor.training.performance;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.*;
 
 // race bug
 public class APlusPlus {
 //    private static Integer population = 0;
 //    private static final Object LOCK = new Object();
-    private static AtomicInteger population = new AtomicInteger(0);
+//    private static AtomicInteger population = new AtomicInteger(0);
 
-    public static class ThreadA extends Thread {
-        public void run() {
+    public static class ThreadA implements Callable<Integer> {
+        public Integer call() {
+            int localPopulation = 0;
             for (int i = 0; i < 10_000; i++) {
-//                synchronized (LOCK) { // resource
-//                    population++;
-//                }
-                population.incrementAndGet();
+                localPopulation ++;
             }
+            return localPopulation;
         }
     }
 
-    public static class ThreadB extends Thread {
-        public void run() {
+    public static class ThreadB implements Callable<Integer> {
+        public Integer call() {
+            int localPopulation = 0;
             for (int i = 0; i < 10_000; i++) {
-//                synchronized (LOCK) {
-//                    population++;
-//                }
-                population.incrementAndGet();
+                localPopulation ++;
             }
+            return localPopulation;
         }
     }
 
     // TODO (bonus): ConcurrencyUtil.useCPU(1)
     // TODO (extra bonus): Analyze with JFR
 
-    public static void main(String[] args) throws InterruptedException {
-        ThreadA threadA = new ThreadA();
-        ThreadB threadB = new ThreadB();
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+
+        Future<Integer> f1 = pool.submit(new ThreadA());
+        Future<Integer> f2 = pool.submit(new ThreadB());
 
         long t0 = System.currentTimeMillis();
 
-        threadA.start();
-        threadB.start();
-        threadA.join();
-        threadB.join();
 
         long t1 = System.currentTimeMillis();
+        int population = f1.get() + f2.get ();
         System.out.println("Total = " + population);
         System.out.println("Took = " + (t1 - t0));
     }
