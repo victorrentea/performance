310ff376e99615ac35425065e5661596a7e50b19
Victor Rentea <victorrentea@gmail.com>
2021-11-01 10:24:42 +0200

    First attempt - thread pool executors

--- src/main/java/victor/training/performance/spring/BarService.java
+++ src/main/java/victor/training/performance/spring/BarService.java
@@ -2,6 +2,7 @@ package victor.training.performance.spring;
 
 
 import lombok.Data;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import static java.util.Arrays.asList;
 import static victor.training.performance.util.PerformanceUtil.sleepq;
@@ -30,12 +34,20 @@ public class BarService implements CommandLineRunner {
       log.debug("Got " + orderDrinks());
    }
 
+   @SneakyThrows
    public List<Object> orderDrinks() {
       log.debug("Requesting drinks...");
       long t0 = System.currentTimeMillis();
 
-      Beer beer = barman.pourBeer();
-      Vodka vodka = barman.pourVodka();
+      ExecutorService pool = Executors.newFixedThreadPool(2);
+
+      Future<Beer> futureBeer = pool.submit(() -> barman.pourBeer());
+      Future<Vodka> futureVodka = pool.submit(() -> barman.pourVodka());
+
+      Beer beer = futureBeer.get(); // main sta aici 1 sec
+      Vodka vodka = futureVodka.get(); // main nu mai sta deloc aici,
+      // caci in timp ce stateai dupa bere, barmanul #2 a turnat si vodka :) yeei
+
 
       long t1 = System.currentTimeMillis();
       List<Object> drinks = asList(beer, vodka);
@@ -52,13 +64,13 @@ class Barman {
 
    public Beer pourBeer() {
       log.debug("Pouring Beer...");
-      sleepq(1000);
+      sleepq(1000); // call de API REST
       return new Beer();
    }
 
    public Vodka pourVodka() {
       log.debug("Pouring Vodka...");
-      sleepq(1000);
+      sleepq(1000); // SQL CRIMINAL
       return new Vodka();
    }
 }
