8417b26e48b25993041a279d59c6b76063ca000d
Victor Rentea <victorrentea@gmail.com>
2021-11-01 17:19:05 +0200

    Sa nu modifici campurile pe care faci hashcode/equals dupa ce pui in

--- src/main/java/victor/training/performance/interview/Complexities.java
+++ src/main/java/victor/training/performance/interview/Complexities.java
@@ -1,8 +1,66 @@
 package victor.training.performance.interview;
 
+import java.util.*;
+
 public class Complexities {
    public static void main(String[] args) {
 
+      List<String> strings = new ArrayList<>();
+      cat_ia(strings);
+
+
+      Map<ElementCustom, Integer> map = new HashMap<>(); // 10M
+      Integer emma = map.get(new ElementCustom("Emma")); // O(1) ct
+
+      Set<ElementCustom> puiiMei = new HashSet<>();
+      ElementCustom childOne = new ElementCustom("Emma");
+
+      puiiMei.add(childOne); // complex O(1)
+
+      System.out.println(puiiMei.contains(childOne));
+      System.out.println(childOne.hashCode());
+
+//      childOne.setName("Emma-Simona");
+
+      System.out.println(childOne.hashCode());
+      System.out.println(puiiMei.contains(childOne));
+
+      puiiMei.add(childOne);
+
+      System.out.println(puiiMei);
 
    }
+
+   private static void cat_ia(List<String> strings) {
+      strings.add("a"); // O(1) tip cat de cat constant indiferent de marimea colectiei
+      strings.remove("b"); // O(N)
+      strings.remove(0); // O(N) pt ArrayList. La LinkedList e O(1) -> de aia le folosim la cozi. (add la final ,remove de la inceput)
+   }
+}
+
+class ElementCustom {
+   private final String name;
+
+   public ElementCustom(String name) {
+      this.name = name;
+   }
+
+//   public void setName(String name) {
+//      this.name = name;
+//   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      ElementCustom that = (ElementCustom) o;
+      return Objects.equals(name, that.name);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hashCode(name);
+//      return 1; // toate elem aju in aceeasi galeata ==> hash map = LinkedList .get .put = O(N)
+   }
+   // daca doua ob sunt eq => hash ==
 }
--- src/main/java/victor/training/performance/interview/TheInfamousStringBuilderProblem.java
+++ src/main/java/victor/training/performance/interview/TheInfamousStringBuilderProblem.java
@@ -1,30 +1,49 @@
 package victor.training.performance.interview;
 
-import org.apache.commons.io.FileUtils;
 import victor.training.performance.util.PerformanceUtil;
 
-import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
 public class TheInfamousStringBuilderProblem {
    public static void main(String[] args) throws IOException {
       List<String> elements = IntStream.range(1, 50_000) // try 50K x 6 chars : see TLAB
-          .mapToObj(n -> "hahaha").collect(toList());
+          .mapToObj(n -> "hahaha")
+          .collect(toList());
 
       PerformanceUtil.waitForEnter();
       long t0 = System.currentTimeMillis();
 
-      String s = "";
-      for (String element : elements) {
-         s += element;
+      try (FileWriter writer = new FileWriter("out.txt")) { // sau pe CLOB sau pe HttpServletResponse.getWriter()
+
+//         Stream<String> lines = Files.lines(new File("out.txt").toPath());
+         evident(elements.stream(), writer);
       }
-      FileUtils.writeStringToFile(new File("out.txt"), s);
+
 
       System.out.println("Done. Took " + (System.currentTimeMillis() - t0));
       PerformanceUtil.waitForEnter();
    }
+
+   private static void evident(Stream<String> elements , FileWriter writer) throws IOException {
+//      for (String element : elements) {
+//         writer.write(element);
+         elements.forEach(e -> {
+            try {
+               writer.write(e);
+            } catch (IOException ex) {
+               throw new RuntimeException(ex);
+            }
+         });
+//      }
+   }
 }
+
+
+// char *s
+// int*
