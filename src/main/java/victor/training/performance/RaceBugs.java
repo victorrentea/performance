f9eb93dcbed6e64f9d4e79bf1791a5ae996d8079
Victor Rentea <victorrentea@gmail.com>
2021-11-01 16:05:08 +0200

    Calluri inutile la check email >> dar intreaga paradigma de programare de folosita este GRESITA in 2021. In loc sa modifici date partajate (List emails) >> workerii sa **INTOARCA** listele gasite de fiecare .

--- src/main/java/victor/training/performance/RaceBugs.java
+++ src/main/java/victor/training/performance/RaceBugs.java
@@ -1,63 +1,78 @@
 package victor.training.performance;
 
+import victor.training.performance.RaceBugs.ThreadA.ThreadB;
 import victor.training.performance.util.PerformanceUtil;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.toList;
 
 public class RaceBugs {
-    private static Integer population = 0;
-    private static List<String> emails = new ArrayList<>();
+   private static final List<String> emails = new ArrayList<>();
+//   private static final ConcurrentHashMap // niciodata !
 
-    public static final int N = 10_000;
-    public static class ThreadA extends Thread {
-        public void run() {
-            for (int i = 0; i < N; i++) {
-                population++;
+   public static final int N = 10_000;
 
-                // TODO Collect all emails with EmailFetcher.retrieveEmail(i)
-                // TODO Avoid duplicated emails
-                // TODO All email should be checked with EmailFetcher.checkEmail(email)
+   public static class ThreadA extends Thread {
+      public void run() {
+         for (int i = 0; i < N; i++) {
+            String email = EmailFetcher.retrieveEmail(i);
+
+            if (EmailFetcher.checkEmail(email)) {
+               synchronized (emails) {
+                  if (!emails.contains(email)) {
+                     emails.add(email);
+                  }
+               }
             }
-        }
-    }
+         }
+      }
 
-    public static class ThreadB extends Thread {
-        public void run() {
-            for (int i = N; i < N+N; i++) {
-                population++;
+      public static class ThreadB extends Thread {
+         public void run() {
+            for (int i = N; i < N + N; i++) {
+               String email = EmailFetcher.retrieveEmail(i);
+
+               if (EmailFetcher.checkEmail(email)) {
+                  synchronized (emails) {
+                     if (!emails.contains(email)) {
+                        emails.add(email);
+                     }
+                  }
+               }
             }
-        }
-    }
+         }
+      }
+   }
 
-    // TODO (bonus): ConcurrencyUtil.useCPU(1)
-    // TODO (extra bonus): Analyze with JFR
+   // TODO (bonus): ConcurrencyUtil.useCPU(1)
+   // TODO (extra bonus): Analyze with JFR
 
-    public static void main(String[] args) throws InterruptedException {
-        ThreadA threadA = new ThreadA();
-        ThreadB threadB = new ThreadB();
+   public static void main(String[] args) throws InterruptedException {
+      ThreadA threadA = new ThreadA();
+      ThreadB threadB = new ThreadB();
 
 
-        System.out.println("Started");
-        long t0 = System.currentTimeMillis();
+      System.out.println("Started");
+      long t0 = System.currentTimeMillis();
 
-        threadA.start();
-        threadB.start();
-        threadA.join();
+      threadA.start();
+      threadB.start();
 
-        threadB.join(); // waits for the thread to finish
+      threadA.join();
+      threadB.join(); // waits for the thread to finish
 
-        long t1 = System.currentTimeMillis();
-        System.out.printf("Result: %,d\n", population);
-//        System.out.printf("Emails.size: %,d\n", emails.size());
+      long t1 = System.currentTimeMillis();
+//        System.out.printf("Result: %,d\n", population.longValue());
+      System.out.printf("Emails.size: %,d\n", emails.size());
 //        System.out.printf("  Emails.size expected: %,d total, %,d unique\n", N*2, N);
-//        System.out.printf("Emails checks: %,d\n", EmailFetcher.emailChecksCounter.get());
-        System.out.println("Time: " + (t1 - t0) + " ms");
-        // Note: avoid doing new Thread() -> use thread pools in a real app
-    }
+        System.out.printf("Emails checks: %,d\n", EmailFetcher.emailChecksCounter.get());
+      System.out.println("Time: " + (t1 - t0) + " ms");
+      // Note: avoid doing new Thread() -> use thread pools in a real app
+   }
 
 
 }
@@ -65,24 +80,29 @@ public class RaceBugs {
 
 class EmailFetcher {
 
-    private static final List<String> ALL_EMAILS = new ArrayList<>();
-    static {
-        List<String> list = IntStream.range(0, RaceBugs.N)
-            .mapToObj(i -> "email" + i + "@example.com") // = 50% overlap
-            .collect(toList());
-//        Collections.shuffle(ALL_EMAILS); // randomize, or
-        ALL_EMAILS.addAll(list); // this produces more dramatic results
-        ALL_EMAILS.addAll(list);
-    }
+   private static final List<String> ALL_EMAILS = new ArrayList<>();
 
-    /** Pretend some external call */
-    public static String retrieveEmail(int i) {
-        return ALL_EMAILS.get(i);
-    }
-    public static AtomicInteger emailChecksCounter = new AtomicInteger(0);
-    public static boolean checkEmail(String email) {
-        emailChecksCounter.incrementAndGet();
-        PerformanceUtil.sleepSomeTime(0,1);
-        return true;
-    }
+   static {
+      List<String> list = IntStream.range(0, RaceBugs.N)
+          .mapToObj(i -> "email" + i + "@example.com") // = 50% overlap
+          .collect(toList());
+//        Collections.shuffle(ALL_EMAILS); // randomize, or
+      ALL_EMAILS.addAll(list); // this produces more dramatic results
+      ALL_EMAILS.addAll(list);
+   }
+
+   /**
+    * Pretend some external call
+    */
+   public static String retrieveEmail(int i) {
+      return ALL_EMAILS.get(i);
+   }
+
+   public static AtomicInteger emailChecksCounter = new AtomicInteger(0);
+
+   public static boolean checkEmail(String email) {
+      emailChecksCounter.incrementAndGet();
+      PerformanceUtil.sleepSomeTime(2, 3);
+      return true;
+   }
 }
--- src/main/java/victor/training/performance/interview/SummingBoxes.java
+++ src/main/java/victor/training/performance/interview/SummingBoxes.java
@@ -1,28 +1,35 @@
 package victor.training.performance.interview;
 
-import static java.util.stream.Collectors.toList;
-
-import java.util.List;
 import java.util.stream.LongStream;
 
 public class SummingBoxes {
 
+
+
     public static void main(String[] args) {
-        List<Long> list = LongStream.range(1, 10_000_000)
-				.boxed().collect(toList());
+        long[] list = LongStream.range(1, 10_000_000)
+            .toArray();
 
         // TODO how about memory efficiency?
+//        System.out.println("Hit ENTER");
+//        new Scanner(System.in).nextLine();
 
         long t0 = System.currentTimeMillis();
 
-        Long sum = 0L;
-        for (Long i : list) {
-            sum += i;
-        }
+        Long sum = profileMe(list);
 
         long t1 = System.currentTimeMillis();
         System.out.println(sum);
         System.out.println(t1 - t0);
+    }
+
+    private static Long profileMe(long[] list) {
+//        long sum = 0L;
+//        for (long i : list) {
+//            sum += i * i;
+//        }
+//        return sum;
+        return LongStream.of(list).sum();
     }
 
 }
