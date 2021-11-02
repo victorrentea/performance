46c420d51875e9119b920850f8c37bea853b9b55
Victor Rentea <victorrentea@gmail.com>
2021-11-02 08:55:51 +0200

    undo caching

--- src/main/java/victor/training/performance/jpa/CountryRepo.java
+++ src/main/java/victor/training/performance/jpa/CountryRepo.java
@@ -1,12 +1,11 @@
 package victor.training.performance.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.QueryHints;
 
-import javax.persistence.QueryHint;
 import java.util.List;
 
 public interface CountryRepo extends JpaRepository<Country, Long> {
-   @QueryHints(@QueryHint(name="org.hibernate.cacheable",value ="true")) // Only caches returned IDs => MUST-HAVE cache on Entity
+   //@QueryHints(@QueryHint(name="org.hibernate.cacheable",value ="true"))
+      // Only caches returned IDs => MUST-HAVE cache on Entity
    List<Country> findAll();
 }
