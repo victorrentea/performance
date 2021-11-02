36de63fc0e5ef26ae39139ac12336c6eeaa06306
Victor Rentea <victorrentea@gmail.com>
2021-11-02 23:11:49 +0200

    added backing off for cache metric if ehcache is backing the spring cacheable

--- src/main/java/victor/training/performance/spring/metrics/ConcurrentMapCacheMeterBinder.java
+++ src/main/java/victor/training/performance/spring/metrics/ConcurrentMapCacheMeterBinder.java
@@ -40,6 +40,5 @@ public class ConcurrentMapCacheMeterBinder extends CacheMeterBinder {
 
     @Override
     protected void bindImplementationSpecificMetrics(MeterRegistry meterRegistry) {
-
     }
 }
--- src/main/java/victor/training/performance/spring/metrics/MeteredSimpleCacheConfig.java
+++ src/main/java/victor/training/performance/spring/metrics/MeteredSimpleCacheConfig.java
@@ -2,6 +2,8 @@ package victor.training.performance.spring.metrics;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.metrics.cache.CacheMeterBinderProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.support.SimpleCacheManager;
@@ -13,8 +15,9 @@ import java.util.List;
 import static java.util.stream.Collectors.toList;
 
 @Slf4j
+@ConditionalOnProperty(value = "spring.cache.cache-names")
 @Configuration
-// from https://medium.com/@iliamsharipov_56660/spring-boot-actuator-for-concurrentmapcache-2c7f0d290934
+// based on https://medium.com/@iliamsharipov_56660/spring-boot-actuator-for-concurrentmapcache-2c7f0d290934
 public class MeteredSimpleCacheConfig extends CachingConfigurerSupport {
    @Value("${spring.cache.cache-names}")
    private List<String> cacheNames;
@@ -25,9 +28,14 @@ public class MeteredSimpleCacheConfig extends CachingConfigurerSupport {
       SimpleCacheManager cacheManager = new SimpleCacheManager();
 
       cacheManager.setCaches(cacheNames.stream()
-          .peek(name -> log.info("Decorating simple cache " + name))
+          .peek(name -> log.info("Decorating simple cache '{}'", name))
           .map(ConcurrentMapCacheMetricsWrapper::new).collect(toList()));
       return cacheManager;
    }
 
+
+   @Bean
+   public CacheMeterBinderProvider<ConcurrentMapCacheMetricsWrapper> method() {
+      return ConcurrentMapCacheMeterBinder::new;
+   }
 }
