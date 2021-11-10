2b9a76a269f1dd238a50d90a397ee5c24032c6f5
Victor Rentea <victorrentea@gmail.com>
2021-11-10 16:31:13 +0200

    Solutia 2 pentru @Blob/@Clob

--- src/test/java/victor/training/performance/jpa/LobTest.java
+++ src/test/java/victor/training/performance/jpa/LobTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.transaction.TestTransaction;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.*;
@@ -21,53 +20,35 @@ public class LobTest {
    @Autowired
    ProfRepo repo;
    @Autowired
+   ProfDataRepo dataRepo;
+   @Autowired
    EntityManager em;
 
    @Test
    void test() {
-      Prof prof = new Prof()
-          .setName("Tavi")
-          .setData(new ProfData()
-              .setCvMarkdown("**Mare Sef**")
-          );
+      Prof prof = new Prof().setName("Tavi");
       repo.save(prof);
+      dataRepo.save(new ProfData().setProf(prof).setCvMarkdown("**MARE CHEF**"));
 
       em.flush();
       em.clear();
 
       List<Prof> profList = repo.findAll();
-      // aici in lista de profi stau instante de Prof.class care au id,name,si in "data" field o instanta "fictiva" de ProfData
-      // acea instanta fictiva
-      // class victor.training.performance.jpa.ProfData$HibernateProxy$FvglDxDt
-      // NU contine datele luate din DB. pt ca i-am spus @OneToOne(LAZY)
-      // Hibernate de fapt a pus in
-
-//      prof.setData(new ProfData(){ // subclasa anonima ~~~ proxy
-//         @Override
-//         public String getCvMarkdown() {
-////            if (!amIncarcatMarkdown) {
-////               setCvMarkdown(iau din DB);  !!!!!!! Trebuie inca Tranzactia curenta sa fie deschisa.
-////               amIncarcatMarkdown = true;
-////            }
-//            return super.getCvMarkdown();
-//         }
-//      });
-
-//      TestTransaction.end(); // asta nu permite proxy-ului sa mai incarce date caci inchide TX: elibereaza (pierde) conexiunea cu DB.
 
       for (Prof p : profList) {
          System.out.println(p.getName());
-         System.out.println(p.getData().getClass());
-         System.out.println(p.getData().getCvMarkdown()); // linia asta triggereaza proxy-ul sa faca LAZY load pe sub mana din DB
       }
-
-      // CLOB folosesti chiar si pentru un string cand e mai mare de 2000 caractere.
-      // CLOB nu sunt indexabile.    SELECT from PROF where cv like '%UPB%'
+      System.out.println("Pana aici nici un query dupa markdown");
+      ProfData data = dataRepo.findByProfId(prof.getId());
+      System.out.println("Data " +data) ;
 
    }
 }
 
 interface ProfRepo extends JpaRepository<Prof, Long> {}
+interface ProfDataRepo extends JpaRepository<ProfData, Long> {
+   ProfData findByProfId(Long profId);
+}
 
 //CLOB sau BLOB
 
@@ -79,8 +60,6 @@ class Prof {
    private Long id;
    private String name;
 
-   @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-   private ProfData data;
 }
 
 @Entity
@@ -89,12 +68,10 @@ class ProfData {
    @Id
    @GeneratedValue
    private Long id;
-//
-//   static class HibernateProxy {
-//      static class FvglDxDt extends ProfData{
-//
-//      }
-//   }
+
+   @OneToOne
+   private Prof prof;
+
    @Lob
    private byte[] cvPdf; // BLOB
 
@@ -103,4 +80,4 @@ class ProfData {
 }
 
 //strategia 1: Parinte ---OneToOne(fetch=LAZY)--> Copil cu CLOB
-//strategia 1: Parinte <---OneToOne(fetch=LAZY)-- Copil cu CLOB
+//strategia 2: Parinte <---OneToOne-- Copil cu CLOB
