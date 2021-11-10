ae38fc6032650c0c577b1eb1fc168ba5d97ca685
Victor Rentea <victorrentea@gmail.com>
2021-11-10 12:22:51 +0200

    Paginare cu driving query + left oin fetch cu IN operator

--- src/main/java/victor/training/performance/spring/Profile2_NPlusOne.java
+++ src/main/java/victor/training/performance/spring/Profile2_NPlusOne.java
@@ -6,18 +6,24 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Slf4j
-//@RestController // TODO uncomment and study
+@RestController // TODO uncomment and study
 @RequestMapping("profile/nplus1")
 @RequiredArgsConstructor
 public class Profile2_NPlusOne implements CommandLineRunner {
@@ -34,24 +40,27 @@ public class Profile2_NPlusOne implements CommandLineRunner {
 
    @GetMapping
    public Page<Parent> query() {
-      return repo.findByNameLike("%ar%", PageRequest.of(0, 20));
+      Page<Long> idPage = repo.findByNameLike("%ar%", PageRequest.of(1, 20).withSort(Direction.ASC, "name"));
 
-//      Page<Long> idPage = repo.findByNameLike("%ar%", PageRequest.of(0, 10));
-//      List<Long> parentIds = idPage.getContent();
-//      Map<Long, Parent> parents = repo.findParentsWithChildren(parentIds).stream().collect(toMap(Parent::getId, identity()));
-//      return idPage.map(parents::get);
+      List<Parent> parents = repo.findParentsWithChildren(idPage.getContent());
+      Map<Long, Parent> parentsById = parents.stream().collect(Collectors.toMap(Parent::getId, Function.identity()));
+
+      return idPage.map(parentId -> parentsById.get(parentId));
    }
 }
 
 interface ParentRepo extends JpaRepository<Parent, Long> {
-   @Query("SELECT p FROM Parent p WHERE p.name LIKE ?1")
-   Page<Parent> findByNameLike(String namePart, Pageable page);
+   // driving query
+  @Query("SELECT p.id FROM Parent p WHERE p.name LIKE ?1")
+   Page<Long> findByNameLike(String namePart, Pageable page);
+
+   // fetching query
+   @Query("SELECT distinct p FROM Parent p LEFT JOIN FETCH p.children WHERE p.id IN ?1")
+   List<Parent> findParentsWithChildren(List<Long> parentIds);
 
 //   @Query("SELECT p.id FROM Parent p WHERE p.name LIKE ?1")
 //   Page<Long> findByNameLike(String namePart, Pageable page);
 
-//   @Query("SELECT p FROM Parent p LEFT JOIN FETCH p.children WHERE p.id IN ?1")
-//   Set<Parent> findParentsWithChildren(List<Long> parentIds);
 }
 
 @Entity
--- src/test/java/victor/training/performance/jpa/NPlusOneTest.java
+++ src/test/java/victor/training/performance/jpa/NPlusOneTest.java
@@ -133,13 +133,11 @@ interface ParentRepo extends JpaRepository<Parent, Long> {
    @Query("SELECT distinct p FROM Parent p LEFT JOIN FETCH p.children")
    List<Parent> loadAllWithChildren();
 
-   @Query(value = """
-   select parent0_.name        as name3_2_0_,
-           STRING_AGG(c.NAME, ',') within group (order by c.name asc) children_names
-    from parent parent0_
-             left outer join child c on parent0_.id = c.parent_id
-    group by parent0_.name
-   """, nativeQuery = true)
+   @Query(value = "select parent0_.name        as name3_2_0_,\n" +
+                  "           STRING_AGG(c.NAME, ',') within group (order by c.name asc) children_names\n" +
+                  "    from parent parent0_\n" +
+                  "             left outer join child c on parent0_.id = c.parent_id\n" +
+                  "    group by parent0_.name", nativeQuery = true)
    List<Object[]> loadDinNative();
 }
 
