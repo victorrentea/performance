ea0c937bcf0e34b27c63972b550873813328cd4e
Victor Rentea <victorrentea@gmail.com>
2021-11-10 11:30:45 +0200

    Native query + Java 17 :)

--- src/main/java/victor/training/performance/jpa/Parent.java
+++ src/main/java/victor/training/performance/jpa/Parent.java
@@ -5,6 +5,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+//@NamedNativeQueries({
+//@NamedNativeQuery(name = "arbitrar", query = "select parent0_.name        as name3_2_0_,\n" +
+//                                             "       STRING_AGG(c.NAME, ',') within group (order by c.name asc) children_names\n" +
+//                                             "from parent parent0_\n" +
+//                                             "         left outer join child c on parent0_.id = c.parent_id\n" +
+//                                             "group by parent0_.name")
+//})
+
 public class Parent {
     @Id
     @GeneratedValue
@@ -14,6 +22,12 @@ public class Parent {
     private int age;
 
     @OneToMany(cascade = CascadeType.ALL)
+//        , fetch = FetchType.EAGER) // NICIODATA pt ca oriunde scoti
+    // findById sau @Query un Parent di nDB, se intample
+    // a) fie un JOIN cu randuri in plus in result fie
+    // b) Query-uri in plus dupa copii
+     // >>>> CHIAR VREAU MEREU COPIII MEI
+            // exceptien 0.1% cand faci: DDD si asta e un Aggregat care are Child private Entities
     @JoinColumn(name = "PARENT_ID")
     private Set<Child> children = new HashSet<>();
 
--- src/test/java/victor/training/performance/jpa/NPlusOneTest.java
+++ src/test/java/victor/training/performance/jpa/NPlusOneTest.java
@@ -6,14 +6,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.transaction.TestTransaction;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -25,83 +26,121 @@ import static org.assertj.core.api.Assertions.tuple;
 @SpringBootTest
 @Transactional
 @Rollback(false)
-@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+//@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class NPlusOneTest {
 
-	@Autowired
-	EntityManager em;
-	@Autowired
-	ParentRepo repo;
-	@Autowired
-	ParentSearchViewRepo searchRepo;
+   @Autowired
+   EntityManager em;
+   @Autowired
+   ParentRepo repo;
+   @Autowired
+   ParentSearchViewRepo searchRepo;
 
-	@BeforeEach
-	void persistData() {
-		repo.save(new Parent("Victor")
-				.setAge(36)
-				.addChild(new Child("Emma"))
-				.addChild(new Child("Vlad"))
-		);
-		repo.save(new Parent("Peter")
-				.setAge(41)
-				.addChild(new Child("Maria"))
-				.addChild(new Child("Paul"))
-				.addChild(new Child("Stephan"))
-		);
-		TestTransaction.end();
+   @BeforeEach
+   void persistData() {
+      repo.save(new Parent("Victor")
+          .setAge(36)
+          .addChild(new Child("Emma"))
+          .addChild(new Child("Vlad"))
+      );
+      repo.save(new Parent("Trofim"));
+      repo.save(new Parent("Peter")
+          .setAge(41)
+          .addChild(new Child("Maria"))
+          .addChild(new Child("Paul"))
+          .addChild(new Child("Stephan"))
+      );
+      TestTransaction.end();
 
-		TestTransaction.start();
-	}
+      TestTransaction.start();
+   }
 
-	@Test
-	void nPlusOne() {
-		List<Parent> parents = repo.findAll();
+   @Test
+   void altLocInCode() {
+      System.out.println(repo.findById(1L).get());
+   }
 
-		// TODO reduce the number of queries ran inside countChildren
-		// TODO reduce the total number of queries ran to one
-		// TODO how to paginate parents while prefetching children?
+   @Test
+   void nPlusOneParentWithoutChildren() {
+      List<Parent> parents = repo.loadAllWithChildren();
+      for (Parent parent : parents) {
+         String childrenStr = parent.getChildren().stream()
+             .map(Child::getName)
+             .collect(joining(","));
+         if (parent.getChildren().size() == 0) {
+            childrenStr = "BURLAC";
+         }
+         System.out.println(parent.getName() + ": " + childrenStr);
+      }
+   }
+   @Test
+   void nPlusOne_dinNative() {
+      List<Object[]> parents = repo.loadDinNative();
 
-		int totalChildren = countChildren(parents);
-		assertThat(totalChildren).isEqualTo(5);
-	}
-
-	// far away...
-	private int countChildren(Collection<Parent> parents) {
-		log.debug("Start counting children of {} parents: {}", parents.size(), parents);
-		int total = 0;
-		for (Parent parent : parents) {
-			total += parent.getChildren().size();
-		}
-		log.debug("Done counting: {} children", total);
-		return total;
-	}
+      for (Object[] parent : parents) {
+         System.out.println(Arrays.toString(parent));
+      }
 
 
+   }
+
+   @Test
+   void nPlusOne() {
+      List<Parent> parents = repo.loadAllWithChildren();
+      // TODO reduce the number of queries ran inside countChildren
+      // TODO reduce the total number of queries ran to one
+      // TODO how to paginate parents while prefetching children?
+
+      int totalChildren = countChildren(parents);
+      assertThat(totalChildren).isEqualTo(5);
+   }
+
+   // far away...
+   private int countChildren(Collection<Parent> parents) {
+      log.debug("Start counting children of {} parents: {}", parents.size(), parents);
+      int total = 0;
+      for (Parent parent : parents) {
+         total += parent.getChildren().size();
+      }
+      log.debug("Done counting: {} children", total);
+      return total;
+   }
 
 
-	@Test
-	@Sql("/create-view.sql")
-	public void searchOnView() {
-		var parentViews = repo.findAll()
-			.stream().map(p -> new ParentSearchView(
-				p.getId(),
-				p.getName(),
-				p.getChildren().stream().map(Child::getName).sorted().collect(joining(","))
-			));
+   @Test
+   @Sql("/create-view.sql")
+   public void searchOnView() {
+      var parentViews = repo.findAll()
+          .stream().map(p -> new ParentSearchView(
+              p.getId(),
+              p.getName(),
+              p.getChildren().stream().map(Child::getName).sorted().collect(joining(","))
+          ));
 //		var parentViews = searchRepo.findAll();
 
-		// TODO 1 restrict to first page (of 1 element)
-		// TODO 2 search by parent age >= 40
-		assertThat(parentViews)
-			.extracting("name","childrenNames")
-			.containsExactlyInAnyOrder(
-				tuple("Victor","Emma,Vlad"),
-				tuple("Peter","Maria,Paul,Stephan"))
-		;
-	}
+      // TODO 1 restrict to first page (of 1 element)
+      // TODO 2 search by parent age >= 40
+      assertThat(parentViews)
+          .extracting("name", "childrenNames")
+          .containsExactlyInAnyOrder(
+              tuple("Victor", "Emma,Vlad"),
+              tuple("Peter", "Maria,Paul,Stephan"))
+      ;
+   }
 }
 
 interface ParentRepo extends JpaRepository<Parent, Long> {
+   @Query("SELECT distinct p FROM Parent p LEFT JOIN FETCH p.children")
+   List<Parent> loadAllWithChildren();
+
+   @Query(value = """
+   select parent0_.name        as name3_2_0_,
+           STRING_AGG(c.NAME, ',') within group (order by c.name asc) children_names
+    from parent parent0_
+             left outer join child c on parent0_.id = c.parent_id
+    group by parent0_.name
+   """, nativeQuery = true)
+   List<Object[]> loadDinNative();
 }
 
 
