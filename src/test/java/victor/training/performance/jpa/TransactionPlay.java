5254ddeac14e7c74f66462eda287a11fc46496e7
Victor Rentea <victorrentea@gmail.com>
2021-11-11 11:02:09 +0200

    Ia din DB id cu id din secventa

---
+++ src/test/java/victor/training/performance/jpa/TransactionPlay.java
@@ -0,0 +1,85 @@
+package victor.training.performance.jpa;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@SpringBootTest
+public class TransactionPlay {
+   @Autowired
+   TransactionPlayground playground;
+
+   @Test
+   void test() { // netranzactiona, ca de ex dintr-un RestController
+      System.out.println(playground.getClass());
+      System.out.println("------ ");
+      playground.tx1(); // HTTP req 1
+      System.out.println("------ ");
+      playground.tx2(); // HTTP req 2
+      System.out.println("------ ");
+   }
+}
+
+//      new TransactionPlayground() {
+//         @Override
+//         public void tx1() {
+//            if (propagation=REQUIRED && n-amTranzactieDeschisaDeja) startTx;
+//            try {
+//               super.tx1();
+//               commitTx();
+//            } catch (Exception e) {
+//               rollbackTx();
+//            }
+//         }
+//      }
+//   }
+
+@Service
+@Transactional // default (propagation = Propagation.REQUIRED)
+   // face un proxy in jurul TransactionPlayground
+class TransactionPlayground {
+   @Autowired
+   MessageRepo messageRepo;
+
+   public void tx1() {
+      Message m = new Message("Mesaj1");
+      messageRepo.save(m);
+      System.out.println("dupa save entitatea ta sa capete ID: " + m.getId() );
+      messageRepo.save(new Message(null));
+   }
+
+   public void tx2() {
+
+   }
+}
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+class Message {
+   @Id
+   @GeneratedValue
+   private Long id;
+   @Column(nullable = false)
+   private String name;
+
+   public Message(String name) {
+      this.name = name;
+   }
+}
+
+interface MessageRepo extends JpaRepository<Message, Long> {
+
+}
