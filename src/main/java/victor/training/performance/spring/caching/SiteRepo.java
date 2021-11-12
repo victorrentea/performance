a4c0f312c3eb50969484fff830a1291d39bbf2fc
Victor Rentea <victorrentea@gmail.com>
2021-11-12 09:18:10 +0200

    CLEANUP unused files

--- src/main/java/victor/training/performance/spring/caching/SiteRepo.java
+++
@@ -1,6 +0,0 @@
-package victor.training.performance.spring.caching;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SiteRepo extends JpaRepository<Site, Long> {
-}
