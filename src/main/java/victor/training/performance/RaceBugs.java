df25d85a9a9ab78145994d5b18da37a2a1ed6c24
Victor Rentea <victorrentea@gmail.com>
2021-12-06 15:39:50 +0200

    Am scos din zona critica apelul scump ca sa maresc paralelizarea

--- src/main/java/victor/training/performance/RaceBugs.java
+++ src/main/java/victor/training/performance/RaceBugs.java
@@ -2,7 +2,8 @@ package victor.training.performance;
 
 import victor.training.performance.util.PerformanceUtil;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -13,59 +14,59 @@ import java.util.stream.IntStream;
 import static java.util.stream.Collectors.toList;
 
 public class RaceBugs {
-    private static List<String> emails = new ArrayList<>();
-
-    private static AtomicInteger population = new AtomicInteger(0);
-    public static final int N = 100_000;
-    private static final Object monitor = new Object();
+   public static final int N = 100;
+   private static final Object monitor = new Object();
+   private static final List<String> emails = new ArrayList<>();
 
 
-    // TODO Collect all emails with EmailFetcher.retrieveEmail(i)
-    // TODO Avoid duplicated emails
-    // TODO All email should be checked with EmailFetcher.checkEmail(email)
-    // TODO Reduce the no of calls to checkEmail
+   // TODO Collect all emails with EmailFetcher.retrieveEmail(i)
+   // TODO Avoid duplicated emails
+   // TODO All email should be checked with EmailFetcher.checkEmail(email)
+   // TODO Reduce the no of calls to checkEmail
 
-    public static class Worker1 implements Runnable {
-        public void run() {
-            for (int i = 0; i < N; i++) {
-//                synchronized (monitor) {
-                    population.incrementAndGet();
-//                }
-            }
-        }
-    }
+   public static void main(String[] args) throws InterruptedException, ExecutionException {
+      ExecutorService pool = Executors.newFixedThreadPool(2);
 
-    public static class Worker2 implements Runnable {
-        public void run() {
-            for (int i = 0; i < N; i++) {
-//                synchronized (monitor) {
-                    population.incrementAndGet();
-//                }
-            }
-        }
-    }
+      System.out.println("Started");
+      long t0 = System.currentTimeMillis();
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
-        ExecutorService pool = Executors.newFixedThreadPool(2);
+      Future<?> future1 = pool.submit(new Worker1());
+      Future<?> future2 = pool.submit(new Worker2());
 
-        System.out.println("Started");
-        long t0 = System.currentTimeMillis();
+      // wait for the tasks to complete
+      future1.get();
+      future2.get();
 
-        Future<?> future1 = pool.submit(new Worker1());
-        Future<?> future2 = pool.submit(new Worker2());
-
-        // wait for the tasks to complete
-        future1.get();
-        future2.get();
-
-        long t1 = System.currentTimeMillis();
-        System.out.printf("Result: %,d\n", population.intValue());
-//        System.out.printf("Emails.size: %,d\n", emails.size());
+      long t1 = System.currentTimeMillis();
+//      System.out.printf("Result: %,d\n", population.intValue());
+      System.out.printf("Emails.size: %,d\n", emails.size());
 //        System.out.printf("  Emails.size expected: %,d total, %,d unique\n", N*2, N);
 //        System.out.printf("Emails checks: %,d\n", EmailFetcher.emailChecksCounter.get());
-        System.out.println("Time: " + (t1 - t0) + " ms");
-        // Note: avoid doing new Thread() -> use thread pools in a real app
-    }
+      System.out.println("Time: " + (t1 - t0) + " ms");
+      // Note: avoid doing new Thread() -> use thread pools in a real app
+   }
+
+   public static class Worker1 implements Runnable {
+      public void run() {
+         for (int i = 0; i < N; i++) {
+            String email = EmailFetcher.retrieveEmail(i);
+            synchronized (monitor) {
+               emails.add(email);
+            }
+         }
+      }
+   }
+
+   public static class Worker2 implements Runnable {
+      public void run() {
+         for (int i = N; i < N + N; i++) {
+            String email = EmailFetcher.retrieveEmail(i);
+            synchronized (monitor) {
+               emails.add(email);
+            }
+         }
+      }
+   }
 
 
 }
@@ -73,24 +74,29 @@ public class RaceBugs {
 
 class EmailFetcher {
 
-    private static final List<String> ALL_EMAILS = new ArrayList<>();
-    static {
-        List<String> list = IntStream.range(0, RaceBugs.N)
-            .mapToObj(i -> "email" + i + "@example.com") // = 50% overlap
-            .collect(toList());
-//        Collections.shuffle(ALL_EMAILS); // randomize, or
-        ALL_EMAILS.addAll(list); // this produces more dramatic results
-        ALL_EMAILS.addAll(list);
-    }
+   private static final List<String> ALL_EMAILS = new ArrayList<>();
+   public static AtomicInteger emailChecksCounter = new AtomicInteger(0);
 
-    /** Pretend some external call */
-    public static String retrieveEmail(int i) {
-        return ALL_EMAILS.get(i);
-    }
-    public static AtomicInteger emailChecksCounter = new AtomicInteger(0);
-    public static boolean checkEmail(String email) {
-        emailChecksCounter.incrementAndGet();
-        PerformanceUtil.sleepSomeTime(0,1);
-        return true;
-    }
+   static {
+      List<String> list = IntStream.range(0, RaceBugs.N)
+          .mapToObj(i -> "email" + i + "@example.com") // = 50% overlap
+          .collect(toList());
+//        Collections.shuffle(ALL_EMAILS); // randomize, or
+      ALL_EMAILS.addAll(list); // this produces more dramatic results
+      ALL_EMAILS.addAll(list);
+   }
+
+   /**
+    * Pretend some external call
+    */
+   public static String retrieveEmail(int i) {
+      PerformanceUtil.sleepq(10);
+      return ALL_EMAILS.get(i);
+   }
+
+   public static boolean checkEmail(String email) {
+      emailChecksCounter.incrementAndGet();
+      PerformanceUtil.sleepSomeTime(0, 1);
+      return true;
+   }
 }
