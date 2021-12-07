1a8b7e99f351d19b58b00dc8e95693cabe800a88
Victor Rentea <victorrentea@gmail.com>
2021-12-07 11:42:15 +0200

    leak 1 fixed.

--- src/main/java/victor/training/performance/ThreadLocals.java
+++ src/main/java/victor/training/performance/ThreadLocals.java
@@ -8,32 +8,35 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
-public class ThreadLocal {
-   public static String currentUser;
+public class ThreadLocals {
+   public static ThreadLocal<String> currentUser = new ThreadLocal<>();
 
    static AtomicInteger counter = new AtomicInteger(0);
 
    public static void main(String[] args) {
       ExecutorService pool = Executors.newFixedThreadPool(10);
-
       Runnable task = () -> {
+         currentUser.set("user" + counter.incrementAndGet());
 
-         currentUser = "user" + counter.incrementAndGet();
-
-         log.debug("Incep fluxul cu userul: " + currentUser);
-         PerformanceUtil.sleepq(1);
-         method();
-
+         try {
+            log.debug("Incep fluxul cu userul: " + currentUser.get());
+            PerformanceUtil.sleepq(1);
+            method();
+         } finally {
+            currentUser.remove(); // PATTERN: imediat dupa set pe un threa dlocal faci try {..} finally {remove}
+         }
       };
       pool.submit(task);
       pool.submit(task);
       pool.submit(task);
       pool.submit(task);
       pool.submit(task);
+
+//      SecurityContextHolder.getContext()
    }
 
    // Repository / DAO
    private static void method() {
-      log.debug("INSERT INTO .. (..., CREATED_BY) VALUES (..., {})", currentUser);
+      log.debug("INSERT INTO .. (..., CREATED_BY) VALUES (..., {})", currentUser.get());
    }
 }
--- src/main/java/victor/training/performance/spring/Leak1_ThreadLocal.java
+++ src/main/java/victor/training/performance/spring/Leak1_ThreadLocal.java
@@ -1,21 +1,39 @@
 package victor.training.performance.spring;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.performance.util.BigObject20MB;
+@Scope(value = "request",proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Component
+@Data
+class RequestData {
+   private BigObject20MB bigObject;
+}
 
 @RestController
 @RequestMapping("leak1")
 public class Leak1_ThreadLocal {
-   public static ThreadLocal<BigObject20MB> threadLocal = new ThreadLocal<>();
+   public static final ThreadLocal<BigObject20MB> threadLocal = new ThreadLocal<>();
+   @Autowired
+   RequestData requestData;
 
    @GetMapping
    public String test() {
       BigObject20MB bigObject = new BigObject20MB();
       threadLocal.set(bigObject);
+      requestData.setBigObject(bigObject);
 
-      businessMethod1();
+      try {
+         businessMethod1();
+      } finally {
+         threadLocal.remove();
+      }
       return "Magic can do harm.";
    }
 
@@ -29,5 +47,6 @@ public class Leak1_ThreadLocal {
       // TODO think of throw new RuntimeException();
    }
 }
+
 
 
