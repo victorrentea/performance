3b92a6b0c9f6bd3671dc484ff78e2e1bac7b87c3
Victor Rentea <victorrentea@gmail.com>
2021-12-09 15:32:27 +0200

    fixed the race bug by synchtonized (private mutext) (oups)

--- src/main/java/victor/training/performance/RaceBugs.java
+++ src/main/java/victor/training/performance/RaceBugs.java
@@ -35,7 +35,7 @@ public class RaceBugs {
     public static class Worker1 implements Runnable {
         public void run() {
             for (int i = 0; i < N/2; i++) {
-                synchronized (RaceBugs.class) {
+                synchronized (mutex) {
                     population++;
                 }
             }
@@ -44,7 +44,7 @@ public class RaceBugs {
     public static class Worker2 implements Runnable {
         public void run() {
             for (int i = N/2; i < N; i++) {
-                synchronized (RaceBugs.class) {
+                synchronized (mutex) {
                     population++;
                 }
             }
