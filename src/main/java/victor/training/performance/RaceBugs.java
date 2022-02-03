3ed8748435c56ec87f34c792a55e7d8ed73fce90
Victor Rentea <victorrentea@gmail.com>
2022-02-03 11:12:09 +0200

    HORROR: schimbare de la mod blocant de a scrie impreuna datee pe variabile la un mecanism  style 'pure function' (map-reduce) : spargi munca si workerii iti intorc rezultatele.

--- src/main/java/victor/training/performance/RaceBugs.java
+++ src/main/java/victor/training/performance/RaceBugs.java
@@ -3,31 +3,33 @@ package victor.training.performance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.*;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.stream.Collectors.toList;
 
 
 public class RaceBugs {
    private static final Logger log = LoggerFactory.getLogger(RaceBugs.class);
-
+   private static final Object LOCK = new Object();
    private final ExternalDependency dependency;
-   public RaceBugs(ExternalDependency dependency) {
-      this.dependency = dependency;
-   }
 
    // this pools shutdowns automatically at test end
 
    // ===================================================================
+   private final List<String> uniqueEmails = new ArrayList<>();
+   // TODO fix population++ race (warmup)
+//   private Integer population = 0;
+   private AtomicInteger population = new AtomicInteger(0);
+
+
+   public RaceBugs(ExternalDependency dependency) {
+      this.dependency = dependency;
+   }
 
    public static void main(String[] args) throws Exception {
       List<Integer> ids = IntStream.range(0, 20_000).boxed().collect(toList());
@@ -50,21 +52,6 @@ public class RaceBugs {
    }
 
 
-   // TODO fix population++ race (warmup)
-//   private Integer population = 0;
-   private AtomicInteger population = new AtomicInteger(0);
-   private static final Object LOCK = new Object();
-
-   private void workHere(List<Integer> idsChunk) { // ran in 2 parallel threads ATENTIE
-      for (Integer id : idsChunk) {
-            // TODO if (dependency.isAlive(id)) { // returns 50% true  => call tests
-//            population++;
-         population.incrementAndGet();
-      }
-   }
-
-
-
    // ================================================================
 
    // TODO Collect all emails with EmailFetcher.retrieveEmail(i)
@@ -72,42 +59,68 @@ public class RaceBugs {
    // TODO All email should be checked with EmailFetcher.checkEmail(email)
    // TODO Reduce the number of calls to checkEmail
 
+   private void workHere(List<Integer> idsChunk) { // ran in 2 parallel threads ATENTIE
+      for (Integer id : idsChunk) {
+         // TODO if (dependency.isAlive(id)) { // returns 50% true  => call tests
+//            population++;
+         population.incrementAndGet();
+      }
+   }
+
    public Collection<String> retrieveEmails(List<Integer> ids) throws Exception {
       int len = ids.size();
       ExecutorService pool = new ThreadPoolExecutor(0, 2, 500, MILLISECONDS, new SynchronousQueue<>());
-      Future<?> future1 = pool.submit(() -> doRetrieveEmails(ids.subList(0, len / 2)));
-      Future<?> future2 = pool.submit(() -> doRetrieveEmails(ids.subList(len / 2, len)));
+      Future<?> future1 = pool.submit(() -> doRetrieveEmails(new LinkedHashSet<>(ids.subList(0, len / 2))));
+      Future<?> future2 = pool.submit(() -> doRetrieveEmails(new LinkedHashSet<>(ids.subList(len / 2, len))));
       log.debug("Bombs away...");
       future1.get();
       future2.get();
-      log.debug("Cate mailuri am strans ?" + allEmails.size());
-      return allEmails;
+
+      CompletableFuture.supplyAsync(() -> retrieveEmails(firstHalf))
+      CompletableFuture.supplyAsync(() -> retrieveEmails(secodnHalf))
+          thenCombine(celalat -> (list1, list) -> merge si elimini duppl in 1 thread ca e in mem super fast)
+
+
+      // LIsta de allEmail aici nu contine duplicate DAR nu au fost inca verificate.
+      List<String> firstHalf = uniqueEmails.subList(0, uniqueEmails.size() / 2);
+      List<String> secondHalf = uniqueEmails.subList(uniqueEmails.size() / 2, uniqueEmails.size());
+
+      List<String> result = supplyAsync(() -> checkEmails(firstHalf), pool)
+          .thenCombineAsync(supplyAsync(() -> checkEmails(secondHalf), pool), this::contact)
+          .get();
+
+      log.debug("Cate mailuri am strans ?" + result.size());
+      return result;
    }
 
-   private final List<String> allEmails = new ArrayList<>();
+   private List<String> contact(List<String> l1, List<String> l2) {
+      List<String> list = new ArrayList<>();
+      list.addAll(l1);
+      list.addAll(l2);
+      return list;
+   }
 
-   private void doRetrieveEmails(List<Integer> idsChunk) {
+   public List<String> checkEmails(List<String> emails) {
+      return emails.stream().filter(dependency::checkEmail).collect(toList());
+   }
+
+
+   private  void doRetrieveEmails(Set<Integer> idsChunk) {
+      List<String> listaMea = new ArrayList<>();
       for (Integer id : idsChunk) {
          String email = dependency.retrieveEmail(id); // 50% overlap
-         synchronized (allEmails) {
-            if (!allEmails.contains(email)) {
-               allEmails.add(email);
+         listaMea.add(email);
+      }
+      //return listaMea; inca si mai bine
+
+      // mai bine sa te sync o sing  data per worker thread.
+      synchronized (uniqueEmails) {
+         for (String email : listaMea) {
+            if (!uniqueEmails.contains(email)) {
+                uniqueEmails.add(email);
             }
+
          }
-
-//         synchronized (allEmails) {
-//         }
-
-
-
-
-
-
-
-//         if (allEmails.contains(email)) {
-//            continue;
-//         }
-//         if (dependency.checkEmail(email))
       }
    }
 }
