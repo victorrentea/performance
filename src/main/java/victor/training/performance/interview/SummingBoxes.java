13927bc8bbf43394f4aed1828a24377d1930c60e
Victor Rentea <victorrentea@gmail.com>
2022-03-16 10:36:13 +0200

    String Builder + boxing/outboxing detectat cu butonu ala cu ceasu de sus din bara in IJ > uitandu-ne la "FLameGraph" (toolul folosit pe sub a fost Java Flight Recorded - JFR)

--- src/main/java/victor/training/performance/interview/SummingBoxes.java
+++ src/main/java/victor/training/performance/interview/SummingBoxes.java
@@ -1,29 +1,30 @@
 package victor.training.performance.interview;
 
-import java.util.List;
 import java.util.stream.LongStream;
-
-import static java.util.stream.Collectors.toList;
 
 public class SummingBoxes {
 
     public static void main(String[] args) {
-        List<Long> list = LongStream.range(1, 10_000_000)
-				.boxed()
-            .collect(toList());
+        double[] list = LongStream.range(1, 10_000_000).mapToDouble(d->d)
+				.toArray();
 
         // TODO how about memory efficiency?
 
         long t0 = System.currentTimeMillis();
 
-        Long sum = 0L;
-        for (Long i : list) {
-            sum += i;
-        }
+        Double sum = logicaMeaDeProd(list);
 
         long t1 = System.currentTimeMillis();
-        System.out.println("sum = " + sum);
+        System.out.println(sum);
         System.out.println(t1 - t0);
+    }
+
+    private static Double logicaMeaDeProd(double[] list) {
+        double sum = 0d;
+        for (double d : list) {
+            sum += d;
+        }
+        return sum;
     }
 
 }
--- src/main/java/victor/training/performance/interview/TheInfamousStringBuilderProblem.java
+++ src/main/java/victor/training/performance/interview/TheInfamousStringBuilderProblem.java
@@ -1,32 +1,42 @@
 package victor.training.performance.interview;
 
-import org.apache.commons.io.FileUtils;
+import org.jooq.lambda.Unchecked;
 import victor.training.performance.util.PerformanceUtil;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
+import java.io.*;
 import java.util.stream.IntStream;
-
-import static java.util.stream.Collectors.toList;
+import java.util.stream.Stream;
 
 public class TheInfamousStringBuilderProblem {
    public static void main(String[] args) throws IOException {
-      List<String> elements = IntStream.range(1, 50_000) // try 50K x 6 chars : see TLAB
-          .mapToObj(n -> "hahaha")
-          .collect(toList());
+      Stream<String> elements = queryDinDBCareImiDaStream();
 
+      File file = new File("out.txt");
       PerformanceUtil.waitForEnter();
-      System.out.println("Start writing contents!");
       long t0 = System.currentTimeMillis();
 
-      String s = "";
-      for (String element : elements) {
-         s += element;
+      try (Writer writer = new BufferedWriter(new FileWriter(file))){
+         elements.forEach(Unchecked.consumer(writer::write));
+
+//         for (String element : elements) {
+//            writer.write(element);
+//         }
       }
-      FileUtils.writeStringToFile(new File("out.txt"), s);
+//      StringBuilder sb = new StringBuilder();
+//         for (String element : elements) {
+//            sb.append(element);
+//         }
+//      FileUtils.writeStringToFile(file, sb.toString());
 
       System.out.println("Done. Took " + (System.currentTimeMillis() - t0));
       PerformanceUtil.waitForEnter();
+   }
+
+   private static Stream<String> queryDinDBCareImiDaStream() {
+      Stream<String> elements = IntStream.range(1, 9_000_000_00) // try 50K x 6 chars : see TLAB 5M * 12 = 60MB
+          .mapToObj(n -> "hahaha")
+//          .collect(toList())
+          ;
+      return elements;
    }
 }
