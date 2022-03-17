ae76d6c23c8499ea3ec5c883b4d2a0c49c969beb
Victor Rentea <victorrentea@gmail.com>
2022-03-17 14:50:55 +0200

    @Timed (micrometer) pentru apeluri externe sa le tii sub control

--- src/main/java/victor/training/performance/concurrency/ExternalDependency.java
+++ src/main/java/victor/training/performance/concurrency/ExternalDependency.java
@@ -2,6 +2,9 @@ package victor.training.performance.concurrency;
 
 public interface ExternalDependency {
    /** Imagine some some external call */
+//   @Timed("retrieve.email") // cauzeaza un Proxy sa intercepteze apelul si sa
+   // raporteze timpul mediu/maxim... cat a durat >>> Prometheus > Grafana >
+   // Telefonul tau alarma pe Slack cand in prod timpul max a sarit de 10s
    String retrieveEmail(int id);
 
    /** Imagine some some external call */
