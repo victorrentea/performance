e8208e75c7310bcb0762a76508418825da34577b
victorrentea <victorrentea@gmail.com>
2022-06-30 09:48:15 +0300

    add Thread Locals examples

---
+++ src/main/java/victor/training/performance/ThreadLocals.java
@@ -0,0 +1,55 @@
+package victor.training.performance;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import static victor.training.performance.util.PerformanceUtil.sleepq;
+
+@Slf4j
+public class ThreadLocals {
+    public static void main(String[] args) {
+        System.out.println("Here come 2 parallel HTTP requests");
+        ThreadLocals app = new ThreadLocals();
+        app.anHttpRequest("alice", "Alice's data");
+        app.anHttpRequest("bob", "Bob's data");
+    }
+
+    private final AController controller = new AController(new AService(new ARepo()));
+
+    public static String staticCurrentUser;
+    // TODO ThreadLocal<String>
+
+    public void anHttpRequest(String currentUser, String data) {
+        log.info("Current user is " + currentUser);
+        staticCurrentUser = currentUser;
+        // TODO pass the current user down to the repo without polluting all signatures
+        controller.create(data);
+    }
+}
+
+
+
+@RequiredArgsConstructor
+class AController {
+    private final AService aService;
+
+    public void create(String data) {
+        aService.create(data);
+    }
+}
+@RequiredArgsConstructor
+class AService {
+    private final ARepo aRepo;
+
+    public void create(String data) {
+        sleepq(10); // some delay, to reproduce the race bug
+        aRepo.save(data);
+    }
+}
+@Slf4j
+class ARepo {
+    public void save(String data) {
+        String currentUser = ThreadLocals.staticCurrentUser; // TODO How to get this?
+        log.info("INSERT INTO A(data, created_by) VALUES ({}, {})", data, currentUser);
+    }
+}
