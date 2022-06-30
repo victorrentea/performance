dbec94ad9a61372517cd8e968b1cef96f7d676dc
victorrentea <victorrentea@gmail.com>
2022-06-30 11:25:05 +0300

    Threadl locals explained

--- src/main/java/victor/training/performance/ThreadLocals.java
+++ src/main/java/victor/training/performance/ThreadLocals.java
@@ -10,29 +10,37 @@ public class ThreadLocals {
     public static void main(String[] args) {
         System.out.println("Here come 2 parallel HTTP requests");
         ThreadLocals app = new ThreadLocals();
-        app.anHttpRequest("alice", "Alice's data");
-        app.anHttpRequest("bob", "Bob's data");
+        // ThreadLocals unrely these things that you did not understand how they work yet:
+        // SecurityContextHolder,
+        // how @Transactional magically propagates to other methods called inside,
+        // Apache Sleuth,
+        // Logback MDC,
+        // why you cannot use the same JDBC conenction on 2 threads, @Scope("request")
+
+        new Thread(() ->app.anHttpRequest("alice", "Alice's data")).start();
+        new Thread(() ->app.anHttpRequest("bob", "Bob's data")).start();
     }
 
     private final AController controller = new AController(new AService(new ARepo()));
 
-    public static String staticCurrentUser;
+    public static ThreadLocal<String> staticCurrentUser = new ThreadLocal<>();
+
+    // a thread local variable is a primitive in HJava that associates some data with the current thread.
+//    Only this thread is able to read/write that value.
     // TODO ThreadLocal<String>
 
     public void anHttpRequest(String currentUser, String data) {
         log.info("Current user is " + currentUser);
-        staticCurrentUser = currentUser;
+        //the security filters before the controller they know the current uyser
+        staticCurrentUser.set(currentUser);
         // TODO pass the current user down to the repo without polluting all signatures
         controller.create(data);
     }
 }
 
-
-
 @RequiredArgsConstructor
 class AController {
     private final AService aService;
-
     public void create(String data) {
         aService.create(data);
     }
@@ -40,7 +48,6 @@ class AController {
 @RequiredArgsConstructor
 class AService {
     private final ARepo aRepo;
-
     public void create(String data) {
         sleepq(10); // some delay, to reproduce the race bug
         aRepo.save(data);
@@ -49,7 +56,7 @@ class AService {
 @Slf4j
 class ARepo {
     public void save(String data) {
-        String currentUser = ThreadLocals.staticCurrentUser; // TODO How to get this?
+        String currentUser = ThreadLocals.staticCurrentUser.get(); // TODO How to get this?
         log.info("INSERT INTO A(data, created_by) VALUES ({}, {})", data, currentUser);
     }
 }
