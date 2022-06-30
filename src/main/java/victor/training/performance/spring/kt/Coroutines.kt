dd736fe73c7aa3a27c7fbec3341e38605f14b8db
victorrentea <victorrentea@gmail.com>
2022-06-30 13:06:13 +0300

    yolo

--- src/main/java/victor/training/performance/spring/kt/Coroutines.kt
+++ src/main/java/victor/training/performance/spring/kt/Coroutines.kt
@@ -4,19 +4,21 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-suspend fun heavyIO() {
+suspend fun myFlow() {
+    heavyIO()
+}
+
+private suspend fun heavyIO() {
     println("Call network")
-    java.lang.RuntimeException("Before").printStackTrace()
     delay(1000)
-    java.lang.RuntimeException("After").printStackTrace() // different call stack
     println("DONE network")
 }
 
 fun main(args: Array<String>) {
     GlobalScope.launch {
         println("Start")
-        heavyIO()
-        heavyIO()
+        myFlow()
+        myFlow()
         println("Done")
         java.lang.RuntimeException().printStackTrace()
     }
