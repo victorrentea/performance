c9f5b5c8fc4838a53b70b3a8a5a595fc00f0a13f
victorrentea <victorrentea@gmail.com>
2022-07-18 17:21:44 +0300

    nu face retea in parallel stream

--- src/main/java/victor/training/performance/concurrency/RaceBugs.java
+++ src/main/java/victor/training/performance/concurrency/RaceBugs.java
@@ -2,14 +2,14 @@ package victor.training.performance.concurrency;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.stream.Collectors.toList;
 
@@ -28,7 +28,7 @@ public class RaceBugs {
    private final Map<String, Boolean> emailValidity = new HashMap<>();
 
    // chemata pe 2 threaduri, fiecare cu cate 10k de id-uri
-   private void doRetrieveEmails(List<Integer> idsChunk) {
+   private void doRetrieveEmailsCuDeToate(List<Integer> idsChunk) {
       for (Integer id : idsChunk) {
          String email = dependency.retrieveEmail(id);
          synchronized (allEmails) {
@@ -46,6 +46,23 @@ public class RaceBugs {
       }
    }
 
+   private List<String> retrieveEmails(List<Integer> idsChunk) { // pe 2 threaduri
+      return idsChunk.stream().map(dependency::retrieveEmail).collect(toList());
+   }
+
+   private List<String> eliminateDuplicates(List<String> toateEmailurile) {
+      List<String> uniqueEmails = new ArrayList<>();
+      for (String email : toateEmailurile) {
+         if (uniqueEmails.stream().noneMatch(e -> e.equalsIgnoreCase(email)))
+            uniqueEmails.add(email);
+      }
+      return uniqueEmails;
+   }
+
+   private List<String> validateEmailChunk(List<String> jumate) { // pe 2 threaduri
+      return jumate.stream().filter(dependency::isEmailValid).collect(toList());
+   }
+
    public static void main(String[] args) throws Exception {
       ExternalDependencyFake dependency = new ExternalDependencyFake(20_000);
       List<Integer> ids = IntStream.range(0, 20_000).boxed().collect(toList());
@@ -61,16 +78,40 @@ public class RaceBugs {
       List<Integer> firstHalf = ids.subList(0, ids.size() / 2);
       List<Integer> secondHalf = ids.subList(ids.size() / 2, ids.size());
 
-      // submit the 2 tasks
-      ExecutorService pool = new ThreadPoolExecutor(0, Integer.MAX_VALUE, 500, MILLISECONDS, new SynchronousQueue<>());
-      Future<?> future1 = pool.submit(() -> doRetrieveEmails(firstHalf));
-      Future<?> future2 = pool.submit(() -> doRetrieveEmails(secondHalf));
-      log.debug("Tasks launched...");
 
-      // wait for the tasks to complete
-      future1.get();
-      future2.get();
-      return allEmails;
+      CompletableFuture<List<String>> futureEmails1 = supplyAsync(() -> retrieveEmails(firstHalf));
+      CompletableFuture<List<String>> futureEmails2 = supplyAsync(() -> retrieveEmails(secondHalf));
+
+      CompletableFuture<List<String>> uniqueEmails = futureEmails1.thenCombine(futureEmails2,
+                      (e1, e2) -> concatLists(e1, e2))
+              .thenApply(this::eliminateDuplicates);
+
+      List<String> nevalidate = uniqueEmails.get();
+
+      // ruleaza pe N_CPU-1 thjreaduri, in commonPool global per JVM
+      // PERICOL: 1: sunt prea multe (eu voiam 2)
+      // PERICOL: 2: Thread pool starvation
+
+      return nevalidate.parallelStream().filter(email -> {
+         log.info("Validez " + email);
+         return dependency.isEmailValid(email); // EVITI sa faci IO pe commonPool, pentru ca il poti starva
+      }).collect(toList());
+
+      // submit the 2 tasks
+//      ExecutorService pool = new ThreadPoolExecutor(0, Integer.MAX_VALUE, 500, MILLISECONDS, new SynchronousQueue<>());
+//      Future<?> future1 = pool.submit(() -> doRetrieveEmailsCuDeToate(firstHalf));
+//      Future<?> future2 = pool.submit(() -> doRetrieveEmailsCuDeToate(secondHalf));
+//      log.debug("Tasks launched...");
+//
+//      // wait for the tasks to complete
+//      future1.get();
+//      future2.get();
+//      return allEmails;
+   }
+
+   @NotNull
+   private List<String> concatLists(List<String> e1, List<String> e2) {
+      return Stream.concat(e1.stream(), e2.stream()).collect(toList());
    }
    //endregion
 
