f5993a9114bc0797e96cb12618174398e8a5976a
victorrentea <victorrentea@gmail.com>
2022-07-20 17:33:03 +0300

    END

--- src/main/java/victor/training/performance/interview/SummingBoxes.java
+++ src/main/java/victor/training/performance/interview/SummingBoxes.java
@@ -1,6 +1,8 @@
 package victor.training.performance.interview;
 
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.stream.LongStream;
@@ -8,6 +10,8 @@ import java.util.stream.LongStream;
 import static java.util.stream.Collectors.toList;
 
 public class SummingBoxes {
+
+    private static final Logger log = LoggerFactory.getLogger(SummingBoxes.class);
 
     public static void main(String[] args) {
         List<Long> list = LongStream.range(1, 100_000_000)
@@ -22,15 +26,17 @@ public class SummingBoxes {
 
         long t1 = System.currentTimeMillis();
         System.out.println("sum = " + sum);
+
+        log.debug("a {}", list);
         System.out.println(t1 - t0);
     }
 
     @NotNull
     private static Long deStudiat(List<Long> list) {
-        long sum = 0L;
+        Long sum = 0L;
         for (Long i : list) {
-            sum += i;
-//            sum = Long.valueOf(sum.longValue() + i);
+//            sum += i;
+            sum = Long.valueOf(sum.longValue() + i);
         }
         return sum;
     }
