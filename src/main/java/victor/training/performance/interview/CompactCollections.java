147407f56155f403f07db33fb72f7831d6c60698
Victor Rentea <victorrentea@gmail.com>
2022-10-20 17:19:06 +0300

    Evitat th pool starvation de tomcat decupand afara munca care tinea timp pe al thread. nu o sa patesti 300 req SIMULTANE.

--- src/main/java/victor/training/performance/interview/CompactCollections.java
+++ src/main/java/victor/training/performance/interview/CompactCollections.java
@@ -2,8 +2,11 @@ package victor.training.performance.interview;
 
 import victor.training.performance.util.PerformanceUtil;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 public class CompactCollections {
@@ -12,13 +15,15 @@ public class CompactCollections {
       // Use-case: I had to keep a large number of IDs throughout a long memory-intensive batch
       long heap0 = PerformanceUtil.getUsedHeapBytes();
 
-      Set<Long> ids = LongStream.range(0, 1_000_000).boxed().collect(Collectors.toSet());
-      // HashSet ..  MB because
-      // ArrayList .. MB because
-      // Long[] .. MB because
-      // long[] .. MB because
-      // int[] .. MB because
-      // LinkedList .. MB because
+      LinkedList<Long> list = LongStream.range(0, 1_000_000).boxed().collect(Collectors.toCollection(LinkedList::new));
+//      new Long[]
+      // HashSet 63  MB because galeti arbori liste inlantuite
+      // LinkedList 37 MB because
+      // ArrayList 36 MB because
+      // Long[] 44 MB because WTF? nu stiu de studiat
+      // long[] 8 MB because
+      // int[] 4 MB because
+      int[] patrat; // iarta0ma da ocupa de 10 ori mai putin decat un Set<Long>
       System.out.println("Allocated!");
 
       long heap1 = PerformanceUtil.getUsedHeapBytes();
