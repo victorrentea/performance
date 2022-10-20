bff349e70ac8e144869c7b54631dcb8f61ab5472
Victor Rentea <victorrentea@gmail.com>
2022-10-20 15:19:06 +0300

    Gill's story  :) #2

--- src/main/java/victor/training/performance/primitives/InchidereMagazin.java
+++ src/main/java/victor/training/performance/primitives/InchidereMagazin.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeoutException;
 
 import static java.util.concurrent.CompletableFuture.delayedExecutor;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
@@ -24,19 +25,26 @@ public class InchidereMagazin {
     }
 
     private CompletableFuture<String> userInputFuture = new CompletableFuture<>();
-    private CompletableFuture<String> closePromise;
-
 
     @PostConstruct
     public void laStartup() {
         log.info("Fire in the hole CS");
-        CompletableFuture.anyOf(
-                        supplyAsync(() -> "T", delayedExecutor(5, SECONDS)),
-                        userInputFuture
-                )
-                .thenAccept(status -> dupa((String) status));
+//        CompletableFuture.anyOf(
+//                        supplyAsync(() -> "TimeoutDefault", delayedExecutor(5, SECONDS)), // emit oricum dupa 5 sec asta
+//                        userInputFuture
+//                )
+//                .thenAccept(status -> dupa((String) status));
+//
+//
+        userInputFuture.orTimeout(5, SECONDS).exceptionally(e -> {
+            log.info("t: ", e);
+            if (e instanceof TimeoutException) {
+                return "TimeoutDefault";
+            }
+            throw new RuntimeException(e);
+        })
+                .thenAccept(this::dupa);
     }
-
     //indiferent daca a dat timeout(status="T") sau nu (status=event).
     public void dupa(String status) {
         userInputFuture.cancel(true);
