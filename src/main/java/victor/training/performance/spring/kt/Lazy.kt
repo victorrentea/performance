23fb32f59928ab4a5e904cbe693267e0987710b8
Victor Rentea <victorrentea@gmail.com>
2022-10-31 19:46:51 +0200

    commet KT

--- src/main/java/victor/training/performance/spring/kt/Lazy.kt
+++ src/main/java/victor/training/performance/spring/kt/Lazy.kt
@@ -1,20 +1,20 @@
-package victor.training.performance.spring.kt
-
-class Lazy {
-//    var _manualLazyInt: Int? = null
-//    val manualLazyInt: Int
-//        get() {
-//            // TODO
-//        }
-
-    val autoLazyInt by lazy {
-        println("Retrieve now!")
-        574
-    }
-}
-
-fun main() {
-    val lazy = Lazy()
-    println(lazy.autoLazyInt)
-    println(lazy.autoLazyInt)
-}
+//package victor.training.performance.spring.kt
+//
+//class Lazy {
+////    var _manualLazyInt: Int? = null
+////    val manualLazyInt: Int
+////        get() {
+////            // TODO
+////        }
+//
+//    val autoLazyInt by lazy {
+//        println("Retrieve now!")
+//        574
+//    }
+//}
+//
+//fun main() {
+//    val lazy = Lazy()
+//    println(lazy.autoLazyInt)
+//    println(lazy.autoLazyInt)
+//}
