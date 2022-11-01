fd30bc92efd6e59aab19314963e6277533186062
Victor Rentea <victorrentea@gmail.com>
2022-11-01 14:10:27 +0200

    magie tata

--- src/main/java/victor/training/performance/spring/BarService.java
+++ src/main/java/victor/training/performance/spring/BarService.java
@@ -1,6 +1,7 @@
 package victor.training.performance.spring;
 
 
+import ch.qos.logback.classic.util.LogbackMDCAdapter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.AllArgsConstructor;
@@ -12,10 +13,12 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.jooq.lambda.Unchecked;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskDecorator;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
@@ -26,6 +29,7 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.AsyncContext;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 import java.util.concurrent.*;
 
 import static java.lang.System.currentTimeMillis;
@@ -43,7 +47,7 @@ public class BarService {
     // apelul de metoda si sa ti-l ruleze pe al t thread !!!!!!\
 
     // niciodata asa: ci cu ThreadPoolTaskExecutor de sprign va rog !
-//    ExecutorService threadPool = Executors.newFixedThreadPool(2); // nu aloci un thread pool la fiecare req ci partajezi cu fratii
+    //    ExecutorService threadPool = Executors.newFixedThreadPool(2); // nu aloci un thread pool la fiecare req ci partajezi cu fratii
     @Autowired
     ThreadPoolTaskExecutor threadPool;
 
@@ -56,12 +60,9 @@ public class BarService {
         useruCurentDinSpring.set(username);
         log.debug("Requesting drinks cui: {}...", barman.getClass());
         long t0 = currentTimeMillis();
-functieCareIaDepeUnThreadLocal();
-        CompletableFuture<Beer> futureBeer = barman.pourBeer()
-                // tu cand chemi pourBeer ea nu incepe executia ATUNCI pe loc, ci
-                // mai tarziu intr-un alt thread. Junioru e in soc anafilactic/spasme
-                // cine face asta?! Proxy-ul
+        functieCareIaDepeUnThreadLocal();
 
+        CompletableFuture<Beer> futureBeer = barman.pourBeer()
                 .exceptionally(e -> {
                     if (e.getCause() instanceof IllegalStateException)
                         return new Beer("bruna");
@@ -72,13 +73,13 @@ functieCareIaDepeUnThreadLocal();
                 ;
 
         CompletableFuture<DillyDilly> futureDilly =
-                futureBeer.thenCombine(futureVodka, (beer, vodka) -> {
+                futureBeer.thenCombineAsync(futureVodka, (beer, vodka) -> {
                     log.info("Amestec Dilly pt : " + useruCurentDinSpring.get());
                     return new DillyDilly(beer, vodka);
-                });
+                }, threadPool);
         long t1 = currentTimeMillis();
 
-//       barman.injur("^*!%$!@^$%*!!("); // Springu: lasa-ma pe mine. ai Incredere in MINE!!
+        //       barman.injur("^*!%$!@^$%*!!("); // Springu: lasa-ma pe mine. ai Incredere in MINE!!
         log.debug("Ajung in patuc?");
 
         log.debug("Threadul Tomcatului scapa de req asta in {} ms", t1 - t0);
@@ -91,7 +92,6 @@ functieCareIaDepeUnThreadLocal();
 
 
     // cine scrie catre client rezultatul efectiv pe HTTP response?
-
 
 
     //<editor-fold desc="History Lesson: Async Servlets 10 ani din Servet 3.0">
@@ -142,7 +142,7 @@ class IntercepteazaMetodeCeReturneazaCF {
         returnedFuture.thenRun(() -> {
             long t1 = currentTimeMillis();
             meterRegistry.timer(pjp.getSignature().getName()).record(t1 - t0, MILLISECONDS);
-            log.info("CF returnat de " + pjp.getSignature().getName() +" s-a terminat in " + (t1-t0) + " ms");
+            log.info("CF returnat de " + pjp.getSignature().getName() + " s-a terminat in " + (t1 - t0) + " ms");
         });
         return returnedFuture;
     }
@@ -151,8 +151,8 @@ class IntercepteazaMetodeCeReturneazaCF {
 @Slf4j
 @lombok.Value
 class DillyDilly {
-     Beer beer;
-     Vodka vodka;
+    Beer beer;
+    Vodka vodka;
 
     public DillyDilly(Beer beer, Vodka vodka) {
         this.beer = beer;
@@ -166,35 +166,40 @@ class DillyDilly {
 class Barman {
 
     private RestTemplate restTemplate = new RestTemplate();
-// @Async pe metoda e rau pentru ca presupune ca blochezi threaduri inautru. Ori tu, om destept, nu faci asta, ci folosesti drivere/clienti reactivi/nonblocanti ca sa-ti faci IO
+    @Autowired
+    private ThreadPoolTaskExecutor threadPool;
+
+    // @Async pe metoda e rau pentru ca presupune ca blochezi threaduri inautru. Ori tu, om destept, nu faci asta, ci folosesti drivere/clienti reactivi/nonblocanti ca sa-ti faci IO
     public CompletableFuture<Beer> pourBeer() { // dureze timp!
         log.debug("Pouring Beer...");
-
-// RAU F RAU pemntru ca starvez commonPool: blocand unul din ce le N-1 (la mine 9) threaduri cu I/O
-//        CompletableFuture<Beer> futureBeer = supplyAsync(() ->
-//                restTemplate.getForObject("http://localhost:9999/api/beer", Beer.class));
+        // RAU F RAU pemntru ca starvez commonPool: blocand unul din ce le N-1 (la mine 9) threaduri cu I/O
+        //        CompletableFuture<Beer> futureBeer = supplyAsync(() ->
+        //                restTemplate.getForObject("http://localhost:9999/api/beer", Beer.class));
 
         //1) fitza: WebClient.....toFuture()
         //2) stilu vechi AsyncRestTemplate
         // driver de DB: https://github.com/aerospike/aerospike-client-java-reactive
         // Maria: https://mariadb.com/docs/connect/programming-languages/java-r2dbc/
-//        CompletableFuture<Beer> futureBeer =
-//                new AsyncRestTemplate().getForEntity("http://localhost:9999/api/beer", Beer.class)
-//                .completable()
-//                .thenApply(HttpEntity::getBody);
+        //        CompletableFuture<Beer> futureBeer =
+        //                new AsyncRestTemplate().getForEntity("http://localhost:9999/api/beer", Beer.class)
+        //                .completable()
+        //                .thenApply(HttpEntity::getBody);
 
         // mai bun, asa e la voi
-//        CompletableFuture<Beer> futureBeer = WebClient.create().get().url("http://localhost:9999/api/beer")...toFuture();
-
-        CompletableFuture<Beer> futureBeer = supplyAsync(() -> new Beer("blonda"),
-                delayedExecutor(1, SECONDS))
+        //        CompletableFuture<Beer> futureBeer = WebClient.create().get().url("http://localhost:9999/api/beer")...toFuture();
+        CompletableFuture<Beer> futureBeer = supplyAsync(() -> {
+                    log.info("Cui torn bere? "+ BarService.useruCurentDinSpring.get());
+                    return new Beer("blonda");
+                },
+                threadPool)
+//                delayedExecutor(1, SECONDS, threadPool)
                 ;
-        futureBeer.thenAccept( b-> log.debug("Beer done: " +b));
+        futureBeer.thenAccept(b -> log.debug("Beer done: " + b));
         return futureBeer;
     }
 
     public Vodka pourVodka() {
-//        new Exception().printStackTrace();
+        //        new Exception().printStackTrace();
 
         log.debug("Pouring Vodka...");
         sleepMillis(1000); // long query maria DB conn ai uitat sa pui
@@ -204,7 +209,7 @@ class Barman {
         return new Vodka();
     }
 
-//    public Vodka addIce(Vodka vodka) { // in0memory instantaneous tranformation
+    //    public Vodka addIce(Vodka vodka) { // in0memory instantaneous tranformation
     public CompletableFuture<Vodka> addIce(Vodka vodka) { // NETWORK call (alt api call)
         return supplyAsync(() -> {
             vodka.setIce(true);
@@ -213,12 +218,12 @@ class Barman {
     }
 
 
-//    @Transactional
-//    @Retryable
-//    @Cacheable
-//    @PreAuthorized
-//    @Secured
-//    @Timed
+    //    @Transactional
+    //    @Retryable
+    //    @Cacheable
+    //    @PreAuthorized
+    //    @Secured
+    //    @Timed
     @Async // asta il face pe Spring sa logeze automat orice eroare apare in fct asta ?
     // dece ? pentru ca stie sigur ca tu NU AI CUM sa mai vezi eroare (intrucat nu returnezi CF<VOid> sau atlceva,..)
     public void injur(String uratura) {
@@ -243,6 +248,7 @@ class Vodka {
 }
 
 @Configuration
+@Slf4j
 class BarConfig {
     //<editor-fold desc="Custom thread pool">
     @Bean // defineste un bean spring numit "barPool" de tip ThreadPoolTaskExecutor
@@ -252,7 +258,23 @@ class BarConfig {
         executor.setMaxPoolSize(n);
         executor.setQueueCapacity(500);
         executor.setThreadNamePrefix("barman-");
-//        executor.setTaskDecorator(new MonitorQueueWaitingTime(meterRegistry.timer("barman-queue-time")));
+        executor.setTaskDecorator(new TaskDecorator() {
+            @Override
+            public Runnable decorate(Runnable runnable) {
+                // aici sunt pe threadul submitterului
+                String closureIntroVar = BarService.useruCurentDinSpring.get();
+                log.info("Gasesc aici useuru? " + closureIntroVar);
+                Map<String, String> map = MDC.getCopyOfContextMap();
+                return () -> {
+                    if (map!=null)
+                        MDC.setContextMap(map);
+                    BarService.useruCurentDinSpring.set(closureIntroVar);
+                    // aici sunt in threadul executor din pool
+                    runnable.run();
+                };
+            }
+        });
+        //        executor.setTaskDecorator(new MonitorQueueWaitingTime(meterRegistry.timer("barman-queue-time")));
         executor.initialize();
         return executor;
     }
