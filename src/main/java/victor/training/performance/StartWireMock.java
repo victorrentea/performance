ea55489e72debf6a0fa4a671b824b8e2b98e1083
Victor Rentea <victorrentea@gmail.com>
2022-11-01 10:32:39 +0200

    yolo

--- src/main/java/victor/training/performance/StartWireMock.java
+++ src/main/java/victor/training/performance/StartWireMock.java
@@ -7,9 +7,10 @@ import java.io.File;
 public class StartWireMock {
    public static void main(String[] args) {
       WireMockServerRunner.main(
-          "--port", "9999",
-          "--root-dir", new File(".", "src/test/resources").getAbsolutePath(),
-          "--global-response-templating" // UUID
+              "--port", "9999",
+              "--root-dir", new File(".", "src/test/resources").getAbsolutePath(),
+              "--global-response-templating", // UUID
+              "--async-response-enabled=true" // enable Wiremock to not bottleneck on heavy load
       );
    }
 }
