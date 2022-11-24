3ea8bba91c39e220b50076bf687d3a5bf105a215
Victor Rentea <victorrentea@gmail.com>
2022-11-24 12:35:24 +0200

    RACE BUG ca scriu pe aceasi variabila in mem din 2+ threaduri
    can REST API

--- src/main/java/victor/training/performance/ThreadLocalIntro.java
+++ src/main/java/victor/training/performance/ThreadLocalIntro.java
@@ -4,16 +4,22 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static victor.training.performance.util.PerformanceUtil.sleepMillis;
+
+// totul e o joaca
+
 
 @Slf4j
 public class ThreadLocalIntro {
     public static void main(String[] args) {
         System.out.println("Here come 2 parallel HTTP requests");
         ThreadLocalIntro app = new ThreadLocalIntro();
-        app.httpRequest("alice", "Alice's data");
+
+        new Thread(()->app.httpRequest("alice", "Alice's data")).start();
+        new Thread(()->app.httpRequest("bob", "Bob's data")).start();
 //        app.frameworkReceivesRequest("bob", "Bob's data");
     }
 
@@ -22,10 +28,10 @@ public class ThreadLocalIntro {
     public static String staticCurrentUser;
     // TODO ThreadLocal<String>
 
-    // inside Spring, JavaEE,..
+    // inside Spring, JavaEE,.. Filter web JWT OAuth AT  { subject: "jdoe" }
     public void httpRequest(String currentUser, String data) {
         log.info("Current user is " + currentUser);
-//        staticCurrentUser = currentUser;
+        staticCurrentUser = currentUser;
         // TODO pass the current user down to the repo WITHOUT polluting all signatures
         controller.create(data);
     }
@@ -36,8 +42,10 @@ public class ThreadLocalIntro {
 @RestController
 @RequiredArgsConstructor
 class AController {
-    private final AService aService;
+//    public String staticCurrentUser; // NICIODATA nu stii date mutabile de request pe campurile beanurilor spring
 
+    private final AService aService;
+//@GetMapping
     public void create(String data) {
         aService.create(data);
     }
@@ -60,7 +68,8 @@ class AService {
 @Slf4j
 class ARepo {
     public void save(String data) {
-        String currentUser = "TODO"; // TODO Where to get this from?
+        String currentUser = ThreadLocalIntro.staticCurrentUser; // TODO Where to get this from?
+//        SecurityContextHolder.getContext().getAuthentication().getName();
         log.info("INSERT INTO A(data, created_by) VALUES ({}, {})", data, currentUser);
     }
 }
