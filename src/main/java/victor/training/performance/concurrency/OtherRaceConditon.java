f03a24adcef38f466b7e256e2c5c83a972f43600
Victor Rentea <victorrentea@gmail.com>
2023-01-30 17:41:30 +0200

    END of day 1

--- src/main/java/victor/training/performance/concurrency/OtherRaceConditon.java
+++ src/main/java/victor/training/performance/concurrency/OtherRaceConditon.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 
 class Credentials {
 }
@@ -14,20 +15,34 @@ class Credentials {
 public class OtherRaceConditon {
 
   private List<String> lastSearches = new ArrayList<>();
-  private Map<String, Credentials> map = new HashMap<>();
+  private Map<String, Credentials> map = Collections.synchronizedMap(new HashMap<>()); // never postpones data, with the price of us blocking
+
+
+//  private Map<String, Credentials> onlyUseForCaches = new ConcurrentHashMap<>(); // inconsistent in theory
+  // DO NOT EVER IMPLEMENT YOUR OWN CACHING IN YOUR BACKYARD. NEVER. VERY RISKY:
+  // caffeine, ehcache, memcache, hazelcast, redis,
+  // - OOM
+  // - eviction (TTL)
+  // - monitorign (size in Grafana)
+  // - concurrency
+  // - disk offloading
+  // - LRU, MRU (eviction policy when it's full) (max size reach)
+  // - distributed caching
+
+
 
   @GetMapping
+  // imagine 2 req with the same user jdoe
   public void getMapping(String user) {
-    String u = UUID.randomUUID().toString();
-    Credentials data = fetchCredetialsForUser(u);
-    synchronized (map) {
-      map.put(user, data); // 200 ms
-    }
+    Credentials data = fetchCredetialsForUser(user);
+//    map.put(user, data); // 200 ms
 
     CompletableFuture.runAsync(() -> {
-      synchronized (map) {
-        Credentials remove = map.remove(user);
-      }
+      System.out.println("pasa data in plain sigth arg" + data);
+//        Credentials remove = map.remove(user); // the first async task running this line
+      // removes jdoe
+
+      // the secodn task gets NULL from map
     });
   }
 
