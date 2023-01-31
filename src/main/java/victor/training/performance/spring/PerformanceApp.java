99a110fe57b7a5176cfd2328179b2488fdcdc22f
Victor Rentea <victorrentea@gmail.com>
2023-01-31 22:51:17 +0200

    glowroot cleanup

--- src/main/java/victor/training/performance/spring/PerformanceApp.java
+++ src/main/java/victor/training/performance/spring/PerformanceApp.java
@@ -6,12 +6,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableAsync;
 import victor.training.performance.jpa.JpaPerfApplication;
+import victor.training.performance.util.GlowrootUtil;
+
+import javax.annotation.PreDestroy;
+import java.io.IOException;
 
 @Slf4j
 @EnableAsync
@@ -30,7 +35,13 @@ public class PerformanceApp {
     public void onStart(ApplicationReadyEvent event) {
         long t1 = System.currentTimeMillis();
 
-        log.info("🌟🌟🌟🌟🌟🌟 PerformanceApp Started in {} seconds 🌟🌟🌟🌟🌟🌟", (t1-t0)/1000);
+        log.info("🌟🌟🌟🌟🌟🌟 PerformanceApp St arted in {} seconds 🌟🌟🌟🌟🌟🌟", (t1-t0)/1000);
+    }
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void deleteGlowrootDatabase() throws IOException {
+        // otherwise Glowroot preserves its data over a restart
+        GlowrootUtil.deleteDatabase();
     }
 
     public static void main(String[] args) {
