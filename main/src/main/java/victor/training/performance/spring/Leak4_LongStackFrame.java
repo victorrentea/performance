7eeb8beec4539220bb05e7696afe0b2ba8efb6fd
Victor Rentea <victorrentea@gmail.com>
2023-04-06 13:12:15 +0300

    leak fixed

--- main/src/main/java/victor/training/performance/spring/Leak4_LongStackFrame.java
+++ main/src/main/java/victor/training/performance/spring/Leak4_LongStackFrame.java
@@ -15,20 +15,25 @@ import static victor.training.performance.util.PerformanceUtil.sleepMillis;
 public class Leak4_LongStackFrame {
 	@GetMapping
 	public String longRunningFunction() {
-		String useful = adapterDesignPattern_da_mi_doar_cemi_trebuie();
+//		String useful = adapterDesignPattern_da_mi_doar_cemi_trebuie();
 		// 🛑 don't reference large objects longer than needed
 
-		businessLogicComplicat3sec(useful);
+		businessLogicComplicat3sec(apiCall().getInterestingPart());
+		// 1) apiCall() is called ce intoarce ob de 80MB
+		// 2) se apeleaza getInterestingPart() pe ob de 80MB
+		// 3) se ia acel string si se da parametru la businessLogicComplicat3sec
+		// de acum incolo nimeni nu mai are POINTER la ob de 80MB
+
 		return "end";
 	}
 
-	private static String adapterDesignPattern_da_mi_doar_cemi_trebuie() {
-		BigObject80MB big = apiCall();
-		String useful = big.getInterestingPart();
-		System.gc(); // NEVER 1: ca forteaza un Full GC d-ala de sta 100ms, si 2: JVM poate sa-l ignore oricum
-		//		big = null; // periculos pt ca cineva o poate sterge "gunoi!"
-		return useful;
-	}
+//	private static String adapterDesignPattern_da_mi_doar_cemi_trebuie() {
+//		BigObject80MB big = apiCall();
+//		String useful = big.getInterestingPart();
+//		System.gc(); // NEVER 1: ca forteaza un Full GC d-ala de sta 100ms, si 2: JVM poate sa-l ignore oricum
+//		//		big = null; // periculos pt ca cineva o poate sterge "gunoi!"
+//		return useful;
+//	}
 
 	private static void businessLogicComplicat3sec(String useful) {
 		sleepMillis(10_000); // start a long-running process (eg 20 minutes)
