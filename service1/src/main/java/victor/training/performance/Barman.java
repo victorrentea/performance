ac0b35f36463599a3a8aa962115412c60cb1bcf1
Victor Rentea <victorrentea@gmail.com>
2023-06-26 16:11:19 +0300

    NU FOLOSI Executors in Spring vreodata. e riscant. eg shutdown

--- service1/src/main/java/victor/training/performance/Barman.java
+++ service1/src/main/java/victor/training/performance/Barman.java
@@ -9,6 +9,11 @@ import victor.training.performance.drinks.Beer;
 import victor.training.performance.drinks.DillyDilly;
 import victor.training.performance.drinks.Vodka;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import static java.lang.System.currentTimeMillis;
 
 @RestController
@@ -18,15 +23,30 @@ public class Barman {
   private RestTemplate rest;
 
   @GetMapping("/drink")
-  public DillyDilly drink() {
+  public DillyDilly drink() throws ExecutionException, InterruptedException {
     long t0 = currentTimeMillis();
 
     //  🛑 independent tasks executed sequentially ~> parallelize
-    Beer beer = rest.getForObject("http://localhost:9999/beer", Beer.class);
-    Vodka vodka = rest.getForObject("http://localhost:9999/vodka", Vodka.class);
+    ExecutorService threadPool = Executors.newFixedThreadPool(2);
+
+    Future<Beer> futureBeer = threadPool.submit(() -> fetchBeer());
+    Future<Vodka> futureVodka = threadPool.submit(() -> fetchVodka());
+
+    Beer beer = futureBeer.get(); // blochez threadul Tomcat 1 sec
+    Vodka vodka = futureVodka.get(); // blochez threadul Tomcat 0 sec, ca deja e gata vodka cand berea e turnata
+
+    threadPool.shutdown();
 
     long t1 = currentTimeMillis();
     log.info("HTTP thread blocked for millis: " + (t1 - t0));
     return new DillyDilly(beer,vodka);
+  }
+
+  private Vodka fetchVodka() {
+    return rest.getForObject("http://localhost:9999/vodka", Vodka.class);
+  }
+
+  private Beer fetchBeer() {
+    return rest.getForObject("http://localhost:9999/beer", Beer.class);
   }
 }
