4d7c7b763353602476c5563df941a521c63e00b4
Victor Rentea <victorrentea@gmail.com>
2023-06-27 11:53:58 +0300

    modu preferat: @Query dedicat sa vad clar ce incarc si unde

--- jpa/src/main/java/victor/training/performance/jpa/Parent.java
+++ jpa/src/main/java/victor/training/performance/jpa/Parent.java
@@ -20,7 +20,7 @@ public class Parent {
    private Integer age;
 
    @OneToMany(mappedBy = "parent", cascade = CascadeType.PERSIST/*, fetch = FetchType.EAGER*/)
-   @BatchSize(size=10) // Hibernate magic care-i spune sa incarce copiii a max 10 parinti odata
+//   @BatchSize(size=10) // Hibernate magic care-i spune sa incarce copiii a max 10 parinti odata
    // pt caz de optimizat app legacy cu minim risc, atunci e bun asta
    // rau: nisa = nu e JPA, magie grea (cum merge? ce parinti incarca in avans), inca exista LAZY LOADING
    private Set<Child> children = new HashSet<>(); // hibernate pune PersistentSet/Bag care lazy-loadeaza colectia
