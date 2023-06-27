2133ed245eded600e423335c920ded1d432f7a9d
Victor Rentea <victorrentea@gmail.com>
2023-06-27 18:26:03 +0300

    upgrade

--- jpa/src/main/java/victor/training/performance/jpa/ScopeEnumConverter.java
+++ jpa/src/main/java/victor/training/performance/jpa/ScopeEnumConverter.java
@@ -5,13 +5,19 @@ import javax.persistence.Converter;
 
 @Converter
 public class ScopeEnumConverter implements AttributeConverter<ScopeEnum, String> {
-   @Override
-   public String convertToDatabaseColumn(ScopeEnum attribute) {
-      return attribute.dbCode;
-   }
+  @Override
+  public String convertToDatabaseColumn(ScopeEnum attribute) {
+    if (attribute == null) {
+       return null;
+    }
+    return attribute.dbCode;
+  }
 
-   @Override
-   public ScopeEnum convertToEntityAttribute(String dbCode) {
-      return ScopeEnum.fromDbCode(dbCode);
-   }
+  @Override
+  public ScopeEnum convertToEntityAttribute(String dbCode) {
+    if (dbCode == null) {
+      return null;
+    }
+    return ScopeEnum.fromDbCode(dbCode);
+  }
 }
