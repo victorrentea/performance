49e2c34703e6158a98734bf204c019769bfaa8e3
Victor Rentea <victorrentea@gmail.com>
2023-09-27 19:56:37 +0300

    Revert "traditinally passing a extra param throughout the entire app"
    This reverts commit a30c123b68ee8ca9fbb3b85ec0fa6070db63fea7.

--- main/src/main/java/victor/training/performance/ThreadLocalIntro.java
+++ main/src/main/java/victor/training/performance/ThreadLocalIntro.java
@@ -18,8 +18,8 @@ public class ThreadLocalIntro {
     }
 
     public void httpRequest(String currentUser, String data) {
-        log.info("Current user is read from the HTTP Header Bearer (JWT) " + currentUser);
-        controller.create(data, currentUser);
+        log.info("Current user is " + currentUser);
+        controller.create(data);
     }
     public static String staticCurrentUser;
 }
@@ -31,8 +31,8 @@ public class ThreadLocalIntro {
 class AController {
     private final AService service;
 
-    public void create(String data, String username) {
-        service.create(data, username);
+    public void create(String data) {
+        service.create(data);
     }
 }
 
@@ -42,15 +42,9 @@ class AController {
 class AService {
     private final ARepo repo;
 
-    // what is Thread Local used in practice for: metadata:
-    // - SecurityContext, @CreatedBy JPA annotation
-    // - @Transactional + JDBC Connection
-    // - Logback MDC (stuff to appear on every log line, set via MDC.set("prop",value")
-    // - TraceID/correlationId
-    // - TenantId
-    public void create(String data, String username) {
+    public void create(String data) {
         sleepMillis(10); // some delay, to reproduce the race bug
-        repo.save(data, username);
+        repo.save(data);
     }
 }
 
@@ -58,8 +52,8 @@ class AService {
 @Repository
 @Slf4j
 class ARepo {
-    public void save(String data, String username) {
-        String currentUser = username;
+    public void save(String data) {
+        String currentUser = "TODO"; // TODO
         log.info("INSERT INTO A(data, created_by) VALUES ({}, {})", data, currentUser);
     }
 }
