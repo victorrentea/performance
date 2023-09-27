53a8d91644436659c42f85f07c7e0f4673fff27a
Victor Rentea <victorrentea@gmail.com>
2023-09-27 19:56:37 +0300

    Revert "the benefit was lost because another a**hole starved the shared commonPOOL"
    This reverts commit 33bc380ebc69e52b20da888559d8aa319ff94ab1.

--- main/src/main/java/victor/training/performance/parallelStream/ParallelStreams.java
+++ main/src/main/java/victor/training/performance/parallelStream/ParallelStreams.java
@@ -11,16 +11,15 @@ import static victor.training.performance.util.PerformanceUtil.sleepMillis;
 @Slf4j
 public class ParallelStreams {
    public static void main(String[] args) {
-      Enemy.parallelRequest(); // demonstrates starvation of the shared commonPool
+//      Enemy.parallelRequest(); // demonstrates starvation of the shared commonPool
 
       long t0 = System.currentTimeMillis();
 
       List<Integer> list = IntStream.range(1,50).boxed().collect(toList());
 
-      // runs on a default "ForkJoinPool.commonPool"
-     // global per JVM😱,  with size  N threads = N CPU-1
+      // runs on a default "ForkJoinPool.commonPool" global per JVM with size  N threads = N CPU-1
       List<Integer> result = list.parallelStream()
-          .map(i -> apiCall(i)) // I/O call can starve the commonPool
+          .map(i -> apiCall(i))
           .collect(toList());
       log.debug("Got result: " + result);
 
