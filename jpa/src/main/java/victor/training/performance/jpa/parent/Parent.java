ca897c23bda536e27dae39cc5d3a9d916f2bbafc
Victor Rentea <victorrentea@gmail.com>
2023-11-07 10:18:30 +0200

    fetch=EAGER - de evitat

--- jpa/src/main/java/victor/training/performance/jpa/parent/Parent.java
+++ jpa/src/main/java/victor/training/performance/jpa/parent/Parent.java
@@ -6,7 +6,11 @@ import victor.training.performance.jpa.uber.Country;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
+
+import static javax.persistence.CascadeType.PERSIST;
+import static javax.persistence.FetchType.EAGER;
 
 @Getter
 @Setter
@@ -19,18 +23,24 @@ public class Parent {
    private String name;
    private Integer age;
 
-   @OneToMany(mappedBy = "parent", cascade = CascadeType.PERSIST)
+   @OneToMany(mappedBy = "parent",
+       fetch = EAGER, // = ask Hibernate to only give you instances of Parent with children pre-loaded
+       // by default hibernate fetches children by 1 query/parent
+       // defapt EAGER e folosit doar ca sa evite lazy loadingul, dar n-ai scapat de N queryuri.
+       // ba mai rau, ori de cate ori primesti isntanta de Parent de la Hibernate, hib a adus si copiii, ca ai ca n-ai nevoie, sunt cu time
+       cascade = PERSIST)
+
    // @BatchSize(size=10) // Hibernate magic that avoids N+1 using ID IN (?,?..)
    private Set<Child> children = new HashSet<>();
 
    @ManyToOne
    private Country country; // surprise !
 
-   protected Parent() {
+   protected Parent() { // MUST HAVE no-arg constructor, devi nu pot chema constructorul
    }
 
-   public Parent(String name) {
-      this.name = name;
+   public Parent(String name) { // eu doar pe asta pot sa-l chema
+      this.name = Objects.requireNonNull(name);
    }
 
    public Parent addChild(Child child) {
--- jpa/src/test/java/victor/training/performance/jpa/NPlusOne.java
+++ jpa/src/test/java/victor/training/performance/jpa/NPlusOne.java
@@ -1,6 +1,5 @@
 package victor.training.performance.jpa;
 
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,9 +20,9 @@ import victor.training.performance.jpa.uber.CountryRepo;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Set;
 
 import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
@@ -35,101 +34,106 @@ import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFOR
 @Rollback(false) // at the end of each @Test, don't rollback the @Transaction, to be able to inspect the DB contents
 @DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD) // nuke Spring + re-init DB with Hibernate
 public class NPlusOne {
-    @Autowired
-    EntityManager entityManager;
-    @Autowired
-    ParentRepo repo;
-    @Autowired
-    private CountryRepo countryRepo;
+  @Autowired
+  EntityManager entityManager;
+  @Autowired
+  ParentRepo repo;
+  @Autowired
+  private CountryRepo countryRepo;
 
-    @BeforeEach
-    void persistData() {
-        repo.save(new Parent("Victor")
-                .setCountry(countryRepo.save(new Country(1L, "Romania")))
-                .setAge(36)
-                .addChild(new Child("Emma"))
-                .addChild(new Child("Vlad"))
-        );
-        repo.save(new Parent("Trofim") // bachelor, no children
-                .setAge(42));
-        repo.save(new Parent("Peter")
-                .setAge(41)
-                .setCountry(countryRepo.save(new Country(2L,"Moldavia")))
-                .addChild(new Child("Maria"))
-                .addChild(new Child("Paul"))
-                .addChild(new Child("Stephan"))
-        );
-        TestTransaction.end();
+  @BeforeEach
+  void persistData() {
+    Country ro = countryRepo.save(new Country(1L, "Romania"));
+    repo.save(new Parent("Victor")
+        .setCountry(ro)
+        .setAge(36)
+        .addChild(new Child("Emma"))
+        .addChild(new Child("Vlad"))
+    );
+    repo.save(new Parent("Trofim") // bachelor, no children
+        .setAge(42));
+    Country md = countryRepo.save(new Country(2L, "Moldavia"));
+    repo.save(new Parent("Peter")
+        .setAge(41)
+        .setCountry(md)
+        .addChild(new Child("Maria"))
+        .addChild(new Child("Paul"))
+        .addChild(new Child("Stephan"))
+    );
+    TestTransaction.end();
 
-        TestTransaction.start();
+    TestTransaction.start();
+  }
+
+  record ParentSearchResult(Long id, String name, String childrenNames) {
+    public ParentSearchResult(Parent parent) {
+      this(
+          parent.getId(),
+          parent.getName(),
+          joinChildrenNames(parent.getChildren()));
     }
 
-    @Value
-    static class ParentSearchResult {
-        Long id;
-        String name;
-        String childrenNames;
-        public ParentSearchResult(Parent parent) {
-            id = parent.getId();
-            name = parent.getName();
-            childrenNames = parent.getChildren().stream().map(Child::getName).sorted().collect(joining(","));
-        }
+    private static String joinChildrenNames(Set<Child> children) {
+      return children.stream() // lazy loading causes 1 query / parent
+          .map(Child::getName).sorted().collect(joining(","));
     }
+  }
 
-    // This is what is displayed in the UI:
-    private static void assertResultsInUIGrid(List<?> results) {
-        assertThat(results)
-                .extracting("name", "childrenNames")
-                .containsExactlyInAnyOrder(
-                        tuple("Trofim", ""),
-                        tuple("Victor", "Emma,Vlad"),
-                        tuple("Peter", "Maria,Paul,Stephan"))
-        ;
-    }
+  // This is what is displayed in the UI:
+  private static void assertResultsInUIGrid(List<?> results) {
+    assertThat(results)
+        .extracting("name", "childrenNames")
+        .containsExactlyInAnyOrder(
+            tuple("Trofim", ""),
+            tuple("Victor", "Emma,Vlad"),
+            tuple("Peter", "Maria,Paul,Stephan"))
+    ;
+  }
 
-    // ======================= OPTION 1: SELECT full @Entity =============================
-    @Test
-    public void selectFullEntity() {
-        List<Parent> parents = repo.findAll();
+  // ======================= OPTION 1: SELECT full @Entity =============================
+  @Test
+  public void selectFullEntity() {
+    List<Parent> parents = repo.findAll(); // 1 SELECT
 
-        log.info("Loaded {} parents: {}", parents.size(), parents);
+    log.info("Loaded {} parents: {}", parents.size(), parents);
 
-        List<ParentSearchResult> results = toSearchResults(parents);
+    List<ParentSearchResult> results = toSearchResults(parents);
 
-        assertResultsInUIGrid(results);
-    }
+    assertResultsInUIGrid(results);
+  }
 
-    private List<ParentSearchResult> toSearchResults(List<Parent> parents) { // eg, in a Mapper
-        log.debug("Converting-->Dto START");
-        List<ParentSearchResult> results = parents.stream().map(ParentSearchResult::new).collect(toList());
-        log.debug("Converting-->Dto DONE");
-        return results;
-    }
+  private List<ParentSearchResult> toSearchResults(List<Parent> parents) { // eg, in a Mapper
+    log.debug("Converting-->Dto START");
+    List<ParentSearchResult> results = parents.stream().map(ParentSearchResult::new).toList(); // N SELECT
+    log.debug("Converting-->Dto DONE");
+    return results;
+  }
 
 
-    // ======================= OPTION 2: native SQL query selecting projections ==============
-    @Autowired
-    ParentSearchViewRepo searchRepo;
-    @Test
-    public void nativeQuery() {
-        List<ParentSearchProjection> results = searchRepo.nativeQueryForProjections();
-        assertResultsInUIGrid(results);
-    }
+  // ======================= OPTION 2: native SQL query selecting projections ==============
+  @Autowired
+  ParentSearchViewRepo searchRepo;
 
-    // ======================= OPTION 3: Hibernate @Subselect ==============
-    @Test
-    public void subselect() {
-        Page<ParentSearchSubselect> results = repo.searchSubselect("%", PageRequest.of(0, 5));
-        assertResultsInUIGrid(results.getContent());
-    }
+  @Test
+  public void nativeQuery() {
+    List<ParentSearchProjection> results = searchRepo.nativeQueryForProjections();
+    assertResultsInUIGrid(results);
+  }
 
-    // ======================= OPTION 4: @Entity on VIEW =============================
-    @Test
-    @Sql("/create-view.sql")
-    public void searchOnView() {
-        List<ParentSearchView> results = searchRepo.findAll();
-        assertResultsInUIGrid(results);
-    }
+  // ======================= OPTION 3: Hibernate @Subselect ==============
+  @Test
+  public void subselect() {
+    Page<ParentSearchSubselect> results = repo.searchSubselect("%", PageRequest.of(0, 5));
+    assertResultsInUIGrid(results.getContent());
+  }
+
+  // ======================= OPTION 4: @Entity on VIEW =============================
+  @Test
+  @Sql("/create-view.sql")
+  public void searchOnView() {
+    List<ParentSearchView> results = searchRepo.findAll();
+    assertResultsInUIGrid(results);
+  }
 
 }
 
