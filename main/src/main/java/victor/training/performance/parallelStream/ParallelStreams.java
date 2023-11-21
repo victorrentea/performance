f77a1f1f8d0c0b192dd9728cb3448c396054ed45
Victor Rentea <victorrentea@gmail.com>
2023-11-21 12:47:35 +0200

    cum lansez parallelStream pe pool-ul meu - e Doamne fereste! de greu

--- main/src/main/java/victor/training/performance/parallelStream/ParallelStreams.java
+++ main/src/main/java/victor/training/performance/parallelStream/ParallelStreams.java
@@ -3,6 +3,8 @@ package victor.training.performance.parallelStream;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.toList;
@@ -10,28 +12,31 @@ import static victor.training.performance.util.PerformanceUtil.sleepMillis;
 
 @Slf4j
 public class ParallelStreams {
-//  static int abominatie = 0;
-   public static void main(String[] args) {
-      Enemy.parallelRequests(); // demonstrates starvation of the shared commonPool
+  //  static int abominatie = 0;
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+    Enemy.parallelRequests(); // demonstrates starvation of the shared commonPool
 
-      long t0 = System.currentTimeMillis();
+    long t0 = System.currentTimeMillis();
 
-      List<Integer> list = IntStream.range(1,100).boxed().collect(toList());
+    List<Integer> list = IntStream.range(1, 100).boxed().collect(toList());
 
-      // si main() lucreaza, de aia FJP.commonPool.size=NCPU-1(main)
+    ForkJoinPool myfjp = new ForkJoinPool(10);
+
+    // si main() lucreaza, de aia FJP.commonPool.size=NCPU-1(main)
 //      List<Integer> result = list.parallelStream() // 500 ms
-      List<Integer> result = list.stream() // 5000 ms
-          .filter(i -> i % 2 == 0)
-          .map(i -> {
-             log.debug("Map " + i);
-             sleepMillis(100); // do some 'paralellizable' I/O work (DB, REST, SOAP)
-             return i * 2;
-          })
-          .collect(toList());
-      log.debug("Got result: " + result);
+    List<Integer> result = myfjp.submit(() ->
+        list.parallelStream() // 5000 ms
+            .filter(i -> i % 2 == 0)
+            .map(i -> {
+              log.debug("Map " + i);
+              sleepMillis(100); // do some 'paralellizable' I/O work (DB, REST, SOAP)
+              return i * 2;
+            })
+            .collect(toList())).get();
+    log.debug("Got result: " + result);
 
-      long t1 = System.currentTimeMillis();
-      log.debug("Took {} ms", t1 - t0);
-   }
+    long t1 = System.currentTimeMillis();
+    log.debug("Took {} ms", t1 - t0);
+  }
 }
 
