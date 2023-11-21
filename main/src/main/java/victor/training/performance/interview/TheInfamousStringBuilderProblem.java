03578c32214dc17dab762824481d6a02a28d121d
Victor Rentea <victorrentea@gmail.com>
2023-11-21 09:54:32 +0200

    StringBuilder

--- main/src/main/java/victor/training/performance/interview/TheInfamousStringBuilderProblem.java
+++ main/src/main/java/victor/training/performance/interview/TheInfamousStringBuilderProblem.java
@@ -14,19 +14,26 @@ import static java.util.stream.Collectors.toList;
 public class TheInfamousStringBuilderProblem {
    public static void main(String[] args) throws IOException {
       PerformanceUtil.printJfrFile();
-      List<String> elements = IntStream.range(1, 50_000) // see TLAB
-          .mapToObj(n -> "hahaha")
+      List<String> elements = IntStream.range(1, 500_000) // see TLAB
+          .mapToObj(n -> "hahaha") // 2 x 6 x 50K = 6000KB
           .collect(toList());
 
       PerformanceUtil.waitForEnter();
       System.out.println("Start writing contents!");
       long t0 = System.currentTimeMillis();
 
-      String s = "";
+//      String s = "";
+//      for (String element : elements) {
+//         s += element; // la fiecare iteratie se face malloc
+//      }
+      StringBuilder s = new StringBuilder();
+      // StringBuilder e pentru String ce ArrayList e pentru []
+      // = prealoca mai multa capacitate decat tine date acum
+      // pt a appenda mai ieftin. din cand in cand tre sa se dubleze
       for (String element : elements) {
-         s += element;
+         s.append(element); // 19 ms fata de 32 SECUNDE
       }
-      FileUtils.writeStringToFile(new File("out.txt"), s, UTF_8);
+      FileUtils.writeStringToFile(new File("out.txt"), s.toString(), UTF_8);
 
       System.out.println("Done. Took " + (System.currentTimeMillis() - t0));
       PerformanceUtil.waitForEnter();
