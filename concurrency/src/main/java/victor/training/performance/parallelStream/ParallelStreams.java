160758d7bc74724aec618876bc84e9026eab01ff
Victor Rentea <victorrentea@gmail.com>
2024-01-16 17:00:50 +0200

    // cum rulez parallelStream pe thread pool privat al meu

--- concurrency/src/main/java/victor/training/performance/parallelStream/ParallelStreams.java
+++ concurrency/src/main/java/victor/training/performance/parallelStream/ParallelStreams.java
@@ -3,28 +3,36 @@ package victor.training.performance.parallelStream;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 import static victor.training.performance.util.PerformanceUtil.sleepMillis;
 
 @Slf4j
 public class ParallelStreams {
-   public static void main(String[] args) {
+   public static void main(String[] args) throws ExecutionException, InterruptedException {
       OnAServer.otherParallelRequestsAreRunning(); // starve the shared commonPool din JVM
 
       List<Integer> list = IntStream.range(1,100).boxed().collect(toList());
 
       long t0 = System.currentTimeMillis();
 
-      List<Integer> result = list.parallelStream()
-          .filter(i -> i % 2 == 0)
-          .map(i -> {
-             log.debug("Map " + i);
-             sleepMillis(100); // time-consuming work (CPU or DB, REST, SOAP)
-             return i * 2; // pretend: return api.call(i);
-          })
-          .collect(toList()); // acum fluxul ruleaza pe 10-1 = 9th + main = 10 threaduri
+     Stream<Integer> stream = list.parallelStream()
+         .filter(i -> i % 2 == 0)
+         .map(i -> {
+           log.debug("Map " + i);
+           sleepMillis(100); // time-consuming work (CPU or DB, REST, SOAP)
+           return i * 2; // pretend: return api.call(i);
+         });
+
+     // cum rulez parallelStream pe thread pool privat al meu
+     ForkJoinPool forkJoinPool = new ForkJoinPool(16);
+     List<Integer> result = forkJoinPool.submit(
+         () -> stream.collect(toList())).get();
+     // acum fluxul ruleaza pe 10-1 = 9th + main = 10 threaduri
      // munca dureaza 500ms nu 5000 ca la inceput
       log.debug("Got result: " + result);
 
