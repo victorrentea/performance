a773182626f1b678c55a2a5d4e59ecba58dd43ee
Victor Rentea <victorrentea@gmail.com>
2024-01-16 11:42:58 +0200

    y

--- concurrency/src/main/java/victor/training/performance/threadpool/ThreadPools.java
+++ concurrency/src/main/java/victor/training/performance/threadpool/ThreadPools.java
@@ -1,9 +1,6 @@
 package victor.training.performance.threadpool;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static victor.training.performance.util.PerformanceUtil.log;
@@ -13,22 +10,36 @@ public class ThreadPools {
 
    public static void main(String[] args) throws InterruptedException {
       // TODO Executor that keeps a fixed number (3) of threads until it is shut down
-      // RISK: out of memory daca vin foarte multe taskuri gramada
-      ExecutorService executor = Executors.newFixedThreadPool(3);
+      // RISK: out of memory daca vin foarte multe taskuri gramada (spike)
+//      ExecutorService executor = Executors.newFixedThreadPool(3);
 
-      // TODO Executor that grows the thread pool as necessary, and kills inactive ones after 1 min
-      // ExecutorService executor = Executors. ?
+      // TODO Executor that grows the thread pool as necessary,
+      //     and kills inactive ones after 1 min
+      // RISK: aloci prea multe threaduri (spike) => OutOfMemory/process hang
+      // nu ai coada de asteptare, intri direct in lucru
+//       ExecutorService executor = Executors.newCachedThreadPool();
 
-      // TODO Executor that have at least 3 thread but can grow up to 10 threads,
-      // with a queue of max 5 elements. Inactive threads die in 1 second.
-      // ExecutorService executor = new ThreadPoolExecutor(...)
+      // TODO Executor that have at least min=3 thread but can grow up to max=10 threads,
+      //  with a bounded queue of max 5 elements.
+      //  Inactive threads kept alive for 1 second.
+       ExecutorService executor = new ThreadPoolExecutor(
+         3, 3, // fixed size = 3
+         1, TimeUnit.SECONDS,
+         new ArrayBlockingQueue<>(5),
+        new ThreadPoolExecutor.CallerRunsPolicy() // o forma pritimiva de backpressure:
+           // impingi munca inapoi in threadul care incearca sa te puna pe tine la munca.
+           // -> incetinindu-l
+           // Risk: sa STARVEZI thread pool-ul upstream. eg sa epuizezi toate 200 th din Tomcat
+           // => rezultat: intarzieri in tratarea req http.
+           // BIZ-CRITICAL endpoint: POST /place-bid
+       );
 
       // TODO Vary the fixed-sized queue to see it grow the pool and then Rejecting tasks
 
       for (int i = 0; i < 40; i++) {
          MyTask task = new MyTask();
          log("Submitted new task #" + task.id);
-         executor.submit(task);
+         executor.submit(task); // acum uneori submit() dureaza 500 millis ca faci TU munca
          sleepSomeTime(100, 200); // simulate random request rate
       }
       executor.shutdown();
