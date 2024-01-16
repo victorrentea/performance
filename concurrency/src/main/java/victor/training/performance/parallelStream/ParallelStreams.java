8cabc642225db58b4eda5fb42488b6a7bf421fce
Victor Rentea <victorrentea@gmail.com>
2024-01-16 16:45:36 +0200

    in productie nu merge pentru ca te bati cu altii pe acelasi pool, da-i aia-s mai nesimtitit ca tine, si fac apeluri mai lungi de retea

--- concurrency/src/main/java/victor/training/performance/parallelStream/ParallelStreams.java
+++ concurrency/src/main/java/victor/training/performance/parallelStream/ParallelStreams.java
@@ -11,7 +11,7 @@ import static victor.training.performance.util.PerformanceUtil.sleepMillis;
 @Slf4j
 public class ParallelStreams {
    public static void main(String[] args) {
-//      OnAServer.otherParallelRequestsAreRunning(); // starve the shared commonPool
+      OnAServer.otherParallelRequestsAreRunning(); // starve the shared commonPool din JVM
 
       List<Integer> list = IntStream.range(1,100).boxed().collect(toList());
 
