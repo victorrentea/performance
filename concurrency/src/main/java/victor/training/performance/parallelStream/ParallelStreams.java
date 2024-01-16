e5aa55c3711788457bc21e22c060226643a245ce
Victor Rentea <victorrentea@gmail.com>
2024-01-16 17:01:23 +0200

    inapoi undo

--- concurrency/src/main/java/victor/training/performance/parallelStream/ParallelStreams.java
+++ concurrency/src/main/java/victor/training/performance/parallelStream/ParallelStreams.java
@@ -3,8 +3,6 @@ package victor.training.performance.parallelStream;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -13,7 +11,7 @@ import static victor.training.performance.util.PerformanceUtil.sleepMillis;
 
 @Slf4j
 public class ParallelStreams {
-   public static void main(String[] args) throws ExecutionException, InterruptedException {
+   public static void main(String[] args) {
       OnAServer.otherParallelRequestsAreRunning(); // starve the shared commonPool din JVM
 
       List<Integer> list = IntStream.range(1,100).boxed().collect(toList());
@@ -28,11 +26,8 @@ public class ParallelStreams {
            return i * 2; // pretend: return api.call(i);
          });
 
-     // cum rulez parallelStream pe thread pool privat al meu
-     ForkJoinPool forkJoinPool = new ForkJoinPool(16);
-     List<Integer> result = forkJoinPool.submit(
-         () -> stream.collect(toList())).get();
-     // acum fluxul ruleaza pe 10-1 = 9th + main = 10 threaduri
+     List<Integer> result = stream
+          .collect(toList()); // acum fluxul ruleaza pe 10-1 = 9th + main = 10 threaduri
      // munca dureaza 500ms nu 5000 ca la inceput
       log.debug("Got result: " + result);
 
