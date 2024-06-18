ff90ad58fb423ca0b572a891e50101df7ce9c85a
Victor Rentea <victorrentea@gmail.com>
2024-06-18 11:22:01 +0300

    am paralelizat. am redus timpu;l de baut la cu 50% am rupt.!

--- concurrency/src/main/java/victor/training/performance/Barman.java
+++ concurrency/src/main/java/victor/training/performance/Barman.java
@@ -9,21 +9,33 @@ import victor.training.performance.drinks.Beer;
 import victor.training.performance.drinks.DillyDilly;
 import victor.training.performance.drinks.Vodka;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 import static java.lang.System.currentTimeMillis;
 
 @Slf4j
 @RestController
 public class Barman {
   @Autowired
-  private RestTemplate rest;
+  private RestTemplate restTemplate;
 
   @GetMapping("/drink")
-  public DillyDilly drink() {
+  public DillyDilly drink() throws ExecutionException, InterruptedException {
+    log.info("Start");
     long t0 = currentTimeMillis();
 
-    //  🛑 independent tasks executed sequentially. What TODO ?
-    Beer beer = rest.getForObject("http://localhost:9999/beer", Beer.class);
-    Vodka vodka = rest.getForObject("http://localhost:9999/vodka", Vodka.class);
+    //  🛑 2 independent tasks executed sequentially. What TODO ?
+    // CompletableFuture(java)===promise(JS)
+    CompletableFuture<Beer> beerPromise = CompletableFuture.supplyAsync(() ->
+        restTemplate.getForObject("http://localhost:9999/beer", Beer.class));
+    CompletableFuture<Vodka> vodkaPromise = CompletableFuture.supplyAsync(() ->
+        restTemplate.getForObject("http://localhost:9999/vodka", Vodka.class));
+
+    Beer beer = beerPromise.get(); // threadul Tomcat sta aici blocat 1 sec
+    Vodka vodka = vodkaPromise.get(); // threadul Tomcat sta aici blocat 0 sec
+    // vodka e deja turnata cand am berea in mana. Ca tot 1 sec a durat IN PARALEL CU MINE
+
     DillyDilly dilly = new DillyDilly(beer, vodka);
 
     log.info("HTTP thread blocked for millis: " + (currentTimeMillis() - t0));
