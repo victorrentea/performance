e5668c7f430435a6789631a1cff5749408f3d807
Victor Rentea <victorrentea@gmail.com>
2024-06-18 15:48:23 +0300

    v4 RC1

--- concurrency/src/main/java/victor/training/performance/ProducerConsumer.java
+++ concurrency/src/main/java/victor/training/performance/ProducerConsumer.java
@@ -1,5 +1,7 @@
 package victor.training.performance;
 
+import org.springframework.cloud.sleuth.brave.bridge.BraveSamplerFunction;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,13 +14,14 @@ import static java.util.Collections.synchronizedList;
 
 public class ProducerConsumer {
   private static List<Integer> buffer = new ArrayList<>();
+  private static final Object lock = new Object();
 
   public static void main(String[] args) {
     ExecutorService submitteri = Executors.newCachedThreadPool();
     for (int i = 0; i < 100; i++) {
       submitteri.submit(() -> {
         for (int j = 0; true; j++) {
-          synchronized (ProducerConsumer.class) {
+          synchronized (lock) {
             buffer.add(j);
           }
           Thread.sleep(10);
@@ -31,7 +34,7 @@ public class ProducerConsumer {
       try {
         List<Integer> newBuffer = new ArrayList<>();
         List<Integer> copie;
-        synchronized (ProducerConsumer.class) {
+        synchronized (lock) {
            copie = buffer;
            buffer = newBuffer;
         }
