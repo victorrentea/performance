4e8554abb3c9e44920e845b3f62b9794647d4637
Victor Rentea <victorrentea@gmail.com>
2024-06-19 23:36:39 +0300

    polish cleanup

--- memory/src/main/java/victor/training/performance/interview/CollectionsSize.java
+++ memory/src/main/java/victor/training/performance/interview/CollectionsSize.java
@@ -7,37 +7,35 @@ import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
 public class CollectionsSize {
-
    public static final int ONE_MILLION = 1_000_000;
 
    public static void main(String[] args) {
-      // Use-case: you have to keep a large number of IDs throughout a long memory-intensive batch
       long heap0 = PerformanceUtil.getUsedHeapBytes();
 
-      // HashSet =  MB because
+      //  MB because HashSet
       Set<Long> x = LongStream.range(0, ONE_MILLION).boxed().collect(Collectors.toSet());
 
-      // ArrayList =  MB because
+      //  MB because ArrayList
 //      List<Long> x = LongStream.range(0, ONE_MILLION).boxed().collect(Collectors.toList());
 
-      // Long[] =  MB because
+      //  MB
+//      int[] x = IntStream.range(0, ONE_MILLION).toArray();
+
+      //  MB
 //      Long[] x = LongStream.range(0, ONE_MILLION).boxed().toArray(Long[]::new);
 
-      // LinkedList =  MB because
+      //  MB
 //      LinkedList<Long> x = LongStream.range(0, ONE_MILLION).boxed().collect(Collectors.toCollection(LinkedList::new));
 
-      // long[] =  MB because
+      //  MB
 //      long[] x = LongStream.range(0, ONE_MILLION).toArray();
-
-      // int[] =  MB because
-//      int[] x = IntStream.range(0, ONE_MILLION).toArray();
 
       long heap1 = PerformanceUtil.getUsedHeapBytes();
       System.out.println("Collection(size="+ONE_MILLION+")" + PerformanceUtil.objectToString(x) + " occupies: " + (heap1 - heap0) / 1024 / 1024 + " MB");
 
-      x.clear();
-      long heap2 = PerformanceUtil.getUsedHeapBytes();
-      System.out.println("Collection.clear()ed) still occupies: " + (heap2 - heap0) / 1024 / 1024 + " MB");
+//      x.clear();
+//      long heap2 = PerformanceUtil.getUsedHeapBytes();
+//      System.out.println("Collection.clear()ed) still occupies: " + (heap2 - heap0) / 1024 / 1024 + " MB");
    }
 
 }
