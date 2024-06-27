5b92a759267996a374b5d4a3292d19078f939240
Victor Rentea <victorrentea@gmail.com>
2024-06-27 16:44:48 +0300

    am oprit testul pentru a ma putea conecta la DB sa vad ce e in el.

--- spring-batch/src/main/java/victor/training/spring/batch/core/StartListener.java
+++ spring-batch/src/main/java/victor/training/spring/batch/core/StartListener.java
@@ -8,7 +8,8 @@ import java.time.LocalDateTime;
 public class StartListener implements JobExecutionListener {
     @Override
     public void beforeJob(JobExecution jobExecution) {
-        jobExecution.getExecutionContext().put("START_TIME", LocalDateTime.now().toString());
+        jobExecution.getExecutionContext().put("START_TIME",
+            LocalDateTime.now().toString());
     }
 
     @Override
