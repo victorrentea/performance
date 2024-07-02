357ec525696db9c7b6832a51cd4c5cd10fbe7467
Victor Rentea <victorrentea@gmail.com>
2024-07-02 13:02:56 +0300

    cum poti sa pierzi un element in hashSet: isi modifica campurile implicate in hashCode

--- memory/src/main/java/victor/training/performance/interview/LoosingElementsInASet.java
+++ memory/src/main/java/victor/training/performance/interview/LoosingElementsInASet.java
@@ -1,8 +1,12 @@
 package victor.training.performance.interview;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.Entity;
 import java.util.HashSet;
+import java.util.Objects;
 
 public class LoosingElementsInASet {
   public static void main(String[] args) {
@@ -23,10 +27,29 @@ public class LoosingElementsInASet {
     System.out.println("Puii meiL: " + hashSet);
   }
 
-  @Data //generates hashCode/equals on all fields
+
+//  @Entity // NU PUI @Data pe @Entity ORM
+//  @Data //generates hashCode/equals on all fields
+  @Getter
+  @Setter
   private static class Child {
     // REGULA ORICE CAMP INCLUZI IN HASH/EQUALS el tre sa fie IMUTABIL
     private /*final*/ Long id;
     private /*final*/ String name;
-  }
+
+
+    // evita sa faci hash/eq pe @Entity. Daca totusi insisti, cauta o "cheie naturala". NU include @Id in hash/eq
+//  @Override
+//  public boolean equals(Object o) {
+//    if (this == o) return true;
+//    if (o == null || getClass() != o.getClass()) return false;
+//    Child child = (Child) o;
+//    return Objects.equals(name, child.name);
+//  }
+//
+//  @Override
+//  public int hashCode() {
+//    return Objects.hashCode(name);
+//  }
+}
 }
