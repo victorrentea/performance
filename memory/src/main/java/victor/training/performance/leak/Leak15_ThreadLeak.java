431d611ad9143bae3be03612763294c486617dd4
Victor Rentea <victorrentea@gmail.com>
2024-07-03 02:07:08 +0300

    polish for extraction

--- memory/src/main/java/victor/training/performance/leak/Leak15_ThreadLeak.java
+++ memory/src/main/java/victor/training/performance/leak/Leak15_ThreadLeak.java
@@ -1,36 +1,33 @@
 package victor.training.performance.leak;
 
-import com.github.dockerjava.api.command.AuthCmd.Exec;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import victor.training.performance.leak.obj.BigObject20MB;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Slf4j
 @RestController
 @RequestMapping("leak15")
-public class Leak15_ThreadLocal {
-   private static final ThreadLocal<BigObject20MB> threadLocal = new ThreadLocal<>();
+public class Leak15_ThreadLeak {
 
    @GetMapping
    public void endpoint() {
       ExecutorService pool = Executors.newFixedThreadPool(2);
       try {
          pool.submit(() -> log.info("Work"));
-         f();
+         surprise();
       } finally {
          pool.shutdown();
       }
-      // TODO tomorrow (not me) when you have time,
-      //   inject and use a Spring-managed ThreadPoolTaskExecutor
    }
 
-   private void f() {
-      if (true) throw new RuntimeException("Intentional");
+   private void surprise() {
+      throw new RuntimeException("#life");
    }
 }
+
+// TODO avoid creating new Thread Pools per request
+//  > inject and use a Spring-managed ThreadPoolTaskExecutor
