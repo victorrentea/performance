b524fbfb489abffcf2920662bd59ed4406aed0db
Victor Rentea <victorrentea@gmail.com>
2024-09-03 13:32:54 +0300

    thread local metadata propagated over to child threads

--- concurrency/src/main/java/victor/training/performance/Barman.java
+++ concurrency/src/main/java/victor/training/performance/Barman.java
@@ -1,7 +1,9 @@
 package victor.training.performance;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -9,9 +11,8 @@ import victor.training.performance.drinks.Beer;
 import victor.training.performance.drinks.DillyDilly;
 import victor.training.performance.drinks.Vodka;
 
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import static java.lang.System.currentTimeMillis;
@@ -21,29 +22,35 @@ import static java.lang.System.currentTimeMillis;
 public class Barman {
   @Autowired
   private RestTemplate rest;
+  /// guice:
+  /// @Inject
+//    ExecutorService pool;
+  @Autowired
+  ThreadPoolTaskExecutor poolBar;
 
   @GetMapping("/drink")
   public DillyDilly drink() throws ExecutionException, InterruptedException {
     long t0 = currentTimeMillis();
 
-    ExecutorService pool = Executors.newCachedThreadPool();
-    try {
-      // #1 it is inefficient to spawn 2 threads to keep them blocked for the API call duration
-      // usa an async client: AsyncRestTemplate, WebClient (reactor)
-      // to call network without blocking any thread for the call duration
+    MDC.put("tenantId", "tenant-" + new Random().nextInt(100));
+    log.info("Start");
+    // #1 it is inefficient to spawn 2 threads to keep them blocked for the API call duration
+    // usa an async client: AsyncRestTemplate, WebClient (reactor)
+    // to call network without blocking any thread for the call duration
 
-      // #2 I forgot the close the pool
-      Future<Beer> futureBeer = pool.submit(() -> rest.getForObject("http://localhost:9999/beer", Beer.class));
-      Future<Vodka> futureVodka = pool.submit(() -> rest.getForObject("http://localhost:9999/vodka", Vodka.class));
+    // #2 I forgot the close the pool
+    Future<Beer> futureBeer = poolBar.submit(() -> beer());
+    Future<Vodka> futureVodka = poolBar.submit(() -> rest.getForObject("http://localhost:9999/vodka", Vodka.class));
 
-      Beer beer = futureBeer.get();
-      Vodka vodka = futureVodka.get();
-      DillyDilly dilly = new DillyDilly(beer, vodka);
-      log.info("HTTP thread blocked for {} durationMillis", currentTimeMillis() - t0);
-      return dilly;
-    } finally {
-      pool.shutdown();
-    }
+    Beer beer = futureBeer.get();
+    Vodka vodka = futureVodka.get();
+    DillyDilly dilly = new DillyDilly(beer, vodka);
+    log.info("HTTP thread blocked for {} durationMillis", currentTimeMillis() - t0);
+    return dilly;
+  }
 
+  private Beer beer() {
+    log.info("Calling beer");
+    return rest.getForObject("http://localhost:9999/beer", Beer.class);
   }
 }
--- concurrency/src/main/java/victor/training/performance/BarmanConfig.java
+++ concurrency/src/main/java/victor/training/performance/BarmanConfig.java
@@ -1,12 +1,18 @@
 package victor.training.performance;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskDecorator;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import victor.training.performance.threadpool.MonitorQueueWaitingTimeTaskDecorator;
+
+import java.util.Map;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
 
 @Configuration
 public class BarmanConfig {
@@ -19,12 +25,43 @@ public class BarmanConfig {
     executor.setCorePoolSize(barPoolSize); // core == max
     executor.setMaxPoolSize(barPoolSize); // how to decide size?
 
-    executor.setQueueCapacity(500); // how to decide?
+    executor.setQueueCapacity(500); // what queue is this?
+    // when there are more tasks than threads
+    // how to decide the number?
+    // if too large => OOME and loooooong waiting time (increase the latency)
+    // if too small => Rejected tasks 500 to clients
 
-    executor.setTaskDecorator(new MonitorQueueWaitingTimeTaskDecorator(meterRegistry.timer("barman-queue-time")));
+    // PTSD:
+//    executor.setTaskDecorator(new MonitorQueueWaitingTimeTaskDecorator(meterRegistry.timer("barman-queue-time")));
+
+    executor.setTaskDecorator(new TaskDecorator() {
+      @Override
+      public Runnable decorate(Runnable runnable) {
+        // here I am in the parent thread
+        String tenantId = MDC.get("tenantId");
+        return () -> {
+          // here I am in the child thread (worker)
+          MDC.put("tenantId", tenantId); // restore the context
+          try {
+            runnable.run();
+          } finally {
+            MDC.remove("tenantId");
+          }
+        };
+      }
+    });
     executor.setThreadNamePrefix("bar-");
-//    executor.setRejectedExecutionHandler(...);
+    executor.setRejectedExecutionHandler(new CallerRunsPolicy());
+
+
     executor.initialize();
     return executor;
   }
+//  @Provides
+//  @Singleton
+//  public ThreadPoolExecutor barPool() {
+//     return new ThreadPoolExecutor(barPoolSize, barPoolSize, 0L, TimeUnit.MILLISECONDS,
+//         new LinkedBlockingQueue<>(500), new ThreadFactory() {
+//       // JavaSE equivalent of the above:
+//   }
 }
