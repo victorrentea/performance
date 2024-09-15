b36b9104097c183836bec94def9e421140bf053d
Victor Rentea <victorrentea@gmail.com>
2024-09-15 23:15:34 +0300

    yolo

--- memory/src/main/java/victor/training/performance/interview/CollectionsSize.java
+++ memory/src/main/java/victor/training/performance/interview/CollectionsSize.java
@@ -2,55 +2,49 @@ package victor.training.performance.interview;
 
 import victor.training.performance.util.PerformanceUtil;
 
-import java.util.Set;
+import java.lang.reflect.Array;
+import java.util.*;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
+import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toList;
+
 public class CollectionsSize {
   public static final int ONE_MILLION = 1_000_000;
+
+  public static void main(String[] args) {
+    // TODO order the following from lowest->highest memory consumption
+
+    // A) ArrayList[1M] =  MB because:
+    measureHeap(() -> (ArrayList<Long>)LongStream.range(0, ONE_MILLION).boxed().collect(toList()));
+
+    // B) long[1M] =  MB because:
+    measureHeap(() -> (long[]) LongStream.range(0, ONE_MILLION).toArray());
+
+    // C) HashSet[1M] =  MB because:
+    measureHeap(() -> (HashSet<Long>) LongStream.range(0, ONE_MILLION).boxed().collect(Collectors.toSet()));
+
+    // D) LinkedList[1M] =  MB because:
+    measureHeap(() -> (LinkedList<Long>) LongStream.range(0, ONE_MILLION).boxed().collect(toCollection(LinkedList::new)));
+
+    // E) Long[1M] =  MB because:
+    measureHeap(() -> (Long[]) LongStream.range(0, ONE_MILLION).boxed().toArray(Long[]::new));
+
+    // F) int[1M] =  MB because:
+    measureHeap(() -> (int[]) IntStream.range(0, ONE_MILLION).toArray());
+
+    // TODO experiment .clear the collections and see if memory is released
+  }
 
   public static void measureHeap(Supplier<Object> allocation) {
     long heap0 = PerformanceUtil.getUsedHeapBytes();
     Object x = allocation.get();
     long heap1 = PerformanceUtil.getUsedHeapBytes();
-    System.out.println(
-        "Collection(size=" + ONE_MILLION + ")"
-        + PerformanceUtil.objectToString(x) +
+    System.out.println(PerformanceUtil.objectToString(x) + " [" + ONE_MILLION + " elements] " +
         " occupies: " + (heap1 - heap0) / 1024 / 1024 + " MB");
-  }
-
-  public static void main(String[] args) {
-    long heap0 = PerformanceUtil.getUsedHeapBytes();
-    // low->high
-
-    // int[] 3 MB (30x smaller than Set<Long>)
-    measureHeap(() -> (int[]) IntStream.range(0, ONE_MILLION).toArray());
-
-    // long[] 8 MB
-    measureHeap(() -> (long[]) LongStream.range(0, ONE_MILLION).toArray());
-
-    // Long[] 27 MB because of boxing (more objects on heap)
-    measureHeap(() -> (Long[]) LongStream.range(0, ONE_MILLION).boxed().toArray(Long[]::new));
-//      int[] x = IntStream.range(0, ONE_MILLION).toArray();
-//      long[] x = LongStream.range(0, ONE_MILLION).toArray();
-//      Long[] x = LongStream.range(0, ONE_MILLION).boxed().toArray(Long[]::new);
-    // 31 MB because ArrayList keeps extra capacity
-//      List<Long> x = LongStream.range(0, ONE_MILLION).boxed().collect(Collectors.toList());
-    // 46 MB
-//      LinkedList<Long> x = LongStream.range(0, ONE_MILLION).boxed().collect(Collectors.toCollection(LinkedList::new));
-    // 69 MB because HashSet
-    Set<Long> x = LongStream.range(0, ONE_MILLION).boxed().collect(Collectors.toSet());
-    //if  SHA512 (previuoslyReceivedXml).equals(SHA512 (newXml)) { skip }
-//      x.toArr
-
-//    long heap1 = PerformanceUtil.getUsedHeapBytes();
-//    System.out.println("Collection(size=" + ONE_MILLION + ")" + PerformanceUtil.objectToString(x) + " occupies: " + (heap1 - heap0) / 1024 / 1024 + " MB");
-//
-//    x.clear();
-//    long heap2 = PerformanceUtil.getUsedHeapBytes();
-//    System.out.println("Collection.clear()ed) still occupies: " + (heap2 - heap0) / 1024 / 1024 + " MB");
   }
 
 }
