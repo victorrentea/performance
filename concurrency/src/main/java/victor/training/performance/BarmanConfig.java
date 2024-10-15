0d8db3e90533810dbaff19749a2da15d1f4db221
Victor Rentea <victorrentea@gmail.com>
2024-10-15 13:44:26 +0300

    pentru Fane+Mario "Thread hopping" de metadata

--- concurrency/src/main/java/victor/training/performance/BarmanConfig.java
+++ concurrency/src/main/java/victor/training/performance/BarmanConfig.java
@@ -1,13 +1,16 @@
 package victor.training.performance;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskDecorator;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import victor.training.performance.threadpool.MonitorQueueWaitingTimeTaskDecorator;
+
+import java.util.Map;
 
 @Configuration
 @EnableAsync //
@@ -23,7 +26,23 @@ public class BarmanConfig {
 
     executor.setQueueCapacity(500); // how to decide?
 
-    executor.setTaskDecorator(new MonitorQueueWaitingTimeTaskDecorator(meterRegistry.timer("barman-queue-time")));
+//    executor.setTaskDecorator(new MonitorQueueWaitingTimeTaskDecorator(meterRegistry.timer("barman-queue-time")));
+
+    executor.setTaskDecorator(new TaskDecorator() {
+      @Override
+      public Runnable decorate(Runnable taskOriginal) {
+        Map<String, String> tot = MDC.getCopyOfContextMap();
+        return () -> { // sunt in worker thread
+          MDC.setContextMap(tot);
+          try {
+            taskOriginal.run(); //delegate to originally submitted task
+          } finally {
+            MDC.clear();
+          }
+        };
+      }
+    });
+
     // executorul poate fi decorat mai departe sa COPIEZE METADATELE IN TH PARINTE IN COPIL
     executor.setThreadNamePrefix("bar-");
 //    executor.setRejectedExecutionHandler(...);
