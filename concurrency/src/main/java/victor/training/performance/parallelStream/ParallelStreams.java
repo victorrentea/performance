6aee3793cd0e5b69d967242fd0ae23c48fd64e1a
Victor Rentea <victorrentea@gmail.com>
2024-10-15 15:49:29 +0300

    parallelStream pe in piscina proprie. #raise

--- concurrency/src/main/java/victor/training/performance/parallelStream/ParallelStreams.java
+++ concurrency/src/main/java/victor/training/performance/parallelStream/ParallelStreams.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static victor.training.performance.util.PerformanceUtil.sleepMillis;
 
@@ -16,13 +18,15 @@ public class ParallelStreams {
     List<Integer> list = IntStream.range(1, 100).boxed().toList();
     long t0 = System.currentTimeMillis();
 
-    var result = list.parallelStream()
+    Stream<Integer> stream = list.parallelStream()
         .filter(i -> i % 2 == 0)
         .map(i -> {
           log.debug("Map " + i);
           sleepMillis(100); // network call (DB, REST, SOAP..) or CPU work
           return i * 2;
-        }).toList();
+        });
+    ForkJoinPool pool = new ForkJoinPool(20);
+    var result = pool.submit(() -> stream.toList()).get();
     // DECI: evita sa faci I/O (blocari) in taskuri pe parallelStream
     // ca sa nu blochezi munca altora care fac si ei parallelStream
     // pe parallelStream faci doar munca CPU
