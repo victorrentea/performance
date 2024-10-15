445d3e5d19120aa0d2def50cfcab5956a004d0f6
Victor Rentea <victorrentea@gmail.com>
2024-10-15 11:35:23 +0300

    upgrade wiremock

--- wiremock/src/main/java/victor/training/performance/StartWireMock.java
+++ wiremock/src/main/java/victor/training/performance/StartWireMock.java
@@ -1,12 +1,9 @@
 package victor.training.performance;
 
 import com.github.tomakehurst.wiremock.standalone.WireMockServerRunner;
-import org.apache.hc.client5.http.classic.methods.HttpPost;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.CompletableFuture;
 
 public class StartWireMock {
   public static void main(String[] args) throws IOException {
@@ -14,11 +11,11 @@ public class StartWireMock {
     File mappingsFolder = new File(rootFolder, "mappings");
     System.out.println("*.json mappings stubs expected at " + mappingsFolder.getAbsolutePath());
 
-    CompletableFuture.runAsync(() -> WatchFolder.watchFolder(mappingsFolder, () ->
-      HttpClients.createDefault().execute(new HttpPost("http://localhost:9999/__admin/mappings/reset"))
-    ));
+//    CompletableFuture.runAsync(() -> WatchFolder.watchFolder(mappingsFolder, () ->
+//      HttpClients.createDefault().execute(new HttpPost("http://localhost:9999/__admin/mappings/reset"))
+//    ));
 
-    WireMockServerRunner.main(
+    new WireMockServerRunner().run(
             "--port", "9999",
             "--root-dir", rootFolder.getAbsolutePath(),
             "--global-response-templating", // UUID
