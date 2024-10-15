2792a2b589a9cc68995d11b7ebb740ba844f641f
Victor Rentea <victorrentea@gmail.com>
2024-10-15 11:24:26 +0300

    thread pools play

--- concurrency/src/main/java/victor/training/performance/ThreadPools.java
+++ concurrency/src/main/java/victor/training/performance/ThreadPools.java
@@ -1,21 +1,33 @@
 package victor.training.performance;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
+import java.util.concurrent.TimeUnit;
 
-import static victor.training.performance.util.PerformanceUtil.*;
+import static victor.training.performance.util.PerformanceUtil.log;
+import static victor.training.performance.util.PerformanceUtil.sleepMillis;
 
 public class ThreadPools {
   public static void main(String[] args) throws InterruptedException {
     // TODO use a fixed number (3) of threads
-    ExecutorService executor = null; //Executors. ?
+//    ExecutorService executor = Executors.newFixedThreadPool(3);
 
     // TODO create new threads as necessary, and kill idle ones after 1 min
-    // ExecutorService executor = Executors.
+//     ExecutorService executor = Executors.newCachedThreadPool();
 
-    // TODO Start 3 threads but max 10 threads, idle threads killed after 1 second
-    //  Keep max 5 element in the queue. => rejection possible; experiment with different policies
-    // ExecutorService executor = new ThreadPoolExecutor(...)
+    // TODO Start 3 threads but max 10 threads (under heat),
+    //  idle threads killed after 1 second
+    //  Keep max 5 element in the queue.
+    //  => daca vin mai multe rejection possible; experiment with different policies
+    ExecutorService executor = new ThreadPoolExecutor(
+        3, 4,
+        1, TimeUnit.SECONDS,
+        new ArrayBlockingQueue<>(5),
 
+        new CallerRunsPolicy() // o forma primitiva de backpressure.
+    );
     for (int i = 0; i < 40; i++) {
       MyTask task = new MyTask(i, 500);
       log("Submitting #" + i);
@@ -24,6 +36,7 @@ public class ThreadPools {
     }
     // TODO shutdown the executor
   }
+
   record MyTask(int id, int durationMillis) implements Runnable {
     public void run() {
       log("Start #" + id);
