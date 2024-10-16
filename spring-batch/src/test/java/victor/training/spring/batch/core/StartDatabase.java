c539fbe16d1b3bfd85511a57213de2518e9ad57f
Victor Rentea <victorrentea@gmail.com>
2024-10-16 17:51:55 +0300

    cherry batch upgrade to SB3+jakarta

---
+++ spring-batch/src/test/java/victor/training/spring/batch/core/StartDatabase.java
@@ -0,0 +1,32 @@
+package victor.training.spring.batch.core;
+
+import java.io.File;
+import java.sql.SQLException;
+
+public class StartDatabase {
+	public static void main(String[] args) throws SQLException {
+		System.out.println("Started DB...");
+		System.out.println("Connecting to 'jdbc:h2:tcp://localhost:9092/~/test' will auto-create a database file 'test.mv.db' in user home (~)...");
+
+		deletePreviousDB();
+
+		// Allow auto-creating new databases on blockDisk at first connection
+		org.h2.tools.Server.createTcpServer("-ifNotExists").start();
+	}
+
+	private static void deletePreviousDB() {
+		File dbFile = new File(System.getProperty("user.home"), "test.mv.db");
+		System.out.println("Db file: " +dbFile.getAbsolutePath());
+
+		if (dbFile.isFile()) {
+			boolean r = dbFile.delete();
+			if (!r) {
+				System.err.println("Could not delete previous db content!");
+			} else {
+				System.out.println("Previous DB contents wiped out");
+			}
+		} else {
+			System.out.println("No previous DB found");
+		}
+	}
+}
