398089f597013866051e069bc8f8f7e1ceaf4f8b
Victor Rentea <victorrentea@gmail.com>
2024-10-16 17:59:33 +0300

    cleanup unused

--- concurrency/src/main/java/victor/training/performance/drinks/Beer.java
+++
@@ -1,8 +0,0 @@
-package victor.training.performance.drinks;
-
-import lombok.Data;
-
-@Data
-public class Beer {
-   private String type;
-}
--- jpa/src/main/java/victor/training/performance/jpa/uuid/UUIDEntityRepo.java
+++
@@ -1,6 +0,0 @@
-package victor.training.performance.jpa.uuid;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UUIDEntityRepo extends JpaRepository<UUIDEntity, String> {
-}
