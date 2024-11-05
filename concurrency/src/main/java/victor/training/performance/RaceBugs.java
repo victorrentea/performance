f1d3c6d5ef7dff65f2b7f0af9308d031e18a127f
Victor Rentea <victorrentea@gmail.com>
2024-11-05 14:50:13 +0200

    best way to do multithjread design: return rather than change data.

--- concurrency/src/main/java/victor/training/performance/RaceBugs.java
+++ concurrency/src/main/java/victor/training/performance/RaceBugs.java
@@ -7,7 +7,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.Future;
 import java.util.stream.IntStream;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
@@ -20,17 +20,21 @@ public class RaceBugs {
   private static List<Integer> evenNumbers = new ArrayList<>(); // mutable and doesn't lose increments
 
   // to assign sequential request Ids, PKs...
-  private static AtomicInteger total = new AtomicInteger(0);
+//  private static AtomicInteger total = new AtomicInteger(0);
+//  private static Integer total = 0;
 
   // many parallel threads run this method:
-  private static void countEven(List<Integer> numbers) {
+  private static int countEven(List<Integer> numbers) {
+    int localTotal = 0;
     log.info("Start");
     for (Integer n : numbers) {
       if (n % 2 == 0) {
-        total.incrementAndGet();
+        localTotal++;
       }
     }
+//    total += localTotal; // there can still be a race.
     log.info("End");
+    return localTotal;
   }
 
   public static void main(String[] args) throws Exception {
@@ -40,12 +44,32 @@ public class RaceBugs {
     List<List<Integer>> parts = splitList(fullList, 2);
 
     ExecutorService pool = Executors.newCachedThreadPool();
-    for (List<Integer> part : parts) {
-      pool.submit(() -> countEven(part));
-    }
+//    List<Future<Integer>> futureList = new ArrayList<>();
+//    for (List<Integer> part : parts) {
+//      Future<Integer> localTotalFuture = pool.submit(new Callable<Integer>() {
+//        @Override
+//        public Integer call() throws Exception {
+//          return countEven(part);// this method is executed by a worker thread
+//        }
+//      });
+////      Future<Integer> localTotalFuture = pool.submit(() -> countEven(part)); // java 8 style
+//
+//      futureList.add(localTotalFuture);
+//    }
+
+    Future<Integer> firstHalfResultsFuture = pool.submit(() -> countEven(parts.get(0)));
+    Future<Integer> secondHalfResultsFuture = pool.submit(() -> countEven(parts.get(1)));
+    // 2 threads are now counting my even numbers
+    int total = 0;
+    total += firstHalfResultsFuture.get(); // blocks the main thread until the results of the first half are ready
+    total += secondHalfResultsFuture.get();
+
     pool.shutdown();
     pool.awaitTermination(1, MINUTES);
 
+//    for (Future<Integer> future : futureList) {
+//      total += future.get();
+//    }
     log.debug("Counted: " + total);
     log.debug("List.size: " + evenNumbers.size());
   }
