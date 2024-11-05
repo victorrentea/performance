3a19be7da2d9e06dc730e317103136c4e6c5d547
Victor Rentea <victorrentea@gmail.com>
2024-11-05 15:05:33 +0200

    🍒

--- concurrency/src/main/java/victor/training/performance/DeadLocks.java
+++ concurrency/src/main/java/victor/training/performance/DeadLocks.java
@@ -8,55 +8,63 @@ import static victor.training.performance.util.PerformanceUtil.log;
 
 @Slf4j
 public class DeadLocks {
-   @Value
-   static class Fork {
-      int id;
-   }
+  @Value
+  static class Fork {
+    int id;
+  }
 
-   static class Philosopher extends Thread {
-      private final Fork leftFork;
-      private final Fork rightFork;
+  static class Philosopher extends Thread {
+    private final Fork leftFork;
+    private final Fork rightFork;
 
-      public Philosopher(String name, Fork leftFork, Fork rightFork) {
-         super(name);
-         this.leftFork = leftFork;
-         this.rightFork = rightFork;
+    public Philosopher(String name, Fork leftFork, Fork rightFork) {
+      super(name);
+      this.leftFork = leftFork;
+      this.rightFork = rightFork;
+    }
+
+    public void run() {
+      Fork firstFork = leftFork;
+      Fork secondFork = rightFork;
+
+      for (int i = 0; i < 5000; i++) {
+        log("I'm hungry");
+
+        log("Taking fork #" + firstFork.id + "...");
+        synchronized (firstFork) {
+          log("Took one fork");
+          log("Taking fork #" + secondFork.id + "...");
+          synchronized (secondFork) {
+            log("Eating🌟...");
+            // sleepNanos(10);
+            log("Done eating. Releasing forks");
+          }
+        }
+        log("Thinking...");
       }
+      log("NORMAL FINISH (no deadlock happened)");
+    }
+  }
 
-      public void run() {
-         Fork firstFork = leftFork;
-         Fork secondFork = rightFork;
+  public static void main(String[] args) {
+    Thread t = new Thread(() -> {
+      PerformanceUtil.sleepMillis(5_000);
+      long pid = ProcessHandle.current().pid();
+      System.out.println("To see a thread dump run in terminal one of:\njstack " + pid + "\nkill -3 " + pid);
+    });
+    t.setDaemon(true); // allow the process to die
+    t.start();
 
-         for (int i = 0; i < 5000; i++) {
-            log("I'm hungry");
-
-            log("Taking fork #" + firstFork.id + "...");
-            synchronized (firstFork) {
-               log("Took one fork");
-               log("Taking fork #" + secondFork.id + "...");
-               synchronized (secondFork) {
-                  log("Eating🌟...");
-                  // sleepNanos(10);
-                  log("Done eating. Releasing forks");
-               }
-            }
-            log("Thinking...");
-         }
-         log("NORMAL FINISH (no deadlock happened)");
-      }
-   }
-
-   public static void main(String[] args) {
-      DeadLocks.log.debug("Start");
-      Fork[] forks = {new Fork(1), new Fork(2), new Fork(3), new Fork(4), new Fork(5)};
-      new Philosopher("Plato", forks[0], forks[1]).start();
-      PerformanceUtil.sleepNanos(1);
-      new Philosopher("Confucius", forks[1], forks[2]).start();
-      PerformanceUtil.sleepNanos(1);
-      new Philosopher("Socrates", forks[2], forks[3]).start();
-      PerformanceUtil.sleepNanos(1);
-      new Philosopher("Voltaire", forks[3], forks[4]).start();
-      PerformanceUtil.sleepNanos(1);
-      new Philosopher("Descartes", forks[4], forks[0]).start();
-   }
+    DeadLocks.log.debug("Start");
+    Fork[] forks = {new Fork(1), new Fork(2), new Fork(3), new Fork(4), new Fork(5)};
+    new Philosopher("Plato", forks[0], forks[1]).start();
+    PerformanceUtil.sleepNanos(1);
+    new Philosopher("Confucius", forks[1], forks[2]).start();
+    PerformanceUtil.sleepNanos(1);
+    new Philosopher("Socrates", forks[2], forks[3]).start();
+    PerformanceUtil.sleepNanos(1);
+    new Philosopher("Voltaire", forks[3], forks[4]).start();
+    PerformanceUtil.sleepNanos(1);
+    new Philosopher("Descartes", forks[4], forks[0]).start();
+  }
 }
