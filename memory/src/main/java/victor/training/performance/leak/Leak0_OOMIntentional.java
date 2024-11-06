9558af39bfbd7c4483cd3ed9fbc2f97a8a84d97b
Victor Rentea <victorrentea@gmail.com>
2024-11-06 12:29:36 +0200

    leak is permanent in a static field

--- memory/src/main/java/victor/training/performance/leak/Leak0_OOMIntentional.java
+++ memory/src/main/java/victor/training/performance/leak/Leak0_OOMIntentional.java
@@ -3,18 +3,18 @@ package victor.training.performance.leak;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import victor.training.performance.leak.obj.BigObject20MB;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@RestController // is a singleton (one isntace is kept in memory)
 @RequestMapping("leak0")
 public class Leak0_OOMIntentional {
 
+  static List<int[]> boom = new ArrayList<>();
+
   @GetMapping
   public String endpoint() {
-    List<int[]> boom = new ArrayList<>();
     while (true) boom.add(new int[1000_000]);
     // throw new OutOfMemoryError("Does not generate heapdump despite the flags");
   }
