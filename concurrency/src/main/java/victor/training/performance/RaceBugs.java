8f4fcd319f507185cf6d0114aa9a6735e2965838
Victor Rentea <victorrentea@gmail.com>
2025-03-12 14:05:12 +0200

    map-reduce. return, don't change shared mutable data in multithreaded flows

--- concurrency/src/main/java/victor/training/performance/RaceBugs.java
+++ concurrency/src/main/java/victor/training/performance/RaceBugs.java
@@ -8,7 +8,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.Future;
 import java.util.stream.IntStream;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
@@ -19,22 +19,22 @@ import static java.util.concurrent.TimeUnit.MINUTES;
 public class RaceBugs {
   private static List<Integer> evenNumbers = new ArrayList<>();
 
-  private static AtomicInteger total = new AtomicInteger(0);
-  private static final Object lock = new Object();
+//  private static AtomicInteger total = new AtomicInteger(0);
+//  private static final Object lock = new Object();
 
   // many parallel threads run this method:
   @SneakyThrows
-  private static void countEven(List<Integer> numbers) {
+  private static int countEven(List<Integer> numbers) {
+    // MAP
     log.info("Start");
+    int myTotal = 0;
     for (Integer n : numbers) {
       if (n % 2 == 0) {
-//        System.out.println(n); // logging/delays make the race less likely 1000x more than the next line
-        //Thread.yield();Thread.sleep(0); // ==
-//        total++;
-        total.incrementAndGet();
+        myTotal++;
       }
     }
     log.info("End");
+    return myTotal;
   }
 
   public static void main(String[] args) throws Exception {
@@ -44,12 +44,13 @@ public class RaceBugs {
     List<List<Integer>> parts = splitList(fullList, 2);
 
     ExecutorService pool = Executors.newCachedThreadPool();
-    for (List<Integer> part : parts) {
-      pool.submit(()-> countEven(part)); // twice
-    }
+    // FORK
+    Future<Integer> future1 = pool.submit(() -> countEven(parts.get(0)));
+    Future<Integer> future2 = pool.submit(() -> countEven(parts.get(1)));
     pool.shutdown();
     pool.awaitTermination(1, MINUTES);
-
+    //REDUCE
+    var total = future1.get() + future2.get();
     log.debug("Counted: " + total);
     log.debug("List.size: " + evenNumbers.size());
   }
