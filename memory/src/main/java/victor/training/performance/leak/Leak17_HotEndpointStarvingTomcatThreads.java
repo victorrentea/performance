76edc5c286790b200f7d1bdfbcf9401ae370c5c2
Victor Rentea <victorrentea@gmail.com>
2025-05-16 13:28:37 +0300

    shape

--- memory/src/main/java/victor/training/performance/leak/Leak17_HotEndpointStarvingTomcatThreads.java
+++ memory/src/main/java/victor/training/performance/leak/Leak17_HotEndpointStarvingTomcatThreads.java
@@ -1,5 +1,6 @@
 package victor.training.performance.leak;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,41 +11,29 @@ import java.time.LocalDateTime;
 import java.util.concurrent.Semaphore;
 
 import static org.springframework.http.ResponseEntity.ok;
-import static org.springframework.http.ResponseEntity.status;
 import static victor.training.performance.util.PerformanceUtil.sleepMillis;
 
 @Slf4j
 @RestController
 @RequestMapping("leak17")
 public class Leak17_HotEndpointStarvingTomcatThreads {
-   private final Semaphore semaphore = new Semaphore(199);
+  private final Semaphore semaphore = new Semaphore(199);
 
-   // accept 8 CPU-bound tasks on 8 physical cores to avoid context-switch penalty
-//   private final Semaphore semaphore = new Semaphore(
-//       Runtime.getRuntime().availableProcessors());
+  //   @Bulkhead // resilience4j
+  @GetMapping // call 200 times with jMeter to saturate Tomcat's thread pool
+  public ResponseEntity<Void> hotEndpoint() throws InterruptedException {
+    tensorFlow();
+    return ok(null);
+  }
 
-//   @Bulkhead // via config of resilience4j
-   @GetMapping // call 200 times to saturate Tomcat's thread pool
-   public ResponseEntity<Void> hotEndpoint() throws InterruptedException {
-      var gotAPermit = semaphore.tryAcquire();
-      if (!gotAPermit) {
-         return status(503).build();
-      }
-      try {
-         tensorFlow();
-      } finally {
-         semaphore.release();
-      }
-      return ok(null);
-   }
+  @GetMapping("/liveness")
+  public String liveness(HttpServletRequest request) {
+    return "k8s, 🙏 please don't kill me: request at %s, response at %s "
+        .formatted(request.getDateHeader("Date"), LocalDateTime.now());
+  }
 
-   private void tensorFlow() {
-      sleepMillis(60_000); // pretend CPU
-      sleepMillis(60_000); // criminal SQL
-   }
-
-   @GetMapping("/liveness")
-   public String liveness() {
-     return "k8s, please don't kill me: returned at " + LocalDateTime.now();
-   }
+  private void tensorFlow() {
+    sleepMillis(20_000); // pretend CPU
+    sleepMillis(20_000); // criminal SQL
+  }
 }
