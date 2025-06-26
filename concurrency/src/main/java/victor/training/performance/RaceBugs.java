eb8b39e140325352e5f2da4730986eccbd5fd167
Victor Rentea <victorrentea@gmail.com>
2025-06-26 15:46:43 +0300

    return don't change mutable state.

--- concurrency/src/main/java/victor/training/performance/RaceBugs.java
+++ concurrency/src/main/java/victor/training/performance/RaceBugs.java
@@ -7,6 +7,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.IntStream;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
@@ -17,31 +18,35 @@ import static java.util.concurrent.TimeUnit.MINUTES;
 public class RaceBugs {
   private static List<Integer> evenNumbers = new ArrayList<>();
 
-  private static Integer total = 0;
-  private static final Object lock = new Object();
-
   // many threads run in parallel this method:
-  private static void countEven(List<Integer> numbers) {
+  private static int countEven(List<Integer> numbers) {
+    int total = 0;
     log.info("Start");
     for (Integer n : numbers) {
       if (n % 2 == 0) {
-        synchronized (lock) {
-          total ++;
-        }
+        total ++;
       }
     }
     log.info("End");
+    return total;
   }
 
   public static void main(String[] args) throws Exception {
     List<Integer> fullList = IntStream.range(0, 10_000).boxed().toList();
 
     // split in [[1..500],[501..1000]]
-    List<List<Integer>> parts = splitList(fullList, 2);
+    List<List<Integer>> parts = splitList(fullList, 4);
 
     ExecutorService pool = Executors.newCachedThreadPool();
+    int total = 0;
+    List<Future<Integer>> futures = new ArrayList<>();
     for (List<Integer> part : parts) {
-      pool.submit(()-> countEven(part));
+      var future = pool.submit(() -> countEven(part));
+      futures.add(future);
+    }
+    // aici zboara 2 threaduri libere
+    for (var future : futures) {
+      total += future.get();
     }
     pool.shutdown();
     pool.awaitTermination(1, MINUTES);
