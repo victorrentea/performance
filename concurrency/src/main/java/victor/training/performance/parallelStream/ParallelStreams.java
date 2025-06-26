69bd03a8d0f8773fbbd1f550b910184aca087554
Victor Rentea <victorrentea@gmail.com>
2025-06-26 17:05:31 +0300

    in prod e la fel de prost, ca alt flow (eg un poller, alt endpoint) e MAI animal ca tine si sta blocat in taskul pe pe ForkJoinPool.commonPool INDECENT DE MULT.

--- concurrency/src/main/java/victor/training/performance/parallelStream/ParallelStreams.java
+++ concurrency/src/main/java/victor/training/performance/parallelStream/ParallelStreams.java
@@ -11,12 +11,15 @@ import static victor.training.performance.util.PerformanceUtil.sleepMillis;
 @Slf4j
 public class ParallelStreams {
   public static void main(String[] args) throws ExecutionException, InterruptedException {
-    // OnAServer.otherParallelRequestsAreRunning(); // starve the shared commonPool din JVM
+     OnAServer.otherParallelRequestsAreRunning(); // mai animale ca tine starve the shared commonPool din JVM
 
     List<Integer> list = IntStream.range(1, 100).boxed().toList();
 
     long t0 = System.currentTimeMillis();
 
+//    ForkJoinPool.commonPool() // ruleaza TOATE
+    // a) parallelStream si
+    // b) CompletableFuture carora nu le-ai specificat un executor
     var result = list.parallelStream()
         .filter(i -> i % 2 == 0)
         .map(i -> process(i))
