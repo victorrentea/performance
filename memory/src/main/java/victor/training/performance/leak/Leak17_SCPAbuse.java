54ba4d5b6437a9931168f6928927a342e57dd073
Victor Rentea <victorrentea@gmail.com>
2025-06-27 11:16:24 +0300

    +intern

--- memory/src/main/java/victor/training/performance/leak/Leak17_SCPAbuse.java
+++ memory/src/main/java/victor/training/performance/leak/Leak17_SCPAbuse.java
@@ -1,38 +1,38 @@
-//package victor.training.performance.leak;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.WeakHashMap;
-//
-//import static org.springframework.http.ResponseEntity.ok;
-//
-//// WIP
-//@Slf4j
-//@RestController
-//@RequestMapping("leak18")
-//public class Leak17_SCPAbuse {
-//  @GetMapping
-//  public ResponseEntity<Void> hotEndpoint(String userData) throws InterruptedException {
-////    userData = userData.intern();
-//    String finalUserData = userData;
-//    userData = canonicStrings.computeIfAbsent(userData, (a, k) -> finalUserData);
-//    return ok(null);
-//  }
-//
-//  // daca nimeni altcineva nu refera cheia, GC poate sterge entry
-//  private static final WeakHashMap<String, String> canonicStrings = new WeakHashMap<>();
-//
-//  public static void main(String[] args) {
-//    String s="a";
-//
-//    f();
-//  }
-//
-//  private static void f() {
-//    String s = "a";
-//  }
-//}
+package victor.training.performance.leak;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
+import java.util.WeakHashMap;
+
+// WIP
+@Slf4j
+@RestController
+@RequestMapping("leak18")
+public class Leak17_SCPAbuse {
+  @GetMapping
+  public List<Integer> hotEndpoint(List<String> tokens) throws InterruptedException {
+    List<String> canonicalizedTokens = tokens.stream().map(s->canonicalize(s)).toList();
+    return canonicalizedTokens.stream().map(s-> System.identityHashCode(s)).toList();
+  }
+
+  private String canonicalize(String original) {
+    return original.intern(); // bad
+//    return canonicStrings.computeIfAbsent(original, unused -> new WeakReference<>(original)).get();
+  }
+
+  // if no one references a key, entry is removed
+  private static final WeakHashMap<String, WeakReference<String>> canonicStrings = new WeakHashMap<>();
+
+  public static void main(String[] args) {
+    String s="a";
+    f(s);
+  }
+  private static void f(String s) {
+    System.out.println(s == "a");
+  }
+}
