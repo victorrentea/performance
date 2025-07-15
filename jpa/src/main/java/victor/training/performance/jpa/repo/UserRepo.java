233f02f6c4c70098514ac2ad6e31a8aafba9f0bb
Victor Rentea <victorrentea@gmail.com>
2025-07-15 15:26:09 +0300

    remove unused

--- jpa/src/main/java/victor/training/performance/jpa/repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package victor.training.performance.jpa.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import victor.training.performance.jpa.entity.User;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-
-}
