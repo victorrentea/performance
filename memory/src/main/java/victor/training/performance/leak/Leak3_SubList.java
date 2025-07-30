836625e5f44ea75201d2a3d56d056827cedaa866
Victor Rentea <victorrentea@gmail.com>
2025-07-30 14:40:37 +0300

    cherry

--- memory/src/main/java/victor/training/performance/leak/Leak3_SubList.java
+++ memory/src/main/java/victor/training/performance/leak/Leak3_SubList.java
@@ -1,19 +1,15 @@
 package victor.training.performance.leak;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
-import static java.util.Collections.list;
-import static java.util.stream.Collectors.toMap;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,33 +18,27 @@ import static java.util.stream.Collectors.toMap;
 public class Leak3_SubList {
    private List<Access> lastTenAccesses = new ArrayList<>();
 
-   record Access(String ip, Map<String, String> headers, LocalDateTime timestamp) {
+   record Access(String ip, LocalDateTime timestamp) {
    }
    @GetMapping
    public synchronized String endpoint(HttpServletRequest request) {
-      var access = createAccess(request);
+      Access access = new Access(request.getRemoteAddr() + ":" + request.getRemotePort(), LocalDateTime.now());
 
       lastTenAccesses.add(access);
       if (lastTenAccesses.size() > 10) {
-         // slide the window to right
-         lastTenAccesses = lastTenAccesses.subList(1, lastTenAccesses.size());
+         lastTenAccesses = lastTenAccesses.subList(1, lastTenAccesses.size()); // skip first
       }
       return "The current window size is " + lastTenAccesses.size();
-   }
-
-   private Access createAccess(HttpServletRequest request) {
-      var headersMap = list(request.getHeaderNames()).stream()
-          .collect(toMap(name -> name, request::getHeader));
-      var hostPort = request.getRemoteAddr() + ":" + request.getRemotePort();
-      return new Access(hostPort, headersMap, LocalDateTime.now());
    }
 
    @GetMapping("many")
-   public String many(HttpServletRequest request) {
-      for (int i = 0; i < 1_000; i++) {
-         endpoint(request);
+   public String mass(HttpServletRequest request) {
+//      RestTemplate rest = new RestTemplate();
+      for (int i = 0; i < 10_000; i++) {
+//          rest.getForObject("http://localhost:8080/leak3", String.class);
+         endpoint(request); // close enough for our experiment
       }
-      return "The current window size is " + lastTenAccesses.size();
+      return "The current window size is " + lastTenAccesses.size() + ": " + lastTenAccesses;
    }
 }
 
