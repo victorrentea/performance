dabf3e32c8e8b0cec40810150b2fa00f32db6b26
Victor Rentea <victorrentea@gmail.com>
2025-07-30 14:18:53 +0300

    copy thread local from parent thread to worker thread(s). #musthave for eg TraceId (opentelemtry)_

--- memory/src/main/java/victor/training/performance/ThreadLocalIntro.java
+++ memory/src/main/java/victor/training/performance/ThreadLocalIntro.java
@@ -12,24 +12,25 @@ import static victor.training.performance.util.PerformanceUtil.sleepMillis;
 
 @Slf4j
 public class ThreadLocalIntro {
-    private final AController controller = new AController(new AService(new ARepo()));
-    public static void main(String[] args) {
-        ThreadLocalIntro app = new ThreadLocalIntro();
-        System.out.println("Imagine incoming HTTP requests...");
-        CompletableFuture.runAsync(() -> app.httpRequest("alice", "alice's data"));
-        CompletableFuture.runAsync(() -> app.httpRequest("bob", "bob's data"));
-        sleepMillis(1000);
-    }
+  private final AController controller = new AController(new AService(new ARepo()));
 
-    public void httpRequest(String currentUser, String data) {
-        log.info("Current user is " + currentUser);
-        staticCurrentUser.set(currentUser);
-        controller.create(data);
-    }
+  public static void main(String[] args) {
+    ThreadLocalIntro app = new ThreadLocalIntro();
+    System.out.println("Imagine incoming HTTP requests...");
+    CompletableFuture.runAsync(() -> app.httpRequest("alice", "alice's data"));
+    CompletableFuture.runAsync(() -> app.httpRequest("bob", "bob's data"));
+    sleepMillis(1000);
+  }
 
-    // each thread sees its own copy of that string.
-    // more examples: traceId, @Transactional, tenantId, operatorId, clientTimestamp, SecurityContext
-    public static ThreadLocal<String> staticCurrentUser = new InheritableThreadLocal<>();
+  public void httpRequest(String currentUser, String data) {
+    log.info("Current user is " + currentUser);
+    staticCurrentUser.set(currentUser);
+    controller.create(data);
+  }
+
+  // each thread sees its own copy of that string.
+  // more examples: traceId, @Transactional, tenantId, operatorId, clientTimestamp, SecurityContext
+  public static ThreadLocal<String> staticCurrentUser = new InheritableThreadLocal<>();
 }
 // ---------- end of framework -----------
 
@@ -37,35 +38,42 @@ public class ThreadLocalIntro {
 @RestController
 @RequiredArgsConstructor
 class AController {
-    private final AService service;
+  private final AService service;
 
-    public void create(String data) {
-        service.create(data);
-    }
+  public void create(String data) {
+    service.create(data);
+  }
 }
 
 // ----------- Service ------------
 @Service
 @RequiredArgsConstructor
 class AService {
-    private final ARepo repo;
+  private final ARepo repo;
 
-    public void create(String data) {
-        sleepMillis(10); // some delay, to reproduce the race bug
-        repo.save(data);
-    }
+  public void create(String data) {
+    sleepMillis(10); // some delay, to reproduce the race bug
+    repo.save(data);
+  }
 }
 
 // ----------- Repository ------------
 @Repository
 @Slf4j
 class ARepo {
-    public void save(String data) {
+  public void save(String data) {
 //        new Thread(()-> { // Allocated .5 MB
-        CompletableFuture.runAsync(()-> {
-            // DON'T. except if spawning Virtual Threads = 1kb Executors.ofVirtual()
-            String currentUser = ThreadLocalIntro.staticCurrentUser.get();
-          log.info("INSERT INTO A (data={}, created_by={}) ", data, currentUser);
-        });
-    }
+
+    // start of framework
+    String currentUser = ThreadLocalIntro.staticCurrentUser.get();
+    CompletableFuture.runAsync(() -> {
+    ThreadLocalIntro.staticCurrentUser.set(currentUser);
+      // end of framework
+
+
+      // DON'T. except if spawning Virtual Threads = 1kb Executors.ofVirtual()
+      String currentUser2 = ThreadLocalIntro.staticCurrentUser.get();
+      log.info("INSERT INTO A (data={}, created_by={}) ", data, currentUser2);
+    });
+  }
 }
