ae75e9fa03a993ec3828367eb9361015eca5ef4c
Victor Rentea <victorrentea@gmail.com>
2025-10-05 16:11:01 +0300

    wip

--- memory/src/main/java/victor/training/performance/leak/Leak6_Files.java
+++ memory/src/main/java/victor/training/performance/leak/Leak6_Files.java
@@ -11,16 +11,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static victor.training.performance.leak.Leak6_Helper.fetchData;
 import static victor.training.performance.util.PerformanceUtil.sleepMillis;
 
 @Slf4j
@@ -28,33 +29,16 @@ import static victor.training.performance.util.PerformanceUtil.sleepMillis;
 @RequiredArgsConstructor
 public class Leak6_Files {
   private final FileProcessor processor;
-
   private static final AtomicInteger counter = new AtomicInteger(0);
 
   @GetMapping("leak6/download") // 🔥 Leak6Load.java
   public String download() {
-    String fileContents = RestClient.create()
-        .get().uri("http://localhost:8080/leak6/file?mb=10")
-        .retrieve()
-        .body(String.class);
-    log.info("Downloaded bytes: " + fileContents.getBytes().length);
-    return processAsync(fileContents) + " repeat 20x within 10 seconds, then heap dump";
-  }
-
-  @PostMapping("leak6/upload") // FIXME delete for devoxx.be
-  public int upload(@RequestParam MultipartFile file) throws IOException {
-    byte[] fileContents = file.getBytes();
-    log.info("Uploaded bytes: " + fileContents.length);
-    // spring.servlet.multipart.max-file-size=30MB
-    // spring.servlet.multipart.max-request-size=30MB
-    return processAsync(new String(fileContents));
-  }
-
-  private int processAsync(String fileContents) {
+    String data = fetchData(10_000_000);
     int taskId = counter.incrementAndGet();
-    CompletableFuture.runAsync(() -> processor.process(fileContents, taskId));
-    log.info("Task submitted: " + taskId);
-    return taskId;
+    CompletableFuture.runAsync(() -> processor.process(data, taskId));
+    return "Task (30s) submitted: " + taskId + "<br>" +
+           "Data.bytes: " + data.length() + " <br>" +
+           "Look for 'fjp' in <a href='http://localhost:8080/actuator/prometheus'>metrics</a>";
   }
 }
 
@@ -67,6 +51,23 @@ class FileProcessor {
     sleepMillis(10_000);
     long newLinesCount = contents.chars().filter(c -> c == 10).count();
     log.debug("Task {} completed: lines = {}", taskId, newLinesCount);
+  }
+}
+
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+class Leak6_FilesUpload {
+  private static final AtomicInteger counter = new AtomicInteger(0);
+  private final FileProcessor processor;
+
+  @PostMapping("leak6/upload")
+  public int upload(@RequestParam MultipartFile file) throws IOException {
+    byte[] fileContents = file.getBytes();
+    int taskId = counter.incrementAndGet();
+    CompletableFuture.runAsync(() -> processor.process(new String(fileContents), taskId));
+    return taskId;
   }
 }
 
@@ -100,7 +101,7 @@ class Leak6Config {
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
     executor.setCorePoolSize(4);
     executor.setMaxPoolSize(4);
-    executor.setQueueCapacity(5); // bounded queue -> rejects tasks when full
+    executor.setQueueCapacity(5); // rejects tasks when the queue is full
     executor.setThreadNamePrefix("myexec-");
     executor.initialize();
     return executor;
@@ -137,13 +138,37 @@ class Leak6_Helper {
         """.formatted(file.getAbsolutePath());
   }
 
+  private static final String line = "lol".repeat(20) + "\n";
+
   @GetMapping(value = "file", produces = "text/plain")
   public void generateLargeFile(
-      @RequestParam(value = "mb", required = false, defaultValue = "10") int sizeInMB,
+      @RequestParam(required = false) Integer mb,
+      @RequestParam(required = false) Integer kb,
       HttpServletResponse response) throws IOException {
-    String line = "lol".repeat(10) + "\n";
-    for (int i = 0; i < sizeInMB * 1024 * 1024 / line.length(); i++) {
+    int bytes;
+    if (kb != null) bytes = kb * 1024;
+    else if (mb != null) bytes = mb * 1024 * 1024;
+    else bytes = 10 * 1024 * 1024;
+    int n = bytes / line.length();
+    for (int i = 0; i < n; i++) {
       response.getWriter().write(line);
     }
+  }
+
+  public static String fetchData(int bytes) {
+//    return RestClient.create()
+//        .get().uri("http://localhost:8080/leak6/file?mb=10")
+//        .retrieve()
+//        .body(String.class);
+    return dataFromMemory(bytes);
+  }
+
+  public static String dataFromMemory(int bytes) {
+    StringWriter stringWriter = new StringWriter();
+    int n = bytes / line.length();
+    for (int i = 0; i < n; i++) {
+      stringWriter.write(line);
+    }
+    return stringWriter.toString();
   }
 }
