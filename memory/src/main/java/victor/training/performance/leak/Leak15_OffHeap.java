a389c940d0e30ad69c7f5cbf1968d410e419568c
Victor Rentea <victorrentea@gmail.com>
2025-10-05 20:02:28 +0300

    invisible

--- memory/src/main/java/victor/training/performance/leak/Leak15_OffHeap.java
+++ memory/src/main/java/victor/training/performance/leak/Leak15_OffHeap.java
@@ -5,23 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.ByteBuffer;
-import java.util.concurrent.CompletableFuture;
 
 import static victor.training.performance.util.PerformanceUtil.sleepSeconds;
 
 @Slf4j
 @RestController
 public class Leak15_OffHeap {
-  public static final int MB100 = 100 * 1024 * 1024;
+  public static final int MB10 = 100 * 1024 * 1024;
 
   @GetMapping("leak15")
   public String offHeap() {
-    CompletableFuture.runAsync(() -> {
-      log.info("start");
-      ByteBuffer byteBuffer = ByteBuffer.allocateDirect(MB100);
-      sleepSeconds(10);
-      log.info("end");
-    });
-    return "Heap doesn't grow much";
+    log.info("start");
+    ByteBuffer byteBuffer = ByteBuffer.allocateDirect(MB10);
+    sleepSeconds(10);
+    return "Heap doesn't grow much " + byteBuffer;
   }
 }
