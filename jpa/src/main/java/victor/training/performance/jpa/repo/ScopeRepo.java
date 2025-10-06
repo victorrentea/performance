60339ff7fa9b54640c9f2c6338abef7c6e10108e
Victor Rentea <victorrentea@gmail.com>
2025-10-06 19:29:01 +0200

    devoxx.be❤️

--- jpa/src/main/java/victor/training/performance/jpa/repo/ScopeRepo.java
+++
@@ -1,7 +0,0 @@
-package victor.training.performance.jpa.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import victor.training.performance.jpa.entity.Scope;
-
-public interface ScopeRepo extends JpaRepository<Scope, Long> {
-}
