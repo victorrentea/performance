67824f87bb79ec9df17884b2c232d07d2649faa0
Victor Rentea <victorrentea@gmail.com>
2025-11-13 16:39:07 +0200

    run a parallelStream on a custom ForkJoinPool (DONT)

--- concurrency/src/main/java/victor/training/performance/parallelStream/ParallelStreams.java
+++ concurrency/src/main/java/victor/training/performance/parallelStream/ParallelStreams.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
 
 import static victor.training.performance.util.PerformanceUtil.sleepMillis;
@@ -11,17 +12,19 @@ import static victor.training.performance.util.PerformanceUtil.sleepMillis;
 @Slf4j
 public class ParallelStreams {
   public static void main(String[] args) throws ExecutionException, InterruptedException {
-     OnAServer.otherParallelRequestsAreRunning(); // starve the shared commonPool din JVM
+    OnAServer.otherParallelRequestsAreRunning(); // starve the shared commonPool din JVM
 
     List<Integer> list = IntStream.rangeClosed(1, 100).boxed().toList();
 
     long t0 = System.currentTimeMillis();
 
-    var result = list.parallelStream()
-        .filter(i -> i % 2 == 0)
-        .map(id -> networkCall(id))
-        // runs on ForkJoinPool.commonCommon pool with 9 threads on vic's mac= 10CPU -1 (requestor)
-        .toList();
+    ForkJoinPool fjp = new ForkJoinPool(20);
+    var result = fjp.submit(() -> list.parallelStream()
+            .filter(i -> i % 2 == 0)
+            .map(id -> networkCall(id))
+            // runs on ForkJoinPool.commonCommon pool with 9 threads on vic's mac= 10CPU -1 (requestor)
+            .toList()) // "terminate" a parallel stream in a FJP task
+        .get();
     // knowing that the common pool has N_CPU - 1 threads,
     // what kind of tasks should you run on parallel stream?
     // => CPU-bound tasks!!! no i/o: DB/API call/Redis/Files
