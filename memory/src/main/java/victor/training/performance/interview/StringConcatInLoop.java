d36192a8be43dc290f244d211b650531165b95d5
Victor Rentea <victorrentea@gmail.com>
2025-11-13 17:22:17 +0200

    streamlining data, without ever accumulating it all.

--- memory/src/main/java/victor/training/performance/interview/StringConcatInLoop.java
+++ memory/src/main/java/victor/training/performance/interview/StringConcatInLoop.java
@@ -4,27 +4,39 @@ import victor.training.performance.util.PerformanceUtil;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StringConcatInLoop {
    public static void main(String[] args) throws IOException {
       PerformanceUtil.printJfrFile();
 
-      List<String> elements = IntStream.range(1, 50_000)
-          .mapToObj(n -> "hahaha") // 6 x 2 bytes/char = 12 bytes / element
-          .toList();
+//      List<String> elements = fromDb() // 6 x 2 bytes/char = 12 bytes / element
+//          .toList();
 
       System.out.println("Start...");
       long t0 = System.currentTimeMillis();
 
+
       // TODO: OPINIONS about this code ?
      try (FileWriter fileWriter = new FileWriter("out.txt")) {
-       for (String element : elements) {
-         fileWriter.write(element);
-       }
+       fromDb().forEach(element-> {
+         try {
+           fileWriter.write(element);
+         } catch (IOException e) {
+           throw new RuntimeException(e);
+         }
+       });
+//       for (String element : elements) {
+//         fileWriter.write(element);
+//       }
      }
 
       System.out.println("Done. Took " + (System.currentTimeMillis() - t0));
    }
+
+  private static Stream<String> fromDb() {
+    return IntStream.range(1, 50_000)
+        .mapToObj(n -> "hahaha");
+  }
 }
