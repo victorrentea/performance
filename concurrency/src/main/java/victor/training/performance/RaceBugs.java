adc5baae417a43075242a87aac4c60b06f0edb8e
Victor Rentea <victorrentea@gmail.com>
2025-11-13 10:24:03 +0200

    map-reduce: split the work in indep parts, parallelize, join results at the end

--- concurrency/src/main/java/victor/training/performance/RaceBugs.java
+++ concurrency/src/main/java/victor/training/performance/RaceBugs.java
@@ -7,6 +7,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
@@ -15,37 +17,47 @@ import static java.util.concurrent.TimeUnit.MINUTES;
 @SuppressWarnings("ALL")
 @Slf4j
 public class RaceBugs {
-  private static List<Integer> evenNumbers = new ArrayList<>();
 
-//  private static AtomicInteger total = new AtomicInteger(0);
-  private static Integer total = 0;
+  private static AtomicInteger total = new AtomicInteger(0);
+  private static List<Integer> even = Collections.synchronizedList(new ArrayList<>());
+//  private static Integer total = 0;
 //  private static final Object LOCK = new Object();
   // many parallel threads run this method:
-  private static void countEven(List<Integer> numbers) {
+  private static List<Integer> countEven(List<Integer> numbers) {
     log.info("Start");
+    List<Integer> myResults = new ArrayList<>();
     for (Integer n : numbers) {
       if (n % 2 == 0) {
-        log.info("now: "+total); // log / breakpoint makes the race dissapear
-        total++;
+        myResults.add(n);
       }
     }
     log.info("End");
+    return myResults;
   }
 
   public static void main(String[] args) throws Exception {
     List<Integer> fullList = IntStream.range(0, 10000).boxed().toList();
     // split in [[1..500],[501..1000]]
-    List<List<Integer>> parts = splitList(fullList, 2);
+    List<List<Integer>> parts = splitList(fullList, 4);
 
     ExecutorService pool = Executors.newCachedThreadPool();
+    List<Future<List<Integer>>> futures = new ArrayList<>();
     for (List<Integer> part : parts) {
-      pool.submit(()-> countEven(part));
+      Future<List<Integer>> future = pool.submit(()-> countEven(part));
+      futures.add(future);
+//      future.get();// blocks main thread until the work is done => all work happens sequentially
     }
+    List<Integer> finalResults = new ArrayList<>();
+    for (Future<List<Integer>> future : futures) {
+      List<Integer> partialResults = future.get();
+      finalResults.addAll(partialResults);
+    }
+
     pool.shutdown();
     pool.awaitTermination(1, MINUTES);
 
     log.debug("Counted: " + total);
-    log.debug("List.size: " + evenNumbers.size());
+    log.debug("List.size: " + finalResults.size());
   }
 
   //<editor-fold desc="utility functions">
